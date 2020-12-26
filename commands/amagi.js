f0ac7ffc8f6e4ad5bd6c0b81096b90a2fd1c6c3b
MauwMa <sgtmauwma@gmail.com>
2020-12-26 08:44:16 +0100

    Akashi

---
+++ commands/amagi.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'amagi',
+    description: 'amagi',
+    execute (message, args){
+
+        if(message.member.roles.cache.has('789941230171062293')){
+            
+
+        } else {
+            message.channel.send('Sikeresen csatlakoztál az Amagi szerverhez nya~');
+            message.member.roles.add('789941230171062293');
+            message.member.roles.remove('789941238421651476');
+            message.member.roles.remove('789941240912805898');
+            message.member.roles.remove('789941242913882143');
+            message.member.roles.remove('789941245307781150');
+        }
+
+
+    }}
