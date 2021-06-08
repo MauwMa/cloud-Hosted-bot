12c8647e1c1970e90db1dc3ef3ab189af40c50ac
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-08 10:26:13 +0200

    Update nagato.js

--- commands/nagato.js
+++ commands/nagato.js
@@ -1,18 +1,15 @@
 module.exports = {
     name: 'nagato',
-    description: 'opsiren',
+    description: 'nagato',
     execute (message, args, Discord){
-        const newEmbed = new Discord.MessageEmbed()
+        const newEmbed1 = new Discord.MessageEmbed()
         .setColor('#304281')
-        .setTitle('Nagato build')
+        .setTitle('Operation Siren Útmutatók')
         .setURL('')
         .setDescription('')
         .addFields('')
-        .setImage ('https://pbs.twimg.com/media/D3TwNO6V4AAqRvc.jpg')
-        .setFooter('by Cica');
-
-
-        message.channel.send(newEmbed);
+        .setImage('https://pbs.twimg.com/media/D3TwNO6V4AAqRvc.jpg')
+        message.channel.send(newEmbed1);
 
     }    
 }
