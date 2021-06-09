3d470bd000c1d1db47c150df8949221cb43d9be6
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-09 12:54:19 +0200

    eee

--- commands/eloora4.js
+++ commands/eloora4.js
@@ -1,14 +1,14 @@
 module.exports = {
-    name: 'eloora',
-    description: 'eloora',
+    name: 'eloora4',
+    description: 'eloora4',
     execute (message, args, Discord){
-        const elooran = new Discord.MessageEmbed()
+        const eloora4 = new Discord.MessageEmbed()
         .setColor('#ffff00')
         .setTitle('Értesítés!')
         .setURL('')
         .setDescription('1 órás  előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 4 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
         .setImage('https://i.imgur.com/0BEHNji.png')
-        message.channel.send(elooran);
+        message.channel.send(eloora4);
      if (message.deletable) message.delete();
     }    
 }
--- commands/eloora8.js
+++ commands/eloora8.js
@@ -1,14 +1,14 @@
 module.exports = {
-    name: 'eloora',
-    description: 'eloora',
+    name: 'eloora8',
+    description: 'eloora8',
     execute (message, args, Discord){
-        const eloorany = new Discord.MessageEmbed()
+        const eloora8 = new Discord.MessageEmbed()
         .setColor('#ffff00')
         .setTitle('Értesítés!')
         .setURL('')
         .setDescription('1 órás  előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 8 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
         .setImage('https://i.imgur.com/0BEHNji.png')
-        message.channel.send(eloorany);
+        message.channel.send(eloora8);
      if (message.deletable) message.delete();
     }    
 }
