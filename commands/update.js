5df2e5e223c49a72aff6f52b971fd6266e418ff6
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-19 08:54:32 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -4,10 +4,10 @@ module.exports = {
     execute (message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('Dawning Ceremony Rerun')
+        .setTitle('Tervrajzok Izumohoz')
         .setURL('')
-        .setDescription('A bevezető event Dawning Ceremony Rerun a Swirling Cherry Blossoms Rerun eventre a karbantartás után elérhető lesz. Gyűjts extra tapasztalatot Akatsuki, Shigure, Shoukaku és Atagóra, hogy felkészülj az eventre.')
-        .setImage('https://images-ext-1.discordapp.net/external/jUmQZECMQtVhjUYBs14bXcmnays578Bd660BHUZiK18/https/pbs.twimg.com/media/E4AZ9nLVkAYsoAO.png%3Alarge')
+        .setDescription('Parancsnokok,\n\n A tervrajz kiegészítő terv Izumohoz elérhető.\nParancsnokok extra Izumo tervrajzokat kaphatnak minden kutatás után.\n A maximum Izumo tervrajz, ami kapható ebben az eventben 30 darab nyaaa~')
+        .setImage('https://images-ext-1.discordapp.net/external/EUn7S8zvc3Y2NwSh_nlbv9oKLb1MkNKigGoFaDtYy1s/https/pbs.twimg.com/media/E4JyatSVkAIe_qN.png%3Alarge')
         message.channel.send(update);
     if (message.deletable) message.delete();
     }    
