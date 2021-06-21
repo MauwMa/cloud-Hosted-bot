899c1472515c146acb9229fe9b643e1991777813
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-21 21:18:33 +0200

    jj

--- commands/eloora8.js
+++ commands/eloora8.js
@@ -6,7 +6,7 @@ module.exports = {
         .setColor('#ffff00')
         .setTitle('Értesítés!')
         .setURL('')
-        .setDescription('1 órás  előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 8 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
+        .setDescription('1 órás előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 8 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
         .setImage('https://i.imgur.com/0BEHNji.png')
         message.channel.send(eloora8);
      if (message.deletable) message.delete();
