3c046b8356d213aabb426a6c962b14bacb10c624
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-22 14:57:04 +0200

    Update karban.js

--- commands/karban.js
+++ commands/karban.js
@@ -6,7 +6,7 @@ module.exports = {
         .setColor('#ffff00')
         .setTitle('Karbantartás!!')
         .setURL('')
-        .setDescription('Parancsnokok, 6/17 Csütörtökön Magyar Időszámítás szerint \nreggel 09:00-kor lesz karbantartás. Kérjük a beosztásaitokat tervezzétek e szerint nyaaa~.')
+        .setDescription('Parancsnokok, 6/24 Csütörtökön Magyar Időszámítás szerint \nreggel 09:00-kor lesz karbantartás. Kérjük a beosztásaitokat tervezzétek e szerint nyaaa~.')
         .setImage('https://i.imgur.com/0BEHNji.png')
         message.channel.send(karban);
     if (message.deletable) message.delete();
