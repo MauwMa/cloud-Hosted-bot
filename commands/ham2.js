4f6e50048b5e02afbe0d916f8c879ebfe4bbeda0
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-22 22:43:44 +0200

    Create ham2.js

---
+++ commands/ham2.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'ham',
+    description: 'ham',
+    execute (client, message, args){
+
+        message.channel.send("<a:AkashiGemDance:721896652126027826>")
+
+    if (message.deletable) message.delete();
+
+    }    
+}
+
+
