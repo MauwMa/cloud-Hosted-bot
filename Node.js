ab1cdb7e2e616e9680eb577b5ca5fa5bab6bce8c
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-22 09:13:16 +0200

    csre

--- Node.js
+++ Node.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
-  
-const fs = require('fs');
- 
+
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
