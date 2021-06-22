29f611f1a8819f6c2d319bbf1218fceaddd27967
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-22 22:22:40 +0200

    Create ham.js

---
+++ commands/ham.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'ham',
+    description: 'ham',
+    execute (client, message, args){
+
+        message.channel.send(":akashigem:");
+
+    if (message.deletable) message.delete();
+
+    }    
+}
