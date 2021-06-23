14fc9638739fff4e9269e665b261022b8d9d8bee
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-23 14:01:54 +0200

    ss

--- commands/egyora.js
+++ commands/egyora.js
@@ -1,19 +1,19 @@
 const schedule = require('node-schedule');
 module.exports = {
-    name: 'idu',
-    description: 'idu',
+    name: 'egyora',
+    description: 'egyora',
     execute (client, message, args, Discord){
 
 
 
         var exportScheduler = schedule.scheduleJob('0 01 12 23 6 *', function() { 
-            const eloora4 = new Discord.MessageEmbed()
+            const egyora = new Discord.MessageEmbed()
             .setColor('#ffff00')
             .setTitle('Értesítés!')
             .setURL('')
             .setDescription('1 órás előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 4 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
             .setImage('https://i.imgur.com/0BEHNji.png')
-            message.channel.send(eloora4); 
+            message.channel.send(egyora); 
         });
 
         //if (message.deletable) message.delete();
