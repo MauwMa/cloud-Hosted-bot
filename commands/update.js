1fa6da6fd47c8ece86a19f13551c47df18155700
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-23 09:19:04 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -4,9 +4,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('Kollab')
+        .setTitle('❀Kazagumo❀')
         .setURL('')
-        .setDescription('★Azur Lane X THE IDOLM@STER kollaborálás★\n\nTöbb információ a kollaborálásról hamarosan nyaaa~.')
+        .setDescription('IJN Kazagumo felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/VnPZpv5uKDwi_-JrzaPieTAwEa-4JjzHx0YORi-U5js/https/pbs.twimg.com/media/E4i5ZvsUUAYR7Ii.jpg%3Alarge?width=1291&height=676')
         message.channel.send(update);
     if (message.deletable) message.delete();
     }    
