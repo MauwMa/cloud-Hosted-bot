733d5da127c0579df13af280ade61ae591930136
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-23 13:58:23 +0200

    Update idu.js

--- commands/idu.js
+++ commands/idu.js
@@ -4,8 +4,16 @@ module.exports = {
     description: 'idu',
     execute (client, message, args, Discord){
 
-        var exportScheduler = schedule.scheduleJob('0 55 11 23 6 *', function() { 
-            message.channel.send("Parancsnokok, véget ért a karbantartás.\nNe felejtsetek venni gemeket tőlem nyaaa~!\n everyone"); 
+
+
+        var exportScheduler = schedule.scheduleJob('0 01 12 23 6 *', function() { 
+            const eloora4 = new Discord.MessageEmbed()
+            .setColor('#ffff00')
+            .setTitle('Értesítés!')
+            .setURL('')
+            .setDescription('1 órás előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 4 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
+            .setImage('https://i.imgur.com/0BEHNji.png')
+            message.channel.send(eloora4); 
         });
 
         //if (message.deletable) message.delete();
