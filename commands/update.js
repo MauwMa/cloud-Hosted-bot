6159f6dedaeb2d7cffb2381d8e71a56cc99e7de8
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-24 13:48:23 +0200

    csere

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀Dragon Hunter: Ryuujous Rise❀')
+        .setTitle('★Shopping Carte Blanche★')
         .setURL('')
-        .setDescription('IJN Ryuujou új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/IC0NMaU4eP6Uzsp830QhGHO_C3WPPmZOwQEfWaAaMlM/https/pbs.twimg.com/media/E4lNUNrVIAEHWmW.jpg%3Alarge?width=1365&height=675')
+        .setDescription('USS Cassin új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/mfhGXwbeFR0LoADRer--5Ngs9HAfWRvtbLlVDtgc5UY/https/pbs.twimg.com/media/E4nrldwVUAwwkrl.jpg%3Alarge?width=1839&height=910')
         message.channel.send(update);
     if (message.deletable) message.delete();
     }    
--- commands/update2.js
+++ commands/update2.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀At School with Foo❀')
+        .setTitle('❀After-School Melody❀')
         .setURL('')
-        .setDescription('IJN Kasumi új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/c77rbDpuvL849u4d6hTMbcHxeWNxgRr9bd0PwoerfYs/https/pbs.twimg.com/media/E4lNunaVcAgBhg1.jpg%3Alarge?width=1365&height=675')
+        .setDescription('IJN Kazagumo új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/g_CSahZTv0kdHtxgE3GaxrMalEONt-uYdbv9jHrwAfo/https/pbs.twimg.com/media/E4onANSVcAYDN7W.jpg%3Alarge?width=1839&height=910')
         message.channel.send(update2);
     if (message.deletable) message.delete();
     }    
