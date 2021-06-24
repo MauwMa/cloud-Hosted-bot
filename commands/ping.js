abbd5111a4f3a55c36eb5294417d8a7c77036290
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-24 12:55:34 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: 'ping',
+    permissions: ["BAN_MEMBERS"],
     description: 'ping',
+    permiss
     execute (client, message, args){
 
         message.channel.send("Pong!");
