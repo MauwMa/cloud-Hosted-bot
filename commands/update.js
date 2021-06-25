eca03146538f57463e5457a7a20c63fd045306fa
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-25 20:18:53 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -3,10 +3,13 @@ module.exports = {
     description: 'update',
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args, Discord){
-
-        message.channel.send("Parancsnokok, a Swirling Cherry Blossoms event visszatért! \nSikerült már találkoznotok Noshiroval és Ryuuhouval?\nhttps://www.youtube.com/watch?v=dGadadIQo0g");
-
+        const update = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('❀Bunny Breeder❀')
+        .setURL('')
+        .setDescription('IJN Shirayuki új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/kck077-8nmxMdNoYbYLGgiySQ3IJ036N-n55LzZAbX0/https/pbs.twimg.com/media/E4t8nOwVgAINpPm.jpg%3Alarge?width=1271&height=629')
+        message.channel.send(update);
     if (message.deletable) message.delete();
-
     }    
 }
