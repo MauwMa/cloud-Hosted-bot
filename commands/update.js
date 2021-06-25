40eb87fa0df233642fc30f8e3b641548dfe419fb
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-25 11:56:48 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ehes',
     description: 'ehes',
     permissions: ["KICK_MEMBERS"],
-    execute (client, message, args){
+    execute (client, message, args, Discord){
 
         message.channel.send("Parancsnokok, a Swirling Cherry Blossoms event visszatért! Sikerült már találkoznotok Noshiroval és Ryuuhouval?\nhttps://www.youtube.com/watch?v=dGadadIQo0g");
 
