c45bf4e5ee6e71bd09076f49d2705b364c4a3ff3
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-26 23:06:16 +0200

    Update frakcio.js

--- commands/frakcio.js
+++ commands/frakcio.js
@@ -10,7 +10,7 @@ module.exports = {
         .setDescription(':purple_circle:=>Sakura | :blue_circle:=>Eagle | :green_circle:=>Sardegna | :white_circle:=>Orosz/Kína\n\n:yellow_circle:=>Royal | :orange_circle:=>Vichy/Iris | :red_circle:=>Iron | :brown_circle:=>Egyéb')
         message.channel.send(frakcio)
         .then ((msg) => {setTimeout(function(){
-            msg.react("🔵","🟣")
+            msg.react('🔵','🟣')
         }, 1000)});
 if (message.deletable) message.delete();
     }    
