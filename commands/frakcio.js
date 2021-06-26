9a7cc6d14df42b2075dcd9600b7baab55f065886
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-26 22:50:53 +0200

    Update frakcio.js

--- commands/frakcio.js
+++ commands/frakcio.js
@@ -8,7 +8,7 @@ module.exports = {
         .setTitle('Melyik frakció fleetet látnátok szíveszen fleet összeállításban Cicától nyaaa~?')
         .setURL('')
         .setDescription(':purple_circle:=>Sakura | :blue_circle:=>Eagle | :green_circle:=>Sardegna | :white_circle:=>Orosz/Kína\n\n:yellow_circle:=>Royal | :orange_circle:=>Vichy/Iris | :red_circle:=>Iron | :brown_circle:=>Egyéb')
-        message.channel.send(frakcio);
+        message.channel.send(frakcio).then(message.react(':purple_circle:'));
 
 if (message.deletable) message.delete();
     }    
