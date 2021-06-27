88a1983b0ff8e3b5fa868122718bfdfbe47b02f0
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-27 13:17:32 +0200

    Update nparancsok.js

--- commands/nparancsok.js
+++ commands/nparancsok.js
@@ -1,14 +1,14 @@
 module.exports = {
-    name: 'parancsok',
-    description: 'parancsok',
+    name: 'nparancsok',
+    description: 'nparancsok',
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args, Discord){
-        const parancsoks = new Discord.MessageEmbed()
+        const nparancsoks = new Discord.MessageEmbed()
         .setColor('#bada55')
         .setTitle('Parancsok')
         .setURL('')
         .setDescription('/ehes | /frakcio | /ham | /ham2 | /karveg | /keres | /tema | /update')
-        message.channel.send(parancsoks);
+        message.channel.send(nparancsoks);
 
         if (message.deletable) message.delete();
     }    
