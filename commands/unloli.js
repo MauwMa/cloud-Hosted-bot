2eba851ed6f874a813ef41e7e057b676fcf4d0a6
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-27 20:03:38 +0200

    Update unloli.js

--- commands/unloli.js
+++ commands/unloli.js
@@ -3,13 +3,13 @@ module.exports = {
     description: 'unloli',
     permissions: ["SEND_MESSAGES"],
     execute (client, message, args, Discord){
-        const unosloli = new Discord.MessageEmbed()
+        const unloli = new Discord.MessageEmbed()
         .setColor('#304281')
         .setTitle('Nagato már nem loli többé')
         .setURL('')
         .setDescription('')
         .setImage('https://i.redd.it/5857dj1riqf41.jpg')
-        message.channel.send(unosloli);
+        message.channel.send(unloli);
 
     }    
 }
