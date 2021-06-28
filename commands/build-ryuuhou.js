3eef62b2b36df61b64036583aacaebd109d83277
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-28 20:59:12 +0200

    cser

---
+++ commands/build-ryuuhou.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'build-ryuuhou',
+    description: 'build-ryuuhou',
+    permissions: ["SEND_MESSAGES"],
+    execute (client, message, args, Discord){
+        const ryuuhou = new Discord.MessageEmbed()
+        .setColor('#ff90ff')
+        .setTitle('Ryuuhou Build')
+        .setURL('')
+        .setDescription('Dmg/ Support(Slow)/ Heal/ budget')
+        .setImage('https://i.imgur.com/Q4ux5qf.png')
+        message.channel.send(ryuuhou);
+
+    }    
+}
--- commands/build.js
+++ commands/build.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('#304281')
         .setTitle('Ezekre készült buildek')
         .setURL('')
-        .setDescription('Alabama, Amagi, Aquila, Atago, Ayanami, Azuma, Bismarck, Friedrich, Heinrich, Helena, Howe, Ibuki, Independence, Intrepid, Jeanne dArc, Kasumi, Laffey, London, LongIsland, Mainz, Montpelier, Mutsuki, Nagato, Nakiriayame, NewJersey, Noshiro, Odin, Perseus, PrinzEugen, Ranger, Richelieu, Roon, SanDiego, SanFrancisco, Shangrila, Shinano, ShirakamiFubuki, SouthDakota , Suruga, Suzutsuki, Swiftsure, Tripitz, Z23')
+        .setDescription('Alabama, Amagi, Aquila, Atago, Ayanami, Azuma, Bismarck, Friedrich, Heinrich, Helena, Howe, Ibuki, Independence, Intrepid, Jeanne dArc, Kasumi, Laffey, London, LongIsland, Mainz, Montpelier, Mutsuki, Nagato, Nakiriayame, NewJersey, Noshiro, Odin, Perseus, PrinzEugen, Ranger, Richelieu, Roon, Ryuuhou, SanDiego, SanFrancisco, Shangrila, Shinano, ShirakamiFubuki, SouthDakota , Suruga, Suzutsuki, Swiftsure, Tripitz, Z23')
         message.channel.send(build);
 
     }    
