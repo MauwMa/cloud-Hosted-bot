541484f1ed5214fabae8f0b61e41761e7152f187
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-28 20:54:43 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,7 +5,7 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('FREEGEMS')
+        .setTitle('Emlékeztető')
         .setURL('')
         .setDescription('Ezek az iskolai kinézetű öltözékek elérhetőek 7/8 magyar időszámítászerint 9:00-ig.\nKészen állsz, hogy Memphisel és többiekkel tanulj?')
         .setImage('https://images-ext-2.discordapp.net/external/EvJUL-Qt2FSb15W2_XHnxtfwHLlPvO26IMpYk3CdgWk/https/pbs.twimg.com/media/E45WOvzVcAgY2s7.jpg%3Alarge')
