66118560c116ba05de6c9908e5e97cdb166f28c8
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-06-30 20:07:13 +0200

    shuba

---
+++ commands/build-formidable.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'build-formidable',
+    description: 'build-formidable',
+    permissions: ["SEND_MESSAGES"],
+    execute (client, message, args, Discord){
+        const formidable = new Discord.MessageEmbed()
+        .setColor('#c5c5ff')
+        .setTitle('Formidable Build')
+        .setURL('')
+        .setDescription('Boss+timing/ Boss/ Mob/ Budget')
+        .setImage('https://i.imgur.com/Gc61HYP.png')
+        message.channel.send(formidable);
+
+    }    
+}
