492bc063d94be00baa86a1b2d58575c055dc9bc9
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-03 18:20:16 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,7 +5,7 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('⛨Marco Polo⛨')
+        .setTitle('★Anchorage★')
         .setURL('')
         .setDescription('Pa... rancsnok? Nem... Tanár? Tancsi-tanár? Anchorage vagyok. Woohoo! Tanár! A kedvencem! Tanááááár! \n\nUSS Anchorage felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
         .setImage('https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/212142023_1496438234053964_34196587938986942_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=730e14&_nc_ohc=Op1tu0BGYpMAX8Ll6Hz&_nc_ht=scontent-vie1-1.xx&oh=e4b73119e416c79ca6c9e7209752d188&oe=60E4A375')
