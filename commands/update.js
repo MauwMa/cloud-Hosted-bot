7a35588bc5665c4e2d21bdef6ef537a964cc95b3
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-03 07:45:05 +0200

    dd

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('Tájékoztató')
+        .setTitle('⛨Marco Polo⛨')
         .setURL('')
-        .setDescription('Júliusi bejelentkezős bútor: Nyári Buborék Medence')
-        .setImage('https://images-ext-2.discordapp.net/external/IwsYJHo4Y0fb19i7uVruXSCN3uCMBpDwHhfnKazZeGY/https/pbs.twimg.com/media/E5SObc-VcAUGTFH.jpg%3Alarge?width=1370&height=623')
+        .setDescription('Remélem tudod mit csinálsz, amiért felébresztettél engem. Hehehe... Vitézség és kúltúra parancsnokának nézel ki. Csodálatos! Én Marco Polo vagyok. Most hát, gyere! Az első ügyrendületünk az, hogy elterjesztjük Sardegna Empire dícsőségét keresztül a világon!\n\nRN Marco Polo felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/204493521_1495816017449519_7146274071691006615_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=730e14&_nc_ohc=WdiZ47toL2oAX9eaCHK&_nc_ht=scontent-vie1-1.xx&oh=6a1ad81e32fa0c6256c988eb91a0af57&oe=60E54E83')
         message.channel.send(update);
     if (message.deletable) message.delete();
     }    
--- commands/update2.js
+++ commands/update2.js
@@ -8,13 +8,13 @@ module.exports = {
 
 
         var exportScheduler = schedule.scheduleJob('0 00 13 26 6 *', function() { 
-            const egyora = new Discord.MessageEmbed()
+            const update2 = new Discord.MessageEmbed()
             .setColor('RANDOM')
-            .setTitle('Event')
+            .setTitle('❀Hakuryuu❀')
             .setURL('')
-            .setDescription('Kazagumo, Shirayuki és a többiek, kik jelenleg építhető 7/8 magyar időszámítás szerint reggel 9:00-ig lesznek elérhetők!')
-            .setImage('https://images-ext-2.discordapp.net/external/AELr3QTzLhHxC4k6c43E_jiq7iGItTZVFjNdwG1ZUGU/https/pbs.twimg.com/media/E4t9tpaVgAUz5ex.jpg%3Alarge')
-            message.channel.send(egyora); 
+            .setDescription('Szóval ez a sik kölök lenne a parancsnok? Hakuryuu a nevem. Figyelj jól - próbálj meg irányítani, és nevetségtárgyává fogod tenni magad. Emlékezz erre! IJN Hakuryuu felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+            .setImage('https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/204119968_1495824594115328_8251861718593711801_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=730e14&_nc_ohc=SwCg8AkkLm8AX84m1sN&_nc_ht=scontent-vie1-1.xx&oh=e4360e117830f3e1859afc1d2a0823ca&oe=60E4103C')
+            message.channel.send(update2); 
         });
 
         if (message.deletable) message.delete();
