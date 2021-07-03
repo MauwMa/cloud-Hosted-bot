71225e981fbcba460dccb9bd457783b04519a73e
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-03 07:54:54 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('RANDOM')
         .setTitle('⛨Marco Polo⛨')
         .setURL('')
-        .setDescription('Remélem tudod mit csinálsz, amiért felébresztettél engem. Hehehe... Vitézség és kúltúra parancsnokának nézel ki. Csodálatos! Én Marco Polo vagyok. Most hát, gyere! Az első ügyrendületünk az, hogy elterjesztjük Sardegna Empire dícsőségét keresztül a világon!\n\nRN Marco Polo felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setDescription('Remélem tudod mit csinálsz, amiért felébresztettél engem. Hehehe... Vitézség és kúltúra parancsnokának nézel ki. Csodálatos! Én Marco Polo vagyok. Most hát, gyere! Az első ügyrendületünk Sardegna Empire dícsőségének elterjesztése az egész világon!\n\nRN Marco Polo felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
         .setImage('https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/204493521_1495816017449519_7146274071691006615_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=730e14&_nc_ohc=WdiZ47toL2oAX9eaCHK&_nc_ht=scontent-vie1-1.xx&oh=6a1ad81e32fa0c6256c988eb91a0af57&oe=60E54E83')
         message.channel.send(update);
     if (message.deletable) message.delete();
