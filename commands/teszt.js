402decc984a698c34c8ba21f3c2c473c1559ff08
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-06 08:38:50 +0200

    Update teszt.js

--- commands/teszt.js
+++ commands/teszt.js
@@ -3,13 +3,13 @@ module.exports = {
     description: 'teszt',
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args, Discord){
-        client.channels.cache.find(channel => channel.id === "853629612986990612")
         const teszt = new Discord.MessageEmbed() 
         .setColor('RANDOM')
         .setTitle('Emlékeztető')
         .setURL('')
         .setDescription('A Manjuu High School bútorzat 7/8-ig elérhető. Ne felejtsétek megvenni, ha még nincs meg.')
         .setImage('https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/207314068_1497852887245832_6482710462920299034_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=730e14&_nc_ohc=6LApO8hLdQ4AX_DoqP6&_nc_ht=scontent-vie1-1.xx&oh=2fdbadd0ef967303bdd4e4ef84294626&oe=60E7F26C')
+        client.channels.cache.find(channel => channel.id === "853629612986990612")
         message.channel.send(teszt);
     if (message.deletable) message.delete();
     }    
