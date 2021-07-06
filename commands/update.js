0da91ff44d744e5ded3e1c1fc4aa206ef7b2d923
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-06 08:23:10 +0200

    cser

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('✠August von Parseval✠')
+        .setTitle('Emlékeztető')
         .setURL('')
-        .setDescription('Végre eljöttél, hűségesem... Elnézést, én "parancsnokom". Unhulde, August von Parseval vagyok. Örvendek a találkozásnak \n\nKMS August von Parseval felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
-        .setImage('https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/207872992_1497336647297456_6572164953915743571_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=730e14&_nc_ohc=ytPLYhx67wQAX-Ai4Bc&_nc_ht=scontent-vie1-1.xx&oh=07adfc63d0a72f8079879b54a8bd5034&oe=60E72CA6')
+        .setDescription('A Manjuu High School bútorzat 7/8-ig elérhető. Ne felejtsétek megvenni, ha még nincs meg.')
+        .setImage('https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/207314068_1497852887245832_6482710462920299034_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=730e14&_nc_ohc=6LApO8hLdQ4AX_DoqP6&_nc_ht=scontent-vie1-1.xx&oh=2fdbadd0ef967303bdd4e4ef84294626&oe=60E7F26C')
         message.channel.send(update);
     if (message.deletable) message.delete();
     }    
