10f556b100e471528d3dbdfc6d76219bf60dd9c3
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-07 21:29:35 +0200

    Update egyora.js

--- commands/egyora.js
+++ commands/egyora.js
@@ -17,8 +17,5 @@ module.exports = {
             client.channels.cache.find(channel => channel.id === "789936292413571102").send(egyora); 
         });
         if (message.deletable) message.delete();
-        if (egyora){
-            return message.channel.send(`El fogom küldeni az egy órás értesítést, amikor eljött az ideje nyaaa~`);
-          }
     }    
 }
