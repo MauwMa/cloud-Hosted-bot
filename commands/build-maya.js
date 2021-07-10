3bd33554f09fc72fb624c0091bb305cba5e23e81
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-10 19:03:07 +0200

    Create build-maya.js

---
+++ commands/build-maya.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'build-maya',
+    description: 'build-maya',
+    permissions: ["SEND_MESSAGES"],
+    execute (client, message, args, Discord){
+        const maya = new Discord.MessageEmbed()
+        .setColor('#ff90ff')
+        .setTitle('Maya Build')
+        .setURL('')
+        .setDescription('Late game | Auto | Manual | Budget')
+        .setImage('https://i.imgur.com/uV1Xng3.png')
+        message.channel.send(maya);
+
+    }    
+}
