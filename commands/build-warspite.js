ab6471b060a352c5a50216f29b9fc1c582156bda
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-12 10:29:43 +0200

    s

---
+++ commands/build-warspite.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'build-warspite',
+    description: 'build-warspite',
+    permissions: ["SEND_MESSAGES"],
+    execute (client, message, args, Discord){
+        const warspite = new Discord.MessageEmbed()
+        .setColor('#c5c5ff')
+        .setTitle('Warspite Build')
+        .setURL('')
+        .setDescription('CH13 | HE | AP | Budget')
+        .setImage('https://i.imgur.com/DiC5J14.png')
+        message.channel.send(warspite);
+
+    }    
+}
