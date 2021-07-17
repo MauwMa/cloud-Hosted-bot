bef23a750c0ce126f8eb94ff5533fc3e19a3e6c1
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-17 20:26:30 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀Haruka Amami❀')
+        .setTitle('❀Chihaya Kisaragi❀')
         .setURL('')
-        .setDescription('Haruka Amami felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
-        .setImage('https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/217558418_1505998573097930_8436897669049415283_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=730e14&_nc_ohc=cfbs3ujWhiIAX_KOoPX&_nc_ht=scontent-vie1-1.xx&oh=03b309b4db79502fc4e701efca3696ca&oe=60F7981F')
+        .setDescription('Chihaya Kisaragi felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/218858097_1506670616364059_3215723211319590926_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=730e14&_nc_ohc=NWKH7ltZ2Y0AX8PTGNe&_nc_oc=AQn66guQQV3yV7Onm-Leata4GvlUqkFmntcM3SZwW-CwVTKrR0HcgU958lPY1hb3aTbP4cxXtnYjbNHSkxd0GvIs&_nc_ht=scontent-vie1-1.xx&oh=d29c77a9dab650ce95677a3b19af33f2&oe=60F8BB6A')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
