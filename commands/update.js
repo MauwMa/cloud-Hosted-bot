68d7c2d4434f5e1f9ba49a13db8139fadbd14f3d
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-18 17:43:53 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀Chihaya Kisaragi❀')
+        .setTitle('❀Ritsuko Akizukii❀')
         .setURL('')
-        .setDescription('Chihaya Kisaragi felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
-        .setImage('https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/218858097_1506670616364059_3215723211319590926_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=730e14&_nc_ohc=NWKH7ltZ2Y0AX8PTGNe&_nc_oc=AQn66guQQV3yV7Onm-Leata4GvlUqkFmntcM3SZwW-CwVTKrR0HcgU958lPY1hb3aTbP4cxXtnYjbNHSkxd0GvIs&_nc_ht=scontent-vie1-1.xx&oh=d29c77a9dab650ce95677a3b19af33f2&oe=60F8BB6A')
+        .setDescription('Ritsuko Akizuki felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/D8YRrSTQnIb4ZMrYAjUoSStCiR_Uo7RxMtXV8nSUGgs/https/pbs.twimg.com/media/E6e-CCwVEAIm1vx.jpg%3Alarge?width=1300&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
