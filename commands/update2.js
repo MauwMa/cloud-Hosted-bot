86bed6aa2a33157714a1dc104881ec741a763def
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-19 14:52:22 +0200

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -1,16 +1,16 @@
 module.exports = {
-    name: 'update',
-    description: 'update',
+    name: 'update2',
+    description: 'update2',
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args, Discord){
-        const update = new Discord.MessageEmbed()
+        const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀Azusa Miura❀')
+        .setTitle('❀Ami Futami❀')
         .setURL('')
-        .setDescription('Azusa Miura felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/W0rpRP4-AoCxwiCqxwQ8Cq2cIlbVfuvBFcM9ch0QWyY/https/pbs.twimg.com/media/E6k9h6OVUAELe5k.jpg%3Alarge?width=1090&height=567')
-        client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
-        if (update){
+        .setDescription('Ami Futami felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/_M3zF8XoI_WbXbkTz_QjtS4Mvgxtz-OdcWpeRu6MxZU/https/pbs.twimg.com/media/E6k940PUcAccrJz.jpg%3Alarge?width=1090&height=567')
+        client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
+        if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
           }
     }    
