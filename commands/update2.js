acb4c16782ad35023adb6cae46fe5ad44b2bc2bb
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-20 14:59:29 +0200

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀Mennyei Virágok Tükörképei❀')
+        .setTitle('❀A Pici Éjszakai Titok❀')
         .setURL('')
-        .setDescription('Chihaya Kisaragi új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/DUuCor1DiCTo97Zy5F2ZcPXaS9VR7TW-TuRXM2CUpXk/https/pbs.twimg.com/media/E6p1QD3VoAAWFVR.jpg%3Alarge?width=1090&height=567')
+        .setDescription('Iori Minase új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/MeBobPEXGUH6xbnfEcF9S_z9eMXhHyj6QnGy-ZFxyto/https/pbs.twimg.com/media/E6p4cuUVIAEVrE5.jpg%3Alarge?width=1090&height=567')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
