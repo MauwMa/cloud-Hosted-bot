01c5cd2462d9f7110e9981b68ee67a10fd936b22
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-20 20:09:09 +0200

    Create update3.js

---
+++ commands/update3.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'update2',
+    description: 'update2',
+    permissions: ["KICK_MEMBERS"],
+    execute (client, message, args, Discord){
+        const update2 = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('')
+        .setURL('https://youtu.be/MBe9uuFpCDE')
+        .setDescription('★Azur Lane x THE IDOLM@STER Kollaborálás Animációja PV★\n\nÚj kollaborálás fő event, Azur Himnusz, Július 22.-én fog kezdődni, amely az IDOLM@ASTER karaktereit fogja bemutatni!')
+        .setImage('')
+          }
+    }    
