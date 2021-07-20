ab98e0478325340bafdd46116aecae0fe11b74fa
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-20 20:43:24 +0200

    Create update4.js

---
+++ commands/update4.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'ehes',
+    description: 'ehes',
+    permissions: ["KICK_MEMBERS"],
+    execute (client, message, args){
+
+        message.channel.send("https://youtu.be/MBe9uuFpCDE");
+
+    if (message.deletable) message.delete();
+
+    }    
+}
