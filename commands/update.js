b63e87adf32c76d407899d63b0a1d51c2b7e4432
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-22 08:49:46 +0200

    ds

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀Nyári Virágok és Perzselő Ütések❀')
+        .setTitle('❀Előkelő Tüzijáték❀')
         .setURL('')
-        .setDescription('Ami Futami új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/IKi7VxVCmOvvAPiZFFLKUkjz-5by4PijWT_069JLDec/https/pbs.twimg.com/media/E6vXaFZVUAMTXvM.jpg%3Alarge?width=1090&height=567')
+        .setDescription('Arashio új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/DMt2sqj10GI9SsC7uEQRC95FUHn7Cplv3Y_00RVt0Ik/https/pbs.twimg.com/media/E61CaLJVcAYbyNI.jpg%3Alarge?width=1146&height=567')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
--- commands/update2.js
+++ commands/update2.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀Csábító Feszitvál❀')
+        .setTitle('❀Ütős Leszámolás - Nyári Formáció❀')
         .setURL('')
-        .setDescription('Mami Futami új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/AG5ll55w1AdXJ4aypwRAu-2NFWBRTe74GymehIgN12A/https/pbs.twimg.com/media/E6vX7DuVoAAeez0.jpg%3Alarge?width=1090&height=567')
+        .setDescription('Ritsuko Akizuki új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/YbSo3PVjl_qBKQIqjZWrtJHlKhhnzqan6L_LRCs5hZg/https/pbs.twimg.com/media/E61CvLWUUAQtjEV.jpg%3Alarge?width=1090&height=567')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
