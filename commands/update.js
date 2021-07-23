9c82421c55819b7c8dcdbab2b5d927f3b4bc9c00
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-23 19:33:57 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('')
+        .setTitle('Rajzverseny')
         .setURL('')
-        .setDescription('Parancsnokok,\n\n A 765 Produkció bútorzatok elérhetőek egészen 8/5 09:00-ig. Ragadjátok meg ezt a bútorzat szettet, hogy megtapasztáljátok a csodálatos irodai környezetet.')
-        .setImage('https://images-ext-2.discordapp.net/external/SrzZKxKnktVH8CNu_EKH7Mk6zhG4iQVFEVdi5Pc1ums/https/pbs.twimg.com/media/E69wxpaUUAMHuwN.jpg%3Alarge?width=958&height=567')
+        .setDescription('Parancsnokok,\n\n A Szavazási Idő a 3. Évforduló rajzversenyére megkezdődött! Látogass el ide: https://t.co/giJxRPcKm2 hogy voksolj a kedvenc rajzodra!')
+        .setImage('https://images-ext-1.discordapp.net/external/NDWrmQ_HlA3_Z2NR4_4oDx53h0kF2UuW52712l1tNqA/https/pbs.twimg.com/media/E6-wJJWVgAMmtGi.jpg%3Alarge?width=567&height=567')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
