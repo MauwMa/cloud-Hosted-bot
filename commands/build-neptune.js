dd5a080e1be44a28cc77ba419510331846ff2b24
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-07-28 12:33:44 +0200

    d

---
+++ commands/build-neptune.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'build-neptune',
+    description: 'build-neptune',
+    permissions: ["SEND_MESSAGES"],
+    execute (client, message, args, Discord){
+        const neptune = new Discord.MessageEmbed()
+        .setColor('#c5c5ff')
+        .setTitle('Neptune Build')
+        .setURL('')
+        .setDescription('AA | DPS | Survival | Budget')
+        .setImage('https://i.imgur.com/djyJVR0.png')
+        message.channel.send(neptune);
+
+    }    
+}
