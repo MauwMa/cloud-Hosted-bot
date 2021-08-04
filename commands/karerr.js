4e6d1f1c678b28c47b6c104dba0644a524ebbddc
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-04 07:42:53 +0200

    Update karerr.js

--- commands/karerr.js
+++ commands/karerr.js
@@ -1,16 +1,16 @@
 module.exports = {
-    name: 'karban',
+    name: 'karerr',
     permissions: ["KICK_MEMBERS"],
-    description: 'karban',
+    description: 'karerr',
     execute (client, message, args, Discord){
-        const karban = new Discord.MessageEmbed()
+        const karerr = new Discord.MessageEmbed()
         .setColor('#ffff00')
         .setTitle('Karbantartás!!')
         .setURL('')
         .setDescription('A főhadiszállás egy váratlan problémába ütközött a karbantartás miatt és emiatt 2 órával meghosszabbítjuk a karbantartást. Bármilyen változás fog történni azt meg fogjuk osztani.\n\nKöszönjük a megértéseteket!')
         .setImage('https://i.imgur.com/0BEHNji.png')
-        client.channels.cache.find(channel => channel.id === "789936292413571102").send(karban);
-        if (karban){
+        client.channels.cache.find(channel => channel.id === "789936292413571102").send(karerr);
+        if (karerr){
             return message.channel.send(`Elküldtem a karbantartás értesítést nyaaa~`);
           }
     }    
