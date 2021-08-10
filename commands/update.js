55dd726dd2396aa04e0d9413ab7c050ab801c354
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-10 18:24:18 +0200

    2darb

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('♚Mischief on the Rocks♚')
+        .setTitle('')
         .setURL('')
-        .setDescription('HMS Abercrombie Ritsuko Akizuki új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/4rLgGkIuG3UmulbDe2AOXEpY05YtU9UzihSdQY0ClXA/https/pbs.twimg.com/media/E8UR6y7UYAQOsoQ.jpg%3Alarge?width=1026&height=560')
+        .setDescription('Elérhető Androidra egy nem kötelező frissítés és bug javítás, amitől az értesítések miatt összeomlött a játék. Emellett egy hotfix, amely kikapcsolja az értesítési rendszert azoknak, akiknek problémát okoz. Főhadiszállás elnézést kér és kompenzációként 1.500 <:pnz:790201580812894218> 1.500 <:olaj:790201521593122816> 5 <:kocka:790201634100740117> lett küldve mindenkinek.')
+        .setImage('')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
--- commands/update2.js
+++ commands/update2.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('RANDOM')
         .setTitle('')
         .setURL('')
-        .setDescription('Parancsnokok,\n\nA 3. évforduló megünepléseképp, az első <:gems:790201608855486513> vásárlás duplázási bónusza reszetelve lesz Augusztus 12.-én.')
+        .setDescription('Parancsnokok,\n\nA 3. évforduló megünepléseképp, az első <:gems:790201608855486513> vásárlás duplázási bónusza reszetelve lesz Augusztus 12.-én. ')
         .setImage('https://images-ext-1.discordapp.net/external/fMI3LDZ3m65PsDUbE_efpfzgB4P8nZculhDxbv8ib58/https/pbs.twimg.com/media/E8WyTTbUYAg3hpu.png%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
