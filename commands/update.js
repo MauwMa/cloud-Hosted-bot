5c8106a5e33275157a72e46a5c32dd4f8cd1d5d6
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-10 11:08:45 +0200

    d

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('')
+        .setTitle('3. évforduló Élő Közvetítés')
         .setURL('')
-        .setDescription('Parancsnokok,\n\n A közelgő karbantartás kötelező frissítés lesz az Azur Lanehez. Kérünk mindenki bizonyosodjon meg róla, hogy le van kötve a felhasználótók az elvesztése elkerüléséért. Minden parancsnokot 200<:gems:790201608855486513> fogunk kompenzálni. ')
-        .setImage('https://images-ext-1.discordapp.net/external/XHfJ2m6qEUXGFerh8-BWt_-aVqRSiUUj1jSdrKzqUHM/https/pbs.twimg.com/media/E7x7jS9VEAI2NXF.jpg%3Alarge')
+        .setDescription('Parancsnokok, Dunkerque és Haruna szinkronszínésze, Harada Sayaka (@sayakaharada) egy különleges üzenettel készült nektek~ A 3. évforduló közvetítés Augusztus 11. 5:00 magyar időszámítás szerint lesz YouTube-on!\n\n #AzurLane #Yostar #AzurLane3rdAnniv https://t.co/OceHuhlXHu')
+        .setImage('https://images-ext-2.discordapp.net/external/snnaRZxqQW8DRQnpFDafaxoZmfeVZRh97hpwmMw1z5I/https/pbs.twimg.com/ext_tw_video_thumb/1424350466046005251/pu/img/AzC-Djtl9p8CrR9i.jpg?width=996&height=560')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
