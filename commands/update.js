5480ef2059b55f71c848679f01bc9270b479a706
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-10 11:14:03 +0200

    dd

--- commands/update.js
+++ commands/update.js
@@ -7,8 +7,8 @@ module.exports = {
         .setColor('RANDOM')
         .setTitle('3. évforduló Élő Közvetítés')
         .setURL('')
-        .setDescription('Parancsnokok, Dunkerque és Haruna szinkronszínésze, Harada Sayaka (@sayakaharada) egy különleges üzenettel készült nektek~ A 3. évforduló közvetítés Augusztus 11. 5:00 magyar időszámítás szerint lesz YouTube-on!\n\n #AzurLane #Yostar #AzurLane3rdAnniv https://t.co/OceHuhlXHu')
-        .setImage('https://images-ext-2.discordapp.net/external/snnaRZxqQW8DRQnpFDafaxoZmfeVZRh97hpwmMw1z5I/https/pbs.twimg.com/ext_tw_video_thumb/1424350466046005251/pu/img/AzC-Djtl9p8CrR9i.jpg?width=996&height=560')
+        .setDescription('Parancsnokok, Enterprise szinkronszínésze, Ishikawa Yui (@YUI_STAFF) egy különleges üzenettel készült nektek~ A 3. évforduló közvetítés Augusztus 11. 5:00 magyar időszámítás szerint lesz YouTube-on!\n\n Élő Közvetítés: https://t.co/3yI8pymSTA\n\n #AzurLane #Yostar #AzurLane3rdAnniv https://t.co/69rNb5L7ci')
+        .setImage('https://pbs.twimg.com/ext_tw_video_thumb/1424772663856144388/pu/img/rSqbhwB0nMk4yOkr.jpg')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
--- commands/update2.js
+++ commands/update2.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('Memória Kinuhoz')
+        .setTitle('')
         .setURL('')
-        .setDescription('Parancsnokok,\n\nA karbantartás után új memória lesz elérhető Kinuhoz.')
-        .setImage('https://images-ext-2.discordapp.net/external/EeWMvXG3WglKKsZhXmUCl-wQIXbk4RXm3NmnIvPxvTI/https/pbs.twimg.com/media/E73ooiyWEAUC6vm.png%3Alarge')
+        .setDescription('Parancsnokok,\n\nA 3. évforduló megünepléseképp, az első <:gems:790201608855486513> vásárlás duplázási bónusza reszetelve lesz Augusztus 12.-én.')
+        .setImage('https://images-ext-1.discordapp.net/external/fMI3LDZ3m65PsDUbE_efpfzgB4P8nZculhDxbv8ib58/https/pbs.twimg.com/media/E8WyTTbUYAg3hpu.png%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
