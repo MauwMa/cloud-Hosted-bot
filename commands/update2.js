8dc502b159217929d0270ceb8902b4efb663c5fa
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-11 10:12:37 +0200

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('')
+        .setTitle('♚Ark Royal (Retrofit)♚')
         .setURL('')
-        .setDescription('Parancsnokok,\n\n Posztolj meg egy 10 másodperces vagy hosszabb videót az Azur Lane kívánságaidról vagy a játék pillanatodról Twitteren vagy Facebookon a #AzurLane3rdAnniv hashtaggel ide (https://t.co/46UryzQ5Qo) és 200<:gems:790201608855486513> kapsz jutalomként.')
-        .setImage('https://images-ext-2.discordapp.net/external/UU6eFeGfxdLNNk3JshbpxvaiSFo_5ptCULgdA4hjyNw/https/pbs.twimg.com/media/E8bxW1kWQBEUMkl.jpg%3Alarge?width=1240&height=676')
+        .setDescription('Jelenleg Ark Royal a kikötőben van fejlesztés miatt. Hamarosan vízre lesz bocsájtva nyaaa~')
+        .setImage('https://images-ext-2.discordapp.net/external/zc97PiZv3gbhza3ykX70IjMaBAxYvl6QcTLZtjdLZkg/https/pbs.twimg.com/media/E8fCUjaVEAcbLx-.jpg%3Alarge?width=546&height=910')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
