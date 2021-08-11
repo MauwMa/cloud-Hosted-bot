7d8eb8425b7d8e8ed48f71e9ee9731330181e3a7
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-11 10:20:26 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('★A Szív Vágya★')
+        .setTitle('★Reno Bunnino★')
         .setURL('')
-        .setDescription('USS North Carolina az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog  várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/iLXz412Dm02eaXpEprZ1178mm0Q5tICmeu09b0GEjbU/https/pbs.twimg.com/media/E8erdWnVIAAsDYy.jpg%3Alarge')
+        .setDescription('USS Reno az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog  várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/W5hd5fsSxbmRd5xAC0WkPO9ycxsd-mSYXNrY7SZsO2M/https/pbs.twimg.com/media/E8fNky0VUAIjdGf.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
