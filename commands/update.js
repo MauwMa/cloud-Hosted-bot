eddcf0789f2c98271080e3aebd6f2af7685dd310
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-11 10:15:36 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -8,7 +8,7 @@ module.exports = {
         .setTitle('★A Szív Vágya★')
         .setURL('')
         .setDescription('USS North Carolina az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog  várni, Parancsnok.')
-        .setImage('')
+        .setImage('https://images-ext-1.discordapp.net/external/iLXz412Dm02eaXpEprZ1178mm0Q5tICmeu09b0GEjbU/https/pbs.twimg.com/media/E8erdWnVIAAsDYy.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
