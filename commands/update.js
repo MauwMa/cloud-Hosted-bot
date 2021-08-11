a5b122d55ee6de9e94a90eb5730d41363fe66b15
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-11 17:24:16 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('★Reno Bunnino★')
+        .setTitle('❀Sengoku☆Smasher!❀')
         .setURL('')
-        .setDescription('USS Reno az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog  várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/W5hd5fsSxbmRd5xAC0WkPO9ycxsd-mSYXNrY7SZsO2M/https/pbs.twimg.com/media/E8fNky0VUAIjdGf.jpg%3Alarge')
+        .setDescription('IJN Isokaze új ruhájába öltözik. A közeljövőben a dokkodban fog  várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/4u1evU3rO3elhk0eYwV6Rc6WT7lI6DFKpRNjVmMqqqE/https/pbs.twimg.com/media/E8gu8y8VUAEMj3x.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
