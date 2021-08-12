d7fd73a30ecdbf2ae9feb5be6536efd57aa59ba2
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-12 18:43:28 +0200

    dddd

--- commands/update.js
+++ commands/update.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('RANDOM')
         .setTitle('')
         .setURL('')
-        .setDescription('Főhadiszállás tisztában van a hálózati problémával, amit mindenki tapasztal. Jelenleg a probléma okát kutatjuk, köszönjük a türelmeteket és támogatásotokat.')
+        .setDescription('Parancsnokok\n\nA hálózati hiba, amit a instabil hálózat okozott meg lett oldva. A főhadiszállás őszintén bocsánatot kér a probléma miatt. Kompenzálásként a főhadiszállás minden parancsnoknak 100<:gems:790201608855486513> küld. Köszönjük a folyamatos támogatásaitokat.')
         .setImage('https://images-ext-1.discordapp.net/external/2fddijLmSP-q8Ze2ZSeE9ruK8JUbXxmKGx4vBTLe-Us/https/pbs.twimg.com/media/E8mK7OiUYAIFyMv.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
