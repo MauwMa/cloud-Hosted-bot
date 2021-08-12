69bc8c4754ec3de4daadd69c5ce04e052b848414
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-12 16:25:00 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('♚Ardent Mester?♚')
+        .setTitle('')
         .setURL('')
-        .setDescription('HMS Ardent új ruhájába öltözik. A közeljövőben a dokkodban fog  várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/hwcmxC3vkl6clvK27grVfAYVqAKhk4LXIDBNaGb0tDI/https/pbs.twimg.com/media/E8gwuEsVcAgq8Vv.jpg%3Alarge?width=1240&height=676')
+        .setDescription('Főhadiszállás tisztában van a hálózati problémával, amit mindenki tapasztal. Jelenleg a probléma okát kutatjuk, köszönjük a türelmeteket és támogatásotokat.')
+        .setImage('https://images-ext-1.discordapp.net/external/2fddijLmSP-q8Ze2ZSeE9ruK8JUbXxmKGx4vBTLe-Us/https/pbs.twimg.com/media/E8mK7OiUYAIFyMv.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
