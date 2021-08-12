5ff06864da38109d4603df552dac4d1efbc101ba
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-12 14:57:07 +0200

    dsa

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀Sengoku☆Smasher!❀')
+        .setTitle('♚Ardent Mester?♚')
         .setURL('')
-        .setDescription('IJN Isokaze új ruhájába öltözik. A közeljövőben a dokkodban fog  várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/4u1evU3rO3elhk0eYwV6Rc6WT7lI6DFKpRNjVmMqqqE/https/pbs.twimg.com/media/E8gu8y8VUAEMj3x.jpg%3Alarge')
+        .setDescription('HMS Ardent új ruhájába öltözik. A közeljövőben a dokkodban fog  várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/hwcmxC3vkl6clvK27grVfAYVqAKhk4LXIDBNaGb0tDI/https/pbs.twimg.com/media/E8gwuEsVcAgq8Vv.jpg%3Alarge?width=1240&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
--- commands/update2.js
+++ commands/update2.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('♚Ark Royal (Retrofit)♚')
+        .setTitle('')
         .setURL('')
-        .setDescription('Jelenleg Ark Royal a kikötőben van fejlesztés miatt. Hamarosan vízre lesz bocsájtva nyaaa~')
-        .setImage('https://images-ext-2.discordapp.net/external/zc97PiZv3gbhza3ykX70IjMaBAxYvl6QcTLZtjdLZkg/https/pbs.twimg.com/media/E8fCUjaVEAcbLx-.jpg%3Alarge?width=546&height=910')
+        .setDescription('Az új zeneszám Takuya Ohata készítésével "Standing By You" elérhető mindenhol! Nagy köszönet Naganawa Maria, Fuchigami Mai, Abe Rika és Kakuma Ainak az előadásáért.\n\nNézzétek meg: https://t.co/l0F7Q1iKK8')
+        .setImage('https://images-ext-1.discordapp.net/external/qUMcXbZmEYQX242bYBu8l__OOgBjkdBXksCAuL-KEzw/https/pbs.twimg.com/media/E8gxpsmVkAsNdDq.jpg%3Alarge?width=676&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
