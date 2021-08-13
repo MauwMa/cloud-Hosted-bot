8f86a0681d728d7966b6ab15a1246415cf40ade2
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-13 11:29:42 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('RANDOM')
         .setTitle('')
         .setURL('')
-        .setDescription('3 Nap van hátra még a 3. Évfordulóhoz, Tashkent, Chang Chun és Surcouf már várja az évfordulót~')
+        .setDescription('3 Nap van hátra még a 3. Évfordulóhoz. Tashkent, Chang Chun és Surcouf már várja az évfordulót~')
         .setImage('https://images-ext-2.discordapp.net/external/31jbrC70Ocz6DvHB96Ey87SL3jroP6fPmclkY8Zrsdk/https/pbs.twimg.com/media/E8pSbkOVIAASVJR.jpg%3Alarge?width=603&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
