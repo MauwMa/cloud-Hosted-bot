e30ccd051808d7cc33767d795c4c074117113fb7
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-14 09:18:48 +0200

    dd

--- commands/tiszta.js
+++ commands/tiszta.js
@@ -1,13 +1,13 @@
 module.exports = {
-    name: 'tiszt',
-    description: 'tiszt',
+    name: 'kuka',
+    description: 'kuka',
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args){
 
     if (isNaN(args[0]) || parseInt(args[0]) <= 0) { return message.reply('Valós számot írj be!') }
 
     if (parseInt(args[0]) > 300) {
-        return message.reply('You can only delete 300 messages at a time!')
+        return message.reply('Maximum 300 üzenetet tudsz törölni')
     } else {
         deleteAmount = parseInt(args[0]);
     }
