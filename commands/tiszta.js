b5ed07ec6b17b9f037898cb4a0669dabaf69fdd0
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-14 09:17:35 +0200

    Update tiszta.js

--- commands/tiszta.js
+++ commands/tiszta.js
@@ -4,15 +4,15 @@ module.exports = {
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args){
 
-    if (isNaN(args[0]) || parseInt(args[0]) <= 0) { return message.reply('Please put a number only!') }
+    if (isNaN(args[0]) || parseInt(args[0]) <= 0) { return message.reply('Valós számot írj be!') }
 
-    if (parseInt(args[0]) > 100) {
-        return message.reply('You can only delete 100 messages at a time!')
+    if (parseInt(args[0]) > 300) {
+        return message.reply('You can only delete 300 messages at a time!')
     } else {
         deleteAmount = parseInt(args[0]);
     }
 
     message.channel.bulkDelete(deleteAmount + 1, true);
-    message.reply(`**Successfully** Deleted ***${deleteAmount}*** Messages.`)
+    message.reply(`Sikeresen Törölt ***${deleteAmount}*** Üzetetet nyaaa~`)
     }    
 }
