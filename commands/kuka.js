f5fd80f4dccde2a1a190d7a17c0c6693e9a295b9
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-14 09:07:56 +0200

    Update kuka.js

--- commands/kuka.js
+++ commands/kuka.js
@@ -3,11 +3,11 @@ module.exports = {
     description: "kuka",
     permissions: ["KICK_MEMBERS"],
     async execute(message, args, client, Discord){
-        if(!args[0]) return message.channel.send("írd be azt a számot, ahány üzenetet akarsz törölni!");
-        if(isNaN(args[0])) return message.channel.send("kérlek írj be egy egész számot!");
+       // if(!args[0]) return message.channel.send("írd be azt a számot, ahány üzenetet akarsz törölni!");
+       // if(isNaN(args[0])) return message.channel.send("kérlek írj be egy egész számot!");
 
-        if(args[0] > 300) return message.channel.send("Nem tudsz 300-nál több üzenetet törölni!");
-        if(args[0] < 1) return message.channel.send("Legalább egy üzenetet törölnöd kell!");
+       // if(args[0] > 300) return message.channel.send("Nem tudsz 300-nál több üzenetet törölni!");
+       // if(args[0] < 1) return message.channel.send("Legalább egy üzenetet törölnöd kell!");
 
         await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
             message.channel.bulkDelete(messages);
