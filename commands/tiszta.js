71e2f5f795b79627a56b41fdb9aec81d7e2bab94
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-14 09:12:52 +0200

    Update tiszta.js

--- commands/tiszta.js
+++ commands/tiszta.js
@@ -4,13 +4,6 @@ module.exports = {
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args){
 
-    const messageArray = message.content.split(' ');
-	const args = messageArray.slice(1);
-
-    if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send('Lack of Perms!');
-    
-    let deleteAmount;
-
     if (isNaN(args[0]) || parseInt(args[0]) <= 0) { return message.reply('Please put a number only!') }
 
     if (parseInt(args[0]) > 100) {
