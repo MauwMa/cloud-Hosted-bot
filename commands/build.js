c0b26dae6b37f00c608dadee013aa51b0ea8dfa5
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-14 09:22:15 +0200

    Update build.js

--- commands/build.js
+++ commands/build.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('#304281')
         .setTitle('Ezekre készült buildek')
         .setURL('')
-        .setDescription('Akashi, Alabama, Aquila, Atago, Ayanami, Azuma, Bismarck, Formidable, Friedrich, Heinrich, Helena, Hipper, Hood, Howe, Ibuki, Independence, Intrepid, Jeanne dArc, Kashino, Kasumi, Kent, Kinggeorge, Laffey, London, LongIsland, Mainz, Maya, Montpelier, Mutsuki, Nagato, Nakiriayame, Neptune, NewJersey, Noshiro, Nurnberg, Odin, Ookamimio, Perseus, PrinzEugen, Ranger, Richelieu, Roon, Ryuuhou, Saintlousis, SanDiego, SanFrancisco, Scharnhorst, Seattle, Shangrila, Shinano, ShirakamiFubuki, Souryuu, SouthDakota, Sovetskaya, Suruga, Suzutsuki, Swiftsure, Tashkent, Tripitz, Tosa, Warspite, Yukikaze, Z23')
+        .setDescription('Akashi, Alabama, Aquila, Atago, Ayanami, Azuma, Bismarck, Formidable, Friedrich, Heinrich, Helena, Hipper, Hood, Howe, Ibuki, Independence, Intrepid, Jeanne dArc, Kashino, Kasumi, Kent, Kinggeorge, Laffey, London, LongIsland, Mainz, Maya, Montpelier, Mutsuki, Nagato, Nakiriayame, Neptune, NewJersey, Noshiro, Nurnberg, Odin, Ookamimio, Perseus, PrinzEugen, Ranger, Richelieu, Roon, Ryuuhou, Saintlousis, SanDiego, SanFrancisco, Scharnhorst, Seattle, Shangrila, Shinano, ShirakamiFubuki, Souryuu, SouthDakota, Sovetskaya, Suruga, Suzutsuki, Swiftsure, Tashkent, Tirpitz, Tosa, Warspite, Yukikaze, Z23')
         message.channel.send(build);
 
     }    
