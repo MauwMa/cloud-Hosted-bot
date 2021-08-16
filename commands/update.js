0a1363182ae8dbe936a5417d9d75f1a27f7cc416
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-16 20:44:19 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('★Nautilus★')
+        .setTitle('')
         .setURL('')
-        .setDescription('2 Nap van hátra még a 3. Évfordulóhoz. Neptune, Gascogne és Prinz Eugen már gyülekezik~')
-        .setImage('https://images-ext-2.discordapp.net/external/P55yOBaWVK5pmBkbks1HRsD0M_E30J3YrgKaRFh2Qa8/https/pbs.twimg.com/media/E8vEEQgVcAAQWkW.jpg%3Alarge?width=604&height=676')
+        .setDescription('Parancsnokok, Boldog 3. Évfordulót!\n\nFőhadiszállás köszöni a támogatást és hogy velünk tartottatok az elmúlt 3 évben.\nBontsuk ki vitorlánkat a következő évfordulónkhoz!')
+        .setImage('https://pbs.twimg.com/media/E86dXBcVEAQpIif.jpg:large')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
