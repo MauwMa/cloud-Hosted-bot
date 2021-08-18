d9d14da1f97ed4daaec60478d3831ab1b17459d0
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-08-18 15:51:03 +0200

    Update update3.js

--- commands/update3.js
+++ commands/update3.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('RANDOM')
         .setTitle('')
         .setURL('')
-        .setDescription('Limitált bútorzatok amellyek 5/27 előtt, kivéve a kolab szettek és új szettek amelyek 5/27 után voltak elérhetők, limitált idejig újra elérhetőek lesznek.')
+        .setDescription('Limitált bútorzatok amelyek 5/27 előtt, kivéve a kolab szettek és új szettek amelyek 5/27 után voltak elérhetők, limitált ideig újra elérhetőek lesznek.')
         .setImage('https://images-ext-1.discordapp.net/external/qL3049K9Xw_LnUzexVXgCxrIdAYWqmalgiRp9eCi8C8/https/pbs.twimg.com/media/E8_1z93XMBEX70_.png%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update3);
         if (update3){
