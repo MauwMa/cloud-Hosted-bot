3005d50da2d98608dc716ab9e72a42a9aa3cd51b
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-08 15:14:15 +0200

    d

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('')
+        .setTitle('【Call to Arms: Sakura Empire】')
         .setURL('')
-        .setDescription('Szeptemberi Becsekkoló bútorzat: Ugráló labda')
-        .setImage('https://images-ext-2.discordapp.net/external/PJ_7GpBHTYK8UQ1tv6VFh2q8luvemBomfW-87J5jhK0/https/pbs.twimg.com/media/E-Ggci2VIAY2Ahz.jpg%3Alarge?width=1440&height=655')
+        .setDescription('Call to Arms: Sakura Empire, egy bevezető event, ami a következő karbantartás után lesz elérhető. Gyűjts extra tapasztalatot Nagato, Jintsuu, Inazuma or Ikazuchivel, hogy felkészülj a főeventre.')
+        .setImage('https://images-ext-2.discordapp.net/external/M2xIu38oedDQOva5HEYVHekK8sjh8P14xejOhQcmpJQ/https/pbs.twimg.com/media/E-rg-CtVIAI07Cr.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
