5a576f8ca7b78eea093d6ca651cc5c87d8b611dc
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-12 21:17:47 +0200

    Update teszt.js

--- commands/teszt.js
+++ commands/teszt.js
@@ -1,17 +1,12 @@
 module.exports = {
     name: 'teszt',
-    description: 'teszt',
     permissions: ["KICK_MEMBERS"],
-    execute (client, message, args, Discord){
-        const teszt = new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .setTitle('teszt')
-        .setURL('')
-        .setDescription('Parancsnokok,\n\n A közelgő karbantartás kötelező frissítés lesz az Azur Lanehez. Kérünk mindenkit bizonyosodjon meg róla, hogy le van kötve a felhasználótók az elvesztése elkerülésére. Minden parancsnokot 200<:gems:790201608855486513> fogunk kompenzálni')
-        .setImage('')
-        client.channels.cache.find(channel => channel.id === "791956226521956394").send(teszt);
+    description: 'teszt',
+    execute (client, message, args){
+        const teszt = client.channels.cache.find(channel => channel.id === "886583291464003635");
+        teszt.send("Parancsnokok, véget ért a évforduló ünneplése. A szoba le lett zárva, de az event megkezdéséig lehet még nézelődni benne nyaaa~");
         if (teszt){
-            return message.channel.send(`A teszt üzenet ki lett küldve nyaaa~`);
+            return message.channel.send(`Informáltam a többieket a karbantartás végéről nyaaa~`);
           }
 
     }    
