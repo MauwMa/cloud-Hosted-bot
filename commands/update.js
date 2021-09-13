98a58b1aad4fea85403ffe07bd5c2c3c6d710089
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-13 16:58:26 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('RANDOM')
         .setTitle('❀Katsuragi❀')
         .setURL('')
-        .setDescription('Hellóka, Parancsnok! Unryuu-osztájú repülőgép hordozó  vagyok, Katsuragi. Nem csak hordozó vagyok, hanem teljes értékű repülőgép hordozó, ami azt jelenti, hogy mindent tudok csinálni, amire egy repülőgép hordozó képes megtenni! Nem mondom el miket nem szeretek, viszont rengeteg hobbim van~ Például~ (1 percel később) ...Szóval jah! Héj, Parancsnok, még mindig figyelsz?\n\nIJN Katsuragi felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setDescription('Hellóka, Parancsnok! Unryuu-osztályú repülőgép hordozó vagyok, Katsuragi. Nem csak hordozó vagyok, hanem teljes értékű repülőgép hordozó, ami azt jelenti, hogy mindent tudok csinálni, amire egy repülőgép hordozó képes megtenni! Nem mondom el miket nem szeretek, viszont rengeteg hobbim van~ Például~ (1 perccel később) ...Szóval jah! Héj, Parancsnok, még mindig figyelsz?\n\nIJN Katsuragi felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
         .setImage('https://images-ext-1.discordapp.net/external/Zgp-YM2OnAOKpgtHqNOLLsIVvZ-AI8m0Xtga-DUMEl4/https/pbs.twimg.com/media/E_KEzsdVgAksCJ8.jpg%3Alarge?width=1032&height=540')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
