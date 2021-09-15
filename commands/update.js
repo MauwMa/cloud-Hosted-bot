5d56f6426b2007a02d8d1986cb076367d79433ce
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-15 10:04:13 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀Succubusok vagyunk?❀')
+        .setTitle('❀És most a Varázs trükk❀')
         .setURL('')
-        .setDescription('Öhm... Ez a ruha kicsit túl sok... Parancsnok, Én... nem nézek helyzetnek felelőtlenül, ugye...?\n\nIJN Yura új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/ig01JECEbmjH-hER0p_4AGmTnBOa00eblXUCTJ8iB5s/https/pbs.twimg.com/media/E_PlJOsWQAM8Qxc.jpg%3Alarge?width=1232&height=672')
+        .setDescription('Voalá! Ne menj sehova, Parancsnok! A varázs műsor másodpercen belül kezdődik! Kezdésnek halljunk egy üvöltést! Rooaaar!\n\nIJN Yamakaze új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://i.imgur.com/OXprMyD.jpg')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
