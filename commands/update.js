412c11603c6952e65f573826a24df25ae5bbd5a2
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-15 16:04:15 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀És most a Varázs trükk❀')
+        .setTitle('♚Addled kísérő ♚')
         .setURL('')
-        .setDescription('Voalá! Ne menj sehova, Parancsnok! A varázs műsor másodpercen belül kezdődik! Kezdésnek halljunk egy üvöltést! Rooaaar!\n\nIJN Yamakaze új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://i.imgur.com/OXprMyD.jpg')
+        .setDescription('Óh... Üdvözöllek, Parancsnok. Felvehetem a rendelésed...? Vagy talán csak hátra akarsz dőlni és pihenni? ...Bárhogy is legyen felőlem okés.\n\nHMS Black Prince új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/50FuYdCPouvOwPIHuufMUvI05rrzXdHEijjgWELKzqA/https/pbs.twimg.com/media/E_UwxFzVIAEZRjH.jpg%3Alarge?width=1232&height=672')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
