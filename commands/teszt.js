68b81f158cf7984946b0007ff93ea5a173fc6725
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-16 08:04:08 +0200

    Update teszt.js

--- commands/teszt.js
+++ commands/teszt.js
@@ -3,8 +3,8 @@ module.exports = {
     permissions: ["KICK_MEMBERS"],
     description: 'teszt',
     execute (client, message, args){
-        const teszt = client.channels.cache.find(channel => channel.id === "886583291464003635");
-        teszt.send("Parancsnokok, véget ért a évforduló ünneplése. A szoba le lett zárva, de az event megkezdéséig lehet még nézelődni benne nyaaa~\nhttps://media.discordapp.net/attachments/886040959803097139/886620690097254420/unknown.png");
+        const teszt = client.channels.cache.find(channel => channel.id === "789936292413571102");
+        teszt.send("8 óra*");
         if (teszt){
             return message.channel.send(`Informáltam a többieket a karbantartás végéről nyaaa~`);
           }
