7b62a1708070f3480c93ced0c6d1648df7492f0a
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-16 08:41:01 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀A Világ Leggyorsabb Nyuszi Pincér❀')
+        .setTitle('★Hold nézés 29½.-én★')
         .setURL('')
-        .setDescription('Ugri bugri! Fogj egy poharat és kortyincs! Huuu, ebbe a ruhába elég trükkösen kellett belebújnom. De azt kell mondjam remek választás volt, Parancsnok!\n\nIJN Shimakaze az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/ZLpEl66Hs8OmEmVwKYE9oU2nXachnI4_t3hiuM51sWM/https/pbs.twimg.com/media/E_VkZplVgAYSxWj.jpg%3Alarge?width=1232&height=672')
+        .setDescription('Az éves hold nézés fesztivál megint jön~ Áh, szóval tudsz ezt-azt a Közép-Őszi Fesztiválról, Parancsnok? Akkor számítok rá, hogy tudod mit fogunk csinálni az este, rendben~?♡\n\nUSS Marblehead új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/iqmM9Q1OEY3TKcH-kwci-bOTHM9fZozAAy0_UWmFGEQ/https/pbs.twimg.com/media/E_VlTGwUcAEErga.jpg%3Alarge?width=1316&height=658')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
