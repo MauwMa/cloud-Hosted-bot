573cf1595d68da0010ca7e4f4d73cd5dd907ebf1
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-19 14:17:22 +0200

    Update Node.js

--- Node.js
+++ Node.js
@@ -11,11 +11,17 @@ client.events = new Discord.Collection();
 client.on('guildMemberAdd', guildMember =>{
 
         const embed = new Discord.MessageEmbed()
-        .setTitle(`Üvözlünk az Azur Lane Magyar Szerverén`)
-        .setThumbnail(member.user.displayAvatarURL({dynamic: true, size: 512}))
-        .setDescription(`Hello <@${guildMember.user.id}>, Üvözlünk az Azur Lane Magyar Szerverén.`)
-        .setFooter(`Welcome ${member.user.username}#${member.user.discriminator}`,member.user.displayAvatarURL({dynamic: true, size: 512}))
-        .setColor('RANDOM')
+        .setColor('#f2082d')
+        .setTitle('Z23 Build')
+        .setURL('')
+        .setDescription('Balanced | FP Meme | Tank | Budget')
+        .setImage('https://i.imgur.com/Vo48OAj.png')   
+          
+  //      .setTitle(`Üvözlünk az Azur Lane Magyar Szerverén`)
+  //      .setThumbnail(member.user.displayAvatarURL({dynamic: true, size: 512}))
+  //      .setDescription(`Hello <@${guildMember.user.id}>, Üvözlünk az Azur Lane Magyar Szerverén.`)
+  //       .setFooter(`Welcome ${member.user.username}#${member.user.discriminator}`,member.user.displayAvatarURL({dynamic: true, size: 512}))
+  //      .setColor('RANDOM')
 
     guildMember.guild.channels.cache.get('781214332583149599').send(embed)
 });
