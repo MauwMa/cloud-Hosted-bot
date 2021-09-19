07a2c62934c7680531911c59ba18defc17bc4472
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-19 20:07:33 +0200

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('★Hold Fesztivál Űrséta★')
+        .setTitle('♚A Macska és a Fehér Paripa♚')
         .setURL('')
-        .setDescription('Eldridge megérkezik a kijelölt magasságba... Hold, holdsütemény és Manjuuk észlelve...\n\nUSS Eldridge felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/Y7TZUMSU3mYFZ32qofkHqEq3mvfp4KiB_7wu9K3azS8/https/pbs.twimg.com/media/E_Vkm3gVQAc4H2N.jpg%3Alarge?width=1316&height=658')
+        .setDescription('"Gazdám~ Eljöttem érted, hogy elvigyelek a bállba~" ... Pontosan ezt fogod megtenni? Eh, nem számít ki csinálja~ Csak az számít, hogy együtt leszünk~♪\n\nUHMS Cheshire az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/jfaFv4XLkB6lmA0dvxiIvZCW88nPCjITP6m44AzbkdI/https/pbs.twimg.com/media/E_o8ITxVIActtoJ.jpg%3Alarge?width=1112&height=405')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
