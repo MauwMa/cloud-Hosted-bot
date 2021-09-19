4541c3e9c88956833baaf28a7ab0591a1af17625
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-19 13:54:09 +0200

    Update zzzzz.js

--- commands/zzzzz.js
+++ commands/zzzzz.js
@@ -1,16 +1,24 @@
+
+const { MessageEmbed } = require("discord.js")
+
 module.exports = (client) => {
-    const channelId = "781214332583149599";
-    const rulesChannel = "781214332583149599";
-    client.on("guildMemberAdd", (member) => {
-      console.log(member);
-  
-      const message = `Welcome <@${
-        member.id
-      }> to our server! Be sure to check out our ${member.guild.channels.cache
-        .get(rulesChannel)
-        .toString()}`;
-  
-      const channel = member.guild.channels.cache.get(channelId);
-      channel.send(message);
-    });
-  };
+    const welcomechannelId = '781214332583149599' //Channel You Want to Send The Welcome Message
+    const targetChannelId = `781214332583149599` //Channel For Rules
+
+    client.on('guildMemberAdd', (member) => {
+        console.log(member) // If You Want The User Info in Console Who Joined Server Then You Can Add This Line. // Optional
+        const channel = member.guild.channels.cache.get(welcomechannelId)
+
+        const embed = new MessageEmbed()
+        .setTitle(`Welcome To ${member.guild.name}`)
+        .setThumbnail(member.user.displayAvatarURL({dynamic: true, size: 512}))
+        .setDescription(`Hello <@${member.user.id}>, Welcome to **${member.guild.name}**. Thanks For Joining Our Server.
+Please Read ${member.guild.channels.cache.get(targetChannelId).toString()}. Have a Nice Time.
+Chat In <#787083837833871404>`)
+        // You Can Add More Fields If You Want
+        .setFooter(`Welcome ${member.user.username}#${member.user.discriminator}`,member.user.displayAvatarURL({dynamic: true, size: 512}))
+        .setColor('RANDOM')
+    channel.send(embed)
+        
+    })
+}
