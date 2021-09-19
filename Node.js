74b5f667dc7872222a67d2acb0f2c633ff0ca554
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-19 14:29:12 +0200

    Update Node.js

--- Node.js
+++ Node.js
@@ -18,10 +18,10 @@ client.on('guildMemberAdd', guildMember =>{
         //.setImage('https://i.imgur.com/Vo48OAj.png')   
 
         .setColor('#f2082d')
-        .setTitle(`Üdvözlünk az  ${guildMember.guild.name}`)
+        .setTitle(`Üdvözlünk az Azur Lane Magyar Szerverén`)
         .setThumbnail(guildMember.user.displayAvatarURL({dynamic: true, size: 512}))
-        .setDescription(`Hello <@${guildMember.user.id}>, Üvözlünk az Azur Lane Magyar Szerverén.`)
-        .setFooter(`Welcome ${guildMember.user.username}#${guildMember.user.discriminator}`,guildMember.user.displayAvatarURL({dynamic: true, size: 512}))
+        .setDescription(`Üdv <@${guildMember.user.id}> Parancsnok`)
+        .setFooter(`Üdv ${guildMember.user.username}#${guildMember.user.discriminator}`,guildMember.user.displayAvatarURL({dynamic: true, size: 512}))
         .setColor('RANDOM')
 
     guildMember.guild.channels.cache.get('781214332583149599').send(embed)
