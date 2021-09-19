5d0e0ca2900eb8456e9af014a531d96a2e94246a
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-19 13:41:10 +0200

    dd

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,10 @@
 module.exports = () =>{
     console.log('Akashi online');
+
+    client.on('guildMemberAdd', guildMember =>{
+        let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'valami');
+     
+        guildMember.roles.add(welcomeRole);
+        guildMember.guild.channels.cache.get('781214332583149599').send(`Welcome <@${guildMember.user.id}> to our server! Make sure to check out the rules channel!`)
+    })
 }
