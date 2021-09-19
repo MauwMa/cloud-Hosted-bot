0719f82280010b58e2a608dfb244ba1d88324fab
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-19 13:48:16 +0200

    Update Node.js

--- Node.js
+++ Node.js
@@ -8,4 +8,11 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 })
 
+client.on('guildMemberAdd', guildMember =>{
+    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'valami');
+ 
+    guildMember.roles.add(welcomeRole);
+    guildMember.guild.channels.cache.get('781214332583149599').send(`Welcome <@${guildMember.user.id}> to our server! Make sure to check out the rules channel!`)
+});
+
 client.login(process.env.token);
