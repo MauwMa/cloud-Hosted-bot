878656fcfc4184869ad7078a87f959b519d126ef
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-20 12:06:47 +0200

    Update teszt2.js

--- commands/teszt2.js
+++ commands/teszt2.js
@@ -6,7 +6,7 @@ module.exports = {
         const teszt2 = new Discord.MessageEmbed()
         .setColor('#2ecc70')
         .setTitle(`Title`)
-        .setThumbnail(`https://c.tenor.com/EPOO70DkSO8AAAAi/azur-lane.gif`({dynamic: true, size: 512}))
+        .setThumbnail(`https://c.tenor.com/EPOO70DkSO8AAAAi/azur-lane.gif`)
         .setDescription(`Description`)
         .setFooter(`footer`)
         message.channel.send(teszt2);
