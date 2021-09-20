a5b8c4786ff6b81223480956c7e35ac92faf815f
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-20 08:32:10 +0200

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('♚A Macska és a Fehér Paripa♚')
+        .setTitle('♚A Dícső Tószt♚')
         .setURL('')
-        .setDescription('"Gazdám~ Eljöttem érted, hogy elvigyelek a bálba~" ... Pontosan ezt fogod megtenni? Eh, nem számít ki csinálja~ Csak az számít, hogy együtt leszünk~♪\n\nUHMS Cheshire az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/jfaFv4XLkB6lmA0dvxiIvZCW88nPCjITP6m44AzbkdI/https/pbs.twimg.com/media/E_o8ITxVIActtoJ.jpg%3Alarge?width=1112&height=405')
+        .setDescription('Egészségedre, Parancsnok. Örülök, hogy a parti sikeres. Nos, nekem is vigyáznom kellene, hogy ne szennyezzem be a királyi család becsületét a cselekedeteimmel.\n\nHMS Exeter az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/kUwxsitp6_MLLkm9oOaZkN318zUCaTPrmfN7fZf2AmE/https/pbs.twimg.com/media/E_o8szYVUAEHQPC.jpg%3Alarge?width=1112&height=405')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
