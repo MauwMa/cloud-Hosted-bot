aebc90f740c7cdf2ad4b3ebc39231b5a254b492f
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-21 09:35:09 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -4,8 +4,8 @@ module.exports = {
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
-        .setColor('♚Angyali Napsütés♚')
-        .setTitle('')
+        .setColor('RANDOM')
+        .setTitle('♚Angyali Napsütés♚')
         .setURL('')
         .setDescription('Az időjárás szép napos, bármit is fogok csinálni ma az biztosan sikeres lesz. Mindenki másnak legyen ilyen szép napja... Awuhau! Kicsi manjuu, jól vagy? Nem hiszem el, hogy megbotlottam...\n\nHMS Fortune új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
         .setImage('https://images-ext-2.discordapp.net/external/HenEsLlTf4mobV-Z7zsIjej4QK1Y1o6PCvLu27D6Yjo/https/pbs.twimg.com/media/E_uFGEvVIAEhT2-.jpg%3Alarge?width=1112&height=405')
