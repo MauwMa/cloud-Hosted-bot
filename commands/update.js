62a7cacaa4e2687d65cb09fe8e0ece365131938a
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-21 18:54:14 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('RANDOM')
         .setTitle('✠Weiss Uhrzeiger✠')
         .setURL('')
-        .setDescription('Már úgy is tudod, nem igaz? Mivel olyan sok időt töltöttem azzal, hogy ilyen ruhába öltözzek a kedvedért... Heh, viszonozásképp rengeteg idődet fogom elvenni. Hehehe.\n\nKMS Peter Strasser új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setDescription('Már úgy is tudod, nem igaz? Mivel olyan sok időt töltöttem azzal, hogy ilyen ruhába öltözzek a kedvedért... Heh, viszonozásképp rengeteg idődet fogom elrabolni. Hehehe.\n\nKMS Peter Strasser új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
         .setImage('https://images-ext-2.discordapp.net/external/ARdU45Wxrxiv8w0ckA04IOflfCo6fcB3Wll-ILYMarY/https/pbs.twimg.com/media/E_zeNiTVkAYHomV.jpg%3Alarge?width=1112&height=405')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
