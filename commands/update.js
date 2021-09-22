3fa1339cf2fe15a0876fd6b6027a279d3f556604
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-22 17:48:10 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('⚜Díszes Tánc⚜')
+        .setTitle('⛨Kisasszony meghívója⛨')
         .setURL('')
-        .setDescription('Bár szerény lehet, de ez nem egy bájos kicsi kert? Gyere, Parancsnok! Táncoljunk Anyatermészettel körbevéve. \n\nFFNF Émile Bertin új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/RdX3o59jvVBnaQ5j77TFM5QocuZSQ4SG9FGbRbEywFA/https/pbs.twimg.com/media/E_zd0RkVQAEVqS4.jpg%3Alarge?width=1112&height=405')
+        .setDescription('"A legfaragatlanabb dolog lenne visszautasítani egy kisasszony meghívóját"... Hehe~ Most hát, Parancsnok, szeretnél egy kis frissítőt? Nem kell aggódnod, mind alkoholmentes♪ \n\nRN Vincenzo Gioberti új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/4sekEXmP_k7jTCnYu9Pk8r8zcn3Yyg4mcLqmT5EGpCc/https/pbs.twimg.com/media/E_40KbVVQAQwE8P.jpg%3Alarge?width=1920&height=698')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
