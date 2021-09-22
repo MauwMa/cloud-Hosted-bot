8f6885c7983a3dae098fe73e012744d4b1eb8f19
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-22 07:37:02 +0200

    dsa

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('✠Weiss Uhrzeiger✠')
+        .setTitle('⚜Díszes Tánc⚜')
         .setURL('')
-        .setDescription('Már úgy is tudod, nem igaz? Mivel olyan sok időt töltöttem azzal, hogy ilyen ruhába öltözzek a kedvedért... Heh, viszonozásképp rengeteg idődet fogom elrabolni. Hehehe.\n\nKMS Peter Strasser új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/ARdU45Wxrxiv8w0ckA04IOflfCo6fcB3Wll-ILYMarY/https/pbs.twimg.com/media/E_zeNiTVkAYHomV.jpg%3Alarge?width=1112&height=405')
+        .setDescription('Bár szerény lehet, de ez nem egy bájos kicsi kert? Gyere, Parancsnok! Táncoljunk Anyatermészettel körbevéve. \n\nFFNF Émile Bertin új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/RdX3o59jvVBnaQ5j77TFM5QocuZSQ4SG9FGbRbEywFA/https/pbs.twimg.com/media/E_zd0RkVQAEVqS4.jpg%3Alarge?width=1112&height=405')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
--- commands/update2.js
+++ commands/update2.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('♚A Dícső Tószt♚')
+        .setTitle('⛨Egy korty a Sardegniani Eleganciából⛨')
         .setURL('')
-        .setDescription('Egészségedre, Parancsnok. Örülök, hogy a parti sikeres. Nos, nekem is vigyáznom kellene, hogy ne szennyezzem be a királyi család becsületét a cselekedeteimmel.\n\nHMS Exeter az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/kUwxsitp6_MLLkm9oOaZkN318zUCaTPrmfN7fZf2AmE/https/pbs.twimg.com/media/E_o8szYVUAEHQPC.jpg%3Alarge?width=1112&height=405')
+        .setDescription('Óh, Parancsnokom? Nem számítottam rá, hogy itt találkozzunk! A szerencse ma rám mosolyog. Hehe~ Van egy perced rám szánni, szeretnél csatlakozni hozzám egy csésze teára?\n\nRN Aquila az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/InOe4Pia21SU2z28SHj3-SgExsH_b1hZdNz1Hri2ocA/https/pbs.twimg.com/media/E_zeeCNVkAAY4Am.jpg%3Alarge?width=1112&height=405')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
