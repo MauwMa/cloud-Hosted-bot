eb0d542f9d7fb4ca136a040326b868e036b76407
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-24 09:26:59 +0200

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -8,7 +8,7 @@ module.exports = {
         .setTitle('Lucky Box')
         .setURL('')
         .setDescription('A 2021-es Party Skin Zsákbamacska elérhető egészen 10/14 Magyar időszámítás szerint reggel 09:00-ig!')
-        .setImage('hhttps://images-ext-1.discordapp.net/external/QFg7y3Ev8vRKkzgeKb1BAWDz4hsLhSAJHksLKwIJmoo/https/pbs.twimg.com/media/E_-Y1JkVUAQ7Dxl.jpg%3Alarge')
+        .setImage('https://images-ext-1.discordapp.net/external/QFg7y3Ev8vRKkzgeKb1BAWDz4hsLhSAJHksLKwIJmoo/https/pbs.twimg.com/media/E_-Y1JkVUAQ7Dxl.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
