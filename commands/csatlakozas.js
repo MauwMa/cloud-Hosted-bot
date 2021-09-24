8b6fa434386a98156a6f8c289f36326228c565c9
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-24 18:41:29 +0200

    Update csatlakozas.js

--- commands/csatlakozas.js
+++ commands/csatlakozas.js
@@ -4,20 +4,14 @@ module.exports = {
     description: 'csatlakozas',
     execute (client, message, args, Discord){
         const csatlakozas = new Discord.MessageEmbed()
-        .setColor('GREEM')
+        .setColor('GREEN')
         .setTitle('Melyik szerveren játszol?')
         .setURL('')
         .setDescription(':white_circle:=>Avrora | :brown_circle:=>Avrora | :purple_circle:=>Lexington \n\n:red_circle:=>Sandy | :blue_circle:=>Washington')
         message.channel.send(csatlakozas)
         .then ((msg) => {setTimeout(function(){
-            msg.react(`⚪`)
             msg.react('<:Murmwhat:862005891327066113>')
-            msg.react("🟣")
-            msg.react("🔴")
-            msg.react("🔵")
-            msg.react("")
-            msg.react("")
-            msg.react("")
+
         }, 1000)})
         
     }    
