eb1b5ea4477b786794b35caab1dba176965c99b8
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-25 08:30:28 +0200

    Update command_handler.js

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -1,10 +1,20 @@
 const fs = require('fs');
 
 module.exports = (client, Discord) =>{
-    const command_files = fs.readdirSync('./commands/', './builds/').filter(file => file.endsWith('js'));
+    const command_files = fs.readdirSync('./commands/').filter(file => file.endsWith('js'));
+    const command_files = fs.readdirSync('./builds/').filter(file => file.endsWith('js'));
 
     for(const file of command_files){
         const command = require (`../commands/${file}`);
+        if(command.name){
+            client.commands.set(command.name, command);
+        } else{
+            continue;
+        }
+    }
+
+    for(const file of command_files){
+        const command = require (`../builds/${file}`);
         if(command.name){
             client.commands.set(command.name, command);
         } else{
