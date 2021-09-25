76b2a80b0469b12a534e3c1d9920823b9c2e874d
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-25 08:11:18 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀Csábítás a tengeri szellőben❀')
+        .setTitle('❀Két szerető regéje❀')
         .setURL('')
-        .setDescription('Az új oath skin a következő karbantartás után elérhető lesz miután megesküszöl Tauhouval.')
-        .setImage('https://images-ext-1.discordapp.net/external/e4U9NgVfJ7Y_om9W3F4Ul9dtAtAy2BKm5sTGBxJRzn8/https/pbs.twimg.com/media/FADei3aUcAINqIh.jpg%3Alarge')
+        .setDescription('Az új oath skin a következő karbantartás után elérhető lesz miután megesküszöl Noshiroval.')
+        .setImage('https://images-ext-2.discordapp.net/external/WhWtx1y5eRNLwaPYNukoFalnjPnZOsxIVwGsMTipHJE/https/pbs.twimg.com/media/FADGIXdUcAY2QhD.jpg%3Alarge?width=794&height=458')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
