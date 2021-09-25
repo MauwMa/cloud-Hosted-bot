9a087081a2afc3c18695d0bf7576c2973647f1b8
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-25 19:41:29 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀Két szerető regéje❀')
+        .setTitle('♚A tisztaság esküje♚')
         .setURL('')
-        .setDescription('Az új oath skin a következő karbantartás után elérhető lesz miután megesküszöl Noshiroval.')
-        .setImage('https://images-ext-2.discordapp.net/external/WhWtx1y5eRNLwaPYNukoFalnjPnZOsxIVwGsMTipHJE/https/pbs.twimg.com/media/FADGIXdUcAY2QhD.jpg%3Alarge?width=794&height=458')
+        .setDescription('Azta,,, Ez az én Egyenruhám a ceremóniára, ami a Tisztaság Esküje néven simert? Hohoho... Ez biztosan a sors. Csatlakozz hozzám, Parancsnok, és csináljuk újra igazzá a világot! ')
+        .setImage('https://images-ext-2.discordapp.net/external/5g-giqIkOTjzNI44E2fc-oSYk7ZnRWtOvlDra2Wme7A/https/pbs.twimg.com/media/FAImM-JVUAA8lov.jpg%3Alarge?width=1172&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
