9e64933ae5eba45cde5a519b12f7a4ab9d6fefbf
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-25 08:20:16 +0200

    d

---
+++ commands/build-lemalin.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'build-lemalin',
+    description: 'build-lemalin',
+    permissions: ["SEND_MESSAGES"],
+    execute (client, message, args, Discord){
+        const lemalin = new Discord.MessageEmbed()
+        .setColor('#f2082d')
+        .setTitle('Le Malin Build')
+        .setURL('')
+        .setDescription('dmg | Auto1 | Auto2 | Budget')
+        .setImage('https://i.imgur.com/e91pLxE.png')
+        message.channel.send(lemalin);
+
+    }    
+}
---
+++ commands/build-shimakaze.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'build-shimakaze',
+    description: 'build-shimakaze',
+    permissions: ["SEND_MESSAGES"],
+    execute (client, message, args, Discord){
+        const shimakaze = new Discord.MessageEmbed()
+        .setColor('#ff90ff')
+        .setTitle('Shimakaze Build')
+        .setURL('')
+        .setDescription('NUKE(Manual) | Auto | Manual | Budget')
+        .setImage('https://i.imgur.com/vVh7F9X.png')
+        message.channel.send(shimakaze);
+
+    }    
+}
