c67b9047d7debfeae0fdc794f42f7967e4c95222
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-09-26 18:43:26 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,10 +5,10 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('Értesítés')
+        .setTitle('❀Yuudachi (Retrofit)❀')
         .setURL('')
-        .setDescription('A karbantartás után a Daily Raidekhez egy új nehézségi szint fog bemutatkozni. A "Supply Line Disruption" heti 2-re lesz lelimitálva. Javasolt hogy az új nehézségi szintre várjatok, amíg elérhető lesz a karbantartás után mielőtt kipróbálnátok.')
-        .setImage('https://images-ext-1.discordapp.net/external/6NG1wllz0P09O2FGV---ETCaVEwKDk9zrJNl81llHs0/https/pbs.twimg.com/media/FAM5SPDVkAUE7lX.jpg%3Alarge')
+        .setDescription('Jelenleg Yuudachi a kikötőben van fejlesztés miatt. Hamarosan vízre lesz bocsájtva nyaaa~')
+        .setImage('https://images-ext-1.discordapp.net/external/QIvzLzufvIhrD4DJQgQP2ga7dvQsM1pmaUoHPIki2p8/https/pbs.twimg.com/media/FAM6FrLUUAE0ASl.jpg%3Alarge?width=845&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
