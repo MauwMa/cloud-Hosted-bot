d1186cb23b60926b4251c455ba9a9d67d5143c38
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-03 11:52:55 +0200

    Update teszt.js

--- commands/teszt.js
+++ commands/teszt.js
@@ -4,7 +4,7 @@ module.exports = {
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args){
 
-        message.channel.send("https://i.imgur.com/QxNKPzU.jpg")
+        message.channel.send("https://i.imgur.com/AJh6R10.png")
 
     if (message.deletable) message.delete();
 
