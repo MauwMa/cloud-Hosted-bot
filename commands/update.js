91a92c9d72dedf80451f9871f7fd9796979b4a9a
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-10 18:21:09 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('Emlékeztető')
+        .setTitle('【Crimson Echoes】')
         .setURL('')
         .setTimestamp()
-        .setDescription('A nyuszi lány skinek magyar időszámítás szerint 10/14 reggel 08:00-ig lesznek elérhetőek.')
-        .setImage('https://i.imgur.com/s4mTORC.jpg')
+        .setDescription('A Crimson Echoes event véglegesen bekerül a War Archives-ba a következő karbantartás után.')
+        .setImage('https://images-ext-2.discordapp.net/external/WXHBPYtXZnsfA8LhcpEGV5CsNgb0uUW6mUeXNFeon5Q/https/pbs.twimg.com/media/FBPqm5lVcAAcjwW.png%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
