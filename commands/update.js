7517ec48edb853b5b07883127f6dd315fff235bf
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-14 19:21:30 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('Emlékeztető')
+        .setTitle('')
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok\n\nA parti skinek 10/14 magyar időszámítás szerint 09:00-ig lesznek elérhetőek.')
-        .setImage('https://i.imgur.com/WjMv86E.jpg')
+        .setDescription('Parancsnokok,\n\n Volt egy hiba amely megengedte, hogy a "Popularity Poll"-ban lehetett szavazni Bismarckra. Top 3 volt 2019-ben ezért nem jogosult a szavazásra. Az összes szavazat, amely Bismarckra lett használva vissza lett küldve üzenetben. Elnézést a kellemetlenségért.')
+        .setImage('https://images-ext-2.discordapp.net/external/x3FlMERywGpP07X7X6y-Tgbmf0_L9D3WStfvGtZmowU/https/pbs.twimg.com/media/FBq4DBRUUAQEiKG.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
