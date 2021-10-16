d8636590aacb0b3fdb823a5b6c885d506792eb7c
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-16 08:09:56 +0200

    Create felmeres.js

---
+++ commands/felmeres.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'felmeres',
+    permissions: ["KICK_MEMBERS"],
+    description: 'felmeres',
+    execute (client, message, args, Discord){
+        const frakcio = new Discord.MessageEmbed()
+        .setColor('WHITE')
+        .setTitle('Felmérés')
+        .setURL('')
+        .setDescription('Kíváncsiskodásképp ki az, aki hanggal játsza a játékot?')
+        message.channel.send(frakcio)
+        .then ((msg) => {setTimeout(function(){
+            msg.react('<:Javyes:883269683439091712>')
+            msg.react('<:Javno:883269657174372382>')
+        }, 1000)})
+        
+    }    
+
+}
