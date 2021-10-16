c0a7c5ee5338a81eb53c98a2d63950aaf5c747ca
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-16 08:17:27 +0200

    Update felmeres2.js

--- commands/felmeres2.js
+++ commands/felmeres2.js
@@ -1,19 +1,19 @@
 module.exports = {
-    name: 'felmeres',
+    name: 'felmeres2',
     permissions: ["KICK_MEMBERS"],
-    description: 'felmeres',
+    description: 'felmeres2',
     execute (client, message, args, Discord){
-        const felmeres = new Discord.MessageEmbed()
+        const felmeres2 = new Discord.MessageEmbed()
         .setColor('#304281')
         .setTitle('Felmérés')
         .setURL('')
         .setDescription('Kíváncsiskodásképp ki az, aki hanggal játsza a játékot?')
-        client.channels.cache.find(channel => channel.id === "853629612986990612").send(felmeres)
+        client.channels.cache.find(channel => channel.id === "853629612986990612").send(felmeres2)
         .then ((msg) => {setTimeout(function(){
             msg.react('<:Javyes:883269683439091712>')
             msg.react('<:Javno:883269657174372382>')
         }, 1000)})
-        if (felmeres){
+        if (felmeres2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
           }
     }    
