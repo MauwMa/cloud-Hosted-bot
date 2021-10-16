31f9a4d60a9beae0d6f3a42d9626dde9aa8bc40f
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-16 08:26:41 +0200

    dsa

--- commands/felmeres.js
+++ commands/felmeres.js
@@ -4,16 +4,18 @@ module.exports = {
     description: 'felmeres',
     execute (client, message, args, Discord){
         const felmeres = new Discord.MessageEmbed()
-        .setColor('#304281')
+        .setColor('#2ecc71')
         .setTitle('Felmérés')
         .setURL('')
         .setDescription('Kíváncsiskodásképp ki az, aki hanggal játsza a játékot?')
-        message.channel.send(felmeres)
+        client.channels.cache.find(channel => channel.id === "789936506193182740").send(felmeres)
         .then ((msg) => {setTimeout(function(){
             msg.react('<:Javyes:883269683439091712>')
             msg.react('<:Javno:883269657174372382>')
         }, 1000)})
-        
+        if (felmeres){
+            return message.channel.send(`Elküldtem az updatet nyaaa~`);
+          }
     }    
 
 }
