0015d40e478c3e6691794e0ab2a3e0876b3114e6
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-17 12:20:46 +0200

    dsa

---
+++ commands/felszer-bbmain.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'felszer-bbmain',
+    description: 'felszer-bbmain',
+    permissions: ["KICK_MEMBERS"],
+    execute (client, message, args){
+
+        message.channel.send("https://i.imgur.com/qzMzc8c.jpg")
+
+    }    
+}
+
+
