f80541e650fd21853219961c6242b754302d3d65
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-17 14:06:11 +0200

    imagetet

--- commands/teszt.js
+++ commands/teszt.js
@@ -3,14 +3,9 @@ module.exports = {
     description: 'teszt',
     permissions: ["SEND_MESSAGES"],
     execute (client, message, args, Discord){
-        const teszt = new Discord.MessageEmbed()
-        .setColor('#304281')
-        .setTitle('Nagato már nem loli többé')
-        .setURL('')
-        .setDescription('')
-        .setImage('https://i.imgur.com/qzMzc8c.jpg')
-        .setTimestamp()
-        message.channel.send(teszt);
+        const image = new Attachment('./bbmain.png')
+        message.channel.send(image);
+
 
     }    
 }
