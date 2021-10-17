d369f6fb4808081c4b2833dff3e712d08be4a7e6
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-17 10:35:05 +0200

    Create bbfelszer.js

---
+++ commands/bbfelszer.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'bbfelszer',
+    description: 'bbfelszer',
+    permissions: ["KICK_MEMBERS"],
+    execute (client, message, args){
+
+        message.channel.send("https://i.imgur.com/6bC3yC8.jpg")
+
+    }    
+}
+
+
