806baffa941e08b9a5d7fd7f0fc9c640d0153a64
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-17 14:10:23 +0200

    dsa

---
+++ commands/tesst.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'tesst',
+    description: 'tesst',
+    permissions: ["SEND_MESSAGES"],
+    execute (client, message, args, Discord){
+        const tesst = new Attachment('./bbmain.png')
+        message.channel.send(tesst);
+
+
+    }    
+}
