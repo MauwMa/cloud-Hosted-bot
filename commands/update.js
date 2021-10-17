68cd5e4ccf70e1df1fa5db988850cffc802930b6
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-17 18:24:31 +0200

    dsa

--- commands/update.js
+++ commands/update.js
@@ -8,8 +8,8 @@ module.exports = {
         .setTitle('')
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok,\n\n Fürdőruhás skinek Amagira, Tosára és sok másra visszatértek. Magyar időszámítás szerint 10/27 8:59-ig lesznek elérhetőek.')
-        .setImage('https://i.imgur.com/IyBX6Q8.jpg')
+        .setDescription('USS New Orleans felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/Z4_KOXnMuisYJUFTPGf5HzKgwX9kOHuE8XNVN9KwKcg/https/pbs.twimg.com/media/FB0ad5rXMAIM_-E.jpg%3Alarge?width=1739&height=910')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
