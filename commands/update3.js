f82ed168e7163e44daf56ce306b420c4c2d71768
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-23 18:30:53 +0200

    Update update3.js

--- commands/update3.js
+++ commands/update3.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update3 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('⚜Le Terrible⚜')
+        .setTitle('⚜Maillé Brézé⚜')
         .setURL('')
         .setTimestamp()
-        .setDescription('Le Terrible vagyok, az elegáns nagy romboló, ki védi az Iris Libre tanítását. Az eszményeidet követve jöttem. Parancsnok, kérlek mutasd meg a harcolásod okát. \n\nFFNF Le Terrible felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
-        .setImage('https://i.imgur.com/weJpk94.jpg')
+        .setDescription('Maillé Brézé romboló, Iris Libre Tomplomos Lovagja megérkezett! Hé te, paraszt, te ki mélyen alszik tudva, hogy én védem eme földeket! ...Mi? T-Te vagy a Parancsnokúr? B-B-Bocsásd meg a tiszteletlenségemet! \n\nFFNF Maillé Brézé felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/L8w97NVO3r-jMeX5wHRRhkTRft9DOYYoVJ4G0kzJrTg/https/pbs.twimg.com/media/FCYkkuBVIAUuJ1y.jpg%3Alarge?width=1739&height=910')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update3);
         if (update3){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
