00e3496e9a12a4a05494856c12c12b949fb491cf
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-25 20:57:20 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('⚜Napfény, Tengeri fuvallat, and Szentség')
+        .setTitle('⛨Heverésző Smaragd⛨')
         .setURL('')
         .setTimestamp()
-        .setDescription('Miért nem lazítasz egy kicsit, mielőtt végiggondolnád a nap történéseit? Biztosan sokkal hatásosabb lenne úgy.\n\nIJN Noshiro az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/AEgrIMe3hpVbz7LcPnbMmmI7YdyZTkk6u4Bj1UWny9Y/https/pbs.twimg.com/media/FCip1StVIAcllYQ.jpg%3Alarge?width=1202&height=676')
+        .setDescription('Hellóka. Bocsásd meg a lenge öltözetemet. Alig van bármilyen okom arra, hogy extravagáns ruhát hordjak másoktól távoli privát helyemen.\n\nRN Littorio az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/wGLixZtox-5Jv5zYHZ8CTny-d2AjiM3GtE5FtTUmhL4/https/pbs.twimg.com/media/FCiqGseVkAYk752.jpg%3Alarge?width=1202&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
