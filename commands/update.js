76e2aabd8551b30ad7cae4a9d66c38594a7f2491
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-25 08:12:45 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -8,8 +8,8 @@ module.exports = {
         .setTitle('❀PJ Poker Party❀')
         .setURL('')
         .setTimestamp()
-        .setDescription('Most te következel, Parancsnok! Melyik legyen az~? Biiiiiztos vagy, hogy ez a kártya legyen? ...Hallak, Suzuya! Ne mond el a Parancsnoknak milyenek a kártyák!\n\nIJN Kumano az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://i.imgur.com/Ygla43t.jpg')
+        .setDescription('Hogy telik a napod, Parancsnok? Hehe, az okos nővéremet keresed, Le Malint? Ha igen, akkor jobban tennéd, ha olyan helyen keresnéd amely kevésbé... népszerű~♪\n\nFFNF Le Terrible az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/JHjss9isTRzw-EdxJtidEofluVHqGN_am8kykYSqd4A/https/pbs.twimg.com/media/FCheaSgVEAIkSMg.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
