dcda4d204c994e8f2ce6db44ad8f75dd6e652932
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-26 14:30:29 +0200

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('⛨Heverésző Smaragd⛨')
+        .setTitle('Értesítés')
         .setURL('')
         .setTimestamp()
-        .setDescription('Hellóka. Bocsásd meg a lenge öltözetemet. Alig van bármilyen okom arra, hogy extravagáns ruhát hordjak másoktól távoli privát helyemen.\n\nRN Littorio az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/wGLixZtox-5Jv5zYHZ8CTny-d2AjiM3GtE5FtTUmhL4/https/pbs.twimg.com/media/FCiqGseVkAYk752.jpg%3Alarge?width=1202&height=676')
+        .setDescription('Parancsnokok\n\nA Facebook bejelentkező hiba meg lett oldva. Parancsnokok mostantól be fognak tudni jelentkezni a facebook felhasználójukkal. Kompenzálásként 3 High-Efficiency Combat Logistics Plan és 6db :kocka:-t osztunk ki. Köszönjük a türelmeteket.')
+        .setImage('https://images-ext-1.discordapp.net/external/uqiaQAlKAye2JfpJWhzxjJmVdlStIR5jXbRvcyQqGcM/https/pbs.twimg.com/media/FCndsIOUcAMEyHO.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
