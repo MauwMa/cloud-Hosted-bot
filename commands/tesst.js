32dde6cdccc2deee8768f5a3a2e25cb068e0dabe
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-26 14:39:21 +0200

    kcokca

--- commands/tesst.js
+++ commands/tesst.js
@@ -1,10 +1,14 @@
 module.exports = {
     name: 'tesst',
     description: 'tesst',
-    permissions: ["SEND_MESSAGES"],
-    execute (client, message, args, Discord){
-        message.channel.send("Message", {files: ["bbmain.png"]});
+    permissions: ["KICK_MEMBERS"],
+    execute (client, message, args){
 
+        message.channel.send(":Kocka:")
+
+    if (message.deletable) message.delete();
 
     }    
 }
+
+
