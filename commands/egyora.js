0a804a80238c0bfece2f8c7b8d9fa2f452572d58
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-10-28 08:12:27 +0200

    Update egyora.js

--- commands/egyora.js
+++ commands/egyora.js
@@ -4,12 +4,12 @@ module.exports = {
     permissions: ["KICK_MEMBERS"],
     description: 'egyora',
     execute (client, message, args, Discord){
-        var exportScheduler = schedule.scheduleJob('0 11 6 21 10 *', function() { 
+        var exportScheduler = schedule.scheduleJob('0 20 6 28 10 *', function() { 
             const egyora = new Discord.MessageEmbed()
             .setColor('#ffff00')
             .setTitle('Értesítés!')
             .setURL('')
-            .setDescription('50 perces előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 6 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
+            .setDescription('40 perces előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 6 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
             .setImage('https://i.imgur.com/0BEHNji.png')
             .setTimestamp()
             client.channels.cache.find(channel => channel.id === "789936292413571102").send(egyora); 
