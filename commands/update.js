3f2c75aadca5b0bc02368c60465cf9bc1a94636a
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-05 10:15:00 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -8,7 +8,7 @@ module.exports = {
         .setTitle('Értesítés')
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok\n\nFacebook hiba miatt, a facebookal összecsatoló bejelentkezéssel jelenleg gondok vannak. Parancsnokok Twitterrel és Yostar felhasználóval probléma mentesen be tudnak jelentkezni. Kérünk legyetek türelmesek, amíg megvizsgáljuk a problémát. Köszönjük a támogatást.')
+        .setDescription('Parancsnokok\n\nA Facebook bejelentkező hiba meg lett oldva. Parancsnokok mostantól be fognak tudni jelentkezni a facebook felhasználójukkal. Azonban, mivel a Facebookal mostanában több hiba is felmerült, a Főhadiszállás erősen ajánlja, hogy vagy Twitterrel vagy a Yostarral is kösd össze a felhasználódat.')
         .setImage('https://images-ext-1.discordapp.net/external/vaDLFsI5igNpQ4bO_5Ufal06OmUkuRwI71-nT7xsz9g/https/images-ext-2.discordapp.net/external/9NTkVab8vxCxJ9usxuWF7C_QAiBrWZ9tCx99hhzdRmA/https/pbs.twimg.com/media/FCX3C50VgAMoOLN.jpg%253Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
