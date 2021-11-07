4220f2b8b24ee982bfaea3db7360ee3ebb72077c
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-07 17:36:54 +0100

    Update update3.js

--- commands/update3.js
+++ commands/update3.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update3 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('✠Utolsó Kör✠')
+        .setTitle('★Szél lovagló★')
         .setURL('')
         .setTimestamp()
-        .setDescription('Viel Glück~ Jó pillanatod van, Parancsnok? Csak hogy tudd az egyetlen személy akinek szurkolok... te vagy~\n\nKMS Prinz Eugen az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/tr7E9FPKdCJc9vOG1712RuGs4M6rdosWNTK1SwNifq8/https/pbs.twimg.com/media/FDgT-C6aAAAyVh7.jpg%3Alarge?width=1738&height=910')
+        .setDescription('Parancsnok, jöttél nézni a meccset? Tudod... Inkább lennék lent versenyezni, mintsem csak itt állni. Az érzés, hogy meg tudod lovagolni a szelet... Nem gondolod, hogy a versenyzés és a vitorlázás ugyan azon a lényegen osztozkodnak?\n\nUSS Enterprise az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/cDmQWZLrEHjuTdHkrTryy8MdxhpFbctypV7wJZmMczQ/https/pbs.twimg.com/media/FDlG2ZVVUAAlbRv.jpg%3Alarge?width=1060&height=555')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update3);
         if (update3){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
