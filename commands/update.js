c589ee59d017d09024320717312d9841e423bcb9
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-10 14:46:41 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -8,8 +8,8 @@ module.exports = {
         .setTitle('Emlékeztető')
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok\n\n La Galissonniére L2D skine és a többi új skin 11/11 magyar időszámítás szerint reggel 7:00-ig lesz elérhető.')
-        .setImage('https://images-ext-1.discordapp.net/external/oWde7_C6alQ3MNyU6KbOvwfhORttVNneVuZg72Y7O3g/https/pbs.twimg.com/media/FC8rCnxVkAQSf0Z.jpg%3Alarge?width=1127&height=555')
+        .setDescription('Parancsnokok\n\n A Cognitive Array Support Terv event 11/11 magyar időszámítás szerint 08:00-kor ér véget. Kérjük mindenki szedje ki a jutalmakat, amíg lehet.')
+        .setImage('https://images-ext-2.discordapp.net/external/292RaXOWs_MyoaQpMk48qXGB0yT8_ns7dM5Gr8dJp18/https/images-ext-1.discordapp.net/external/e-ITop4dEK09gz6doLFu0WEIEZxgXhlc84BVViOf7So/https/pbs.twimg.com/media/FA8Bw8mVEAUioZe.png%253Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
