fc45486c358c85d2353193bf86197e6e36075ef3
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-10 20:12:41 +0100

    dsa

--- commands/felszer.js
+++ commands/felszer.js
@@ -13,8 +13,9 @@ module.exports = {
             {name: 'Csatahajó útmutatók:', value: ':small_blue_diamond: Főfegyver: /felszer-bbmain'},
             {name: 'Repülőgép-Hordozó útmutatók:', value: ':small_blue_diamond: Repülőgépek: /felszer-cvrepcsik'},
             {name: 'Cirkáló útmutatók', value: ':small_blue_diamond: Főfegyver: /felszer-cruisermain'},
-            {name: 'Általános útmutatók:', value: ':small_blue_diamond: Légvédelem: /felszer-antiair'},
-            {name: 'Romboló útmutatók:', value: ':small_blue_diamond: Főfegyver: /felszer-ddmaingun'}
+            {name: 'Romboló útmutatók:', value: ':small_blue_diamond: Főfegyver: /felszer-ddmaingun'},
+            {name: 'Általános útmutatók:', value: ':small_blue_diamond: Légvédelem: /felszer-antiair'}
+            
         )
         message.channel.send(felszer);
 
