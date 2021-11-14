bb9939913f85dfdea6a399cf3e6f94cdc602b81a
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-14 16:05:08 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('【The Flame-Touched Dagger】')
+        .setTitle('★Black Friday Rerun Outfits★')
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok,\n\nEzek a limitált skinek 11/25 magyar időszámítás szerint 07:59-ig elérhetőek. Ne hadjátok ki a lehetőséget, hogy megszerezzétek!')
-        .setImage('https://images-ext-1.discordapp.net/external/PewGqojlZtxSD3zF9gVtv0aMjjmeew9iLbGwMszUImw/https/pbs.twimg.com/media/FD_ZKzcVIA00KXi.jpg%3Alarge?width=1125&height=554')
+        .setDescription('Hogy megünnepeljük a black friday-t, Black Friday Rerun Outfits különleges listája visszatér November 18.-án. A Black Friday Rerun Outfits, 2021 Június 10.-e előtti skinek lesznek elérhetők. (Collabb skinek kivételével)')
+        .setImage('https://images-ext-1.discordapp.net/external/S6jq9ozaEmC5g23lQYLAF4st8ypOZSCcQ6j1o4NUyWE/https/pbs.twimg.com/media/FEKT2WdaMAANTdm.png%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
