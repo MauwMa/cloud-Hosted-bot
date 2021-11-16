384bd1fb21e6d4fc43193458b0b91e34c3120be9
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-16 12:07:57 +0100

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,15 @@
-module.exports = () =>{
-    console.log('Akashi online');
+//module.exports = () =>{
+//    console.log('Akashi online');
+//}
+
+
+module.exports = async (client) => {
+    client.Ready = true, 
+    client.user.setPresence({
+      status: "online",  // You can show online, idle, and dnd
+      activity: {
+          name: "Azur Lane",  // The message shown
+          type: "PLAYING", // PLAYING, WATCHING, LISTENING, STREAMING,
+      }
+  });
 }
