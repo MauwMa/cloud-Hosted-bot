1f474faaf90d7206b48f89fc3603c81a2e255fe1
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-16 12:21:22 +0100

    Update Node.js

--- Node.js
+++ Node.js
@@ -19,16 +19,16 @@ client.on("message", (message) => {
 }}
 );
 
-module.exports = async (client) => {
-    client.Ready = true, 
+client.on("ready", () =>{
     client.user.setPresence({
       status: "online",  // You can show online, idle, and dnd
       activity: {
           name: "Azur Lane",  // The message shown
           type: "PLAYING", // PLAYING, WATCHING, LISTENING, STREAMING,
       }
-  });
-  }
+  })
+});
+
 
 client.on('guildMemberAdd', guildMember =>{
         const udvozloembed = new Discord.MessageEmbed()
