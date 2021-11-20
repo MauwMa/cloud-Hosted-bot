f9e197871b247033bc2bb770f15b15ee50e89cdc
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-20 19:55:48 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('★Azur Lane | SSSS.GRIDMAN / SSSS.DYNAZENON Collaboration★')
+        .setTitle('★Rikka Takarada★')
         .setURL('')
         .setTimestamp()
-        .setDescription('Megünneplésképp, a Főhadiszállás különleges ajándékot oszt ki, ami tartalmaz 2.000<:Olaj:790201521593122816>, 4.000<:Pnz:790201580812894218>, 50 cognitive arrayt és egyéb dolgokat a parancsnokoknak!')
-        .setImage('https://images-ext-2.discordapp.net/external/LgIfXJivgMItT0KH2ziIbHy-bkmfgeW-IzVVf6QDEK4/https/pbs.twimg.com/media/FEo43q3XsAUuQ8e.jpg%3Alarge?width=1202&height=676')
+        .setDescription('Rikka Takarada felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/KQ4KOJ0kYzh4ILHiIU_rTGks2_gr9KjZ8c8A90ZUuhc/https/pbs.twimg.com/media/FEo6RMcWUAMtRLF.jpg%3Alarge?width=1352&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
