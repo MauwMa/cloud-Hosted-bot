2388cb47189bfd4406e04a1fa181e7b6e444ea0a
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-22 17:29:58 +0100

    Update azurtalk.js

--- commands/azurtalk.js
+++ commands/azurtalk.js
@@ -9,10 +9,16 @@ module.exports = {
         .setURL('')
         .setTimestamp()
         .setDescription('AzurTalk Szombaton!!! Délután kinek mikor lenne jó?')
-        client.channels.cache.find(channel => channel.id === "791956226521956394").send(azurtalk)
+        client.channels.cache.find(channel => channel.id === "856904370595561473").send(azurtalk)
            .then ((msg) => {setTimeout(function(){
            msg.react('1️⃣')
            msg.react('2️⃣')
+           msg.react('3️⃣')
+           msg.react('4️⃣')
+           msg.react('5️⃣')
+           msg.react('6️⃣')
+           msg.react('7️⃣')
+           msg.react('8️⃣')
         }, 1000)})
         
 //if (message.deletable) message.delete();
