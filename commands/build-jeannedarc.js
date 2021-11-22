caff046fe562392c21fc455b9b4e9081dc9f8186
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-22 20:13:31 +0100

    Update build-jeannedarc.js

--- commands/build-jeannedarc.js
+++ commands/build-jeannedarc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    name: 'build-jeanne',
-    description: 'build-jeanne',
+    name: 'build-jeannedarc',
+    description: 'build-jeannedarc',
     permissions: ["SEND_MESSAGES"],
     execute (client, message, args, Discord){
-        const jeanne = new Discord.MessageEmbed()
+        const jeannedarc = new Discord.MessageEmbed()
         .setColor('#16108b')
         .setTitle('Jeanne dArc Build')
         .setURL('')
         .setDescription('Offtank 1 | Helena Fight | Offtank 2 | Budget')
         .setImage('https://i.imgur.com/9VXUYx3.png')
         .setTimestamp()
-        message.channel.send(jeanne);
+        message.channel.send(jeannedarc);
 
     }    
 }
