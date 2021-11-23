2692164db09dfe4aa31d51b898a8b140cad41696
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-23 16:02:43 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('★Vakáció kettőnek★')
+        .setTitle('★Utcán nevelkedett kisasszony★')
         .setURL('')
         .setTimestamp()
-        .setDescription('Üdv újra itthon, Parancsnok. Jó sok cuccot hoztál nekem, mi? Akkor hát, ne csak álldogálj ott, siess és gyere ide hozzám~\n\nAkane Shinjo az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/YZO1LiSXKeLOwTFMC_grafj43r3eD-qF-wyzuIoTl1I/https/pbs.twimg.com/media/FEy39G3VQAses4L.jpg%3Alarge?width=1239&height=551')
+        .setDescription('Wóó, itt vagy! Szóval, milyen volt? Ezt a találkozóhelyet elég könnyű megtalálni, igaz? Nos, akkor induljunk meg neki!\n\nNamiko az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/a_eSSwdeIoVbH28y2rwGppe8vzc_AxDve3W5bhtScK8/https/pbs.twimg.com/media/FE3y8bFUUAkdB56.jpg%3Alarge?width=1239&height=551')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
