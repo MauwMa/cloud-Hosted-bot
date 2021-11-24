3c2666df1f9d8731a196343e4833746a8760fce3
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-24 08:43:12 +0100

    dd

--- commands/tesst.js
+++ commands/tesst.js
@@ -2,13 +2,16 @@ module.exports = {
     name: 'tesst',
     description: 'tesst',
     permissions: ["KICK_MEMBERS"],
-    execute (client, message, args){
-
-        message.channel.send("Stream végével a szoba is le lett zárva. Az event kezdésekor (11/25) a szoba törlésre kerül nyaaa~")
-
-    if (message.deletable) message.delete();
+        execute (client, message, args, Discord){
+            const tesst = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle('Merch')
+            .setURL('')
+            .setTimestamp()
+            .setDescription('Parancsnokok\n\n Ezek az imádni való desszert-témájú plüssek Le Malin és Perseusról November 25.-én magyar időszámítás szerint 05:00-kor lesznek elérhetőek\n\nBolt link: https://t.co/fdl58JDrlB\n\nEmail: store.cs@yo-star.com')
+            .setImage({files: ["twitter.mp4"]})
+            message.channel.send(tesst);
 
     }    
 }
-
 
--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('★Álmodás a nyitott ablakpárkányon★')
+        .setTitle('Merch')
         .setURL('')
         .setTimestamp()
-        .setDescription('Milyen csodás. Innen látható az egész kikötő pont ahogy a csillogó óceán... Óh Parancsnok, itt vagy hogy élvezd ezt a kilátást velem?\n\nYume Minami az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/p-wOLXwJvpgsFh-_Q4oQI8ZN2kU-oTqbtBrobWRt_AA/https/pbs.twimg.com/media/FE3zZfDVUAQxOpL.jpg%3Alarge?width=1239&height=551')
+        .setDescription('Parancsnokok\n\n Ezek az imádni való desszert-témájú plüssek Le Malin és Perseusról November 25.-én magyar időszámítás szerint 05:00-kor lesznek elérhetőek\n\nBolt link: https://t.co/fdl58JDrlB\n\nEmail: store.cs@yo-star.com')
+        .setImage({files: ["twitter.mp4"]})
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
