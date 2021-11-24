57f5123ae817a1b559e9665c043172fe27d7f1a9
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-24 08:46:49 +0100

    d

--- commands/update.js
+++ commands/update.js
@@ -9,7 +9,7 @@ module.exports = {
         .setURL('')
         .setTimestamp()
         .setDescription('Parancsnokok\n\n Ezek az imádni való desszert-témájú plüssek Le Malin és Perseusról November 25.-én magyar időszámítás szerint 05:00-kor lesznek elérhetőek\n\nBolt link: https://t.co/fdl58JDrlB\n\nEmail: store.cs@yo-star.com')
-        .setImage({files: ["twitter.mp4"]})
+        .setImage('')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
