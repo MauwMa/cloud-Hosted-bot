e6330d75fa51ec3f24809ec8260e14e5f4c38397
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-26 08:23:32 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('Black Friday')
+        .setTitle('Hálaadás')
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok, a Black Friday megérkezett a Yostar boltba is 10%-os kedvezmény 100$ feletti rendelésre az ÖSSZES merchre belevéve az újakra is! Ha be akarod zsebelni őket, akkor ne hagyd ki~\n\nBolt Link: https://t.co/yaYKZ998xa')
-        .setImage('https://i.imgur.com/Yqo9L91.jpg')
+        .setDescription('Parancsnokok, az Azur Lane 2021 Hálaadás merch elérhető lett. Látogass el a boltba, hogy megnézd őket. Emellet, 10% kedvezmény azokra a rendelésekre, amelyek 100$ felett vannak!\n\nBolt Link: https://t.co/fdl58JV2d9\n\nEmail: store.cs@yo-star.com')
+        .setImage('https://images-ext-1.discordapp.net/external/BO9dDXPlmNLBiX8VFlIIetWGEgPBM_t-jV_OeTMzvdc/%3Fformat%3Dpng%26name%3D386x202/https/pbs.twimg.com/card_img/1463337155200262146/xtb9XNf3')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
