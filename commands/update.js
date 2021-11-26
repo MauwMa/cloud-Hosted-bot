c5e4b06332dd94853e2788d5b0547f94b55910e0
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-26 08:18:53 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('Boldog Hálaadást')
+        .setTitle('Black Friday')
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok, Boldog Hálaadást! Reméljük jól telik az ünneplés~')
-        .setImage('https://images-ext-1.discordapp.net/external/3EvYYuZHQ04vCl1KIhMn94dVRLuGZmEID37zQBbKO4Q/https/pbs.twimg.com/media/FFDpuiVVUAcBE-t.jpg%3Alarge?width=1202&height=676')
+        .setDescription('Parancsnokok, a Black Friday megérkezett a Yostar boltba is 10%-os kedvezmény 100$ feletti rendelésre az ÖSSZES merchre belevéve az újakra is! Ha be akarod zsebelni őket, akkor ne hagyd ki~\n\nBolt Link: https://t.co/yaYKZ998xa')
+        .setImage('https://i.imgur.com/Yqo9L91.jpg')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
