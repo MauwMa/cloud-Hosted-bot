75cb46e030676b5f0d8ec0647f5f74d23a5adc6c
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-11-29 12:34:52 +0100

    sss

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('Hálaadás')
+        .setTitle('Emlékeztető')
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok, az Azur Lane 2021 Hálaadás merch elérhető lett. Látogass el a boltba, hogy megnézd őket. Emellet, 10% kedvezmény azokra a rendelésekre, amelyek 100$ felett vannak!\n\nBolt Link: https://t.co/fdl58JV2d9\n\nEmail: store.cs@yo-star.com')
-        .setImage('https://images-ext-1.discordapp.net/external/BO9dDXPlmNLBiX8VFlIIetWGEgPBM_t-jV_OeTMzvdc/%3Fformat%3Dpng%26name%3D386x202/https/pbs.twimg.com/card_img/1463337155200262146/xtb9XNf3')
+        .setDescription('Ezek a limnitált skinek az Auzr Lane | SSSS.GRIDMAN / SSSS.DYNAZENON Collabból magyar időszámítás szerint 12/9 7:59-ig lesznek elérhetőek.')
+        .setImage('https://images-ext-1.discordapp.net/external/gBHlq1hulHLjrGxYOK_VfhSjdYvInu0r44wvoqXsiFw/https/pbs.twimg.com/media/FFRn0ndaQAIbi_M.jpg%3Alarge?width=1847&height=910')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
