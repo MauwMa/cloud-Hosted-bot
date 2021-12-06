5927bc79b9a8426d660103ceb57c42a880f77f45
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-06 10:27:35 +0100

    Update karban.js

--- commands/karban.js
+++ commands/karban.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('#ffff00')
         .setTitle('Karbantartás!!')
         .setURL('')
-        .setDescription('Parancsnokok, 11/25 Csütörtökön Magyar Időszámítás szerint \nreggel 08:00-kor lesz karbantartás. Kérjük a beosztásaitokat tervezzétek e szerint nyaaa~')
+        .setDescription('Parancsnokok, 12/09 Csütörtökön Magyar Időszámítás szerint \nreggel 08:00-kor lesz karbantartás. Kérjük a beosztásaitokat tervezzétek e szerint nyaaa~')
         .setImage('https://images-ext-2.discordapp.net/external/PGLOP1lyRoOuSu0_VOVLG4pDYnNQRtj_iBvYnSzdF30/https/pbs.twimg.com/media/FE21HPTVUAEzk36.jpg%3Alarge?width=965&height=551')
         .setTimestamp()
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(karban);
