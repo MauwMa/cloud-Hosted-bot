c4f68dc5eaa05f27fe6c14af03b8092aea748002
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-14 17:14:41 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('★Esti Kikapcsolódás★')
+        .setTitle('【Scherzo of Iron and Blood】')
         .setURL('')
         .setTimestamp()
-        .setDescription('Tekintetedre és a folyamatos győzelmeidre, Parancsnok. Hehe, ma este rumos kedvembe vagyok. Csatlakozol hozzám egy ideig?\n\nUSS New Orleans az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/pAmQeSCoeMrqVr654uzM4G5ByWccefbuHjT6YQU5oBE/https/pbs.twimg.com/media/FGQosFhUUAE5oqJ.jpg%3Alarge?width=1010&height=551')
+        .setDescription('Scherzo of Iron and Blood event véglegesen bekelrül a War Archives-ba a következő karbantartáskor.')
+        .setImage('https://images-ext-2.discordapp.net/external/JWdn3--G17JOPftDnUiB_343Ie137w2S7LDGvNoUBrI/https/pbs.twimg.com/media/FGib978VIAAk5J4.jpg%3Alarge?width=1110&height=550')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
