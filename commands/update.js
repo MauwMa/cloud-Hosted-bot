3627392503b0b43a07913c4a623d9a7bf13c2639
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-17 08:35:01 +0100

    up

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle("【Today's Debut✩Tomorrow's Star! Rerun】")
+        .setTitle("Emlékeztető")
         .setURL('')
         .setTimestamp()
-        .setDescription(`Li'l Sandy vissza fog térni a "Today's Debut✩Tomorrow's Star!" event Rerunjával. Szerezz elegendő "Friendship Point"-ot, hogy ténylegesen csatlakozzon Li'l Sandy hozzád!`)
-        .setImage('https://images-ext-1.discordapp.net/external/ARt9Fr33Xv0eUxRPhyOAWJUB7sLvZGbIeRtEKJ4ImSM/https/pbs.twimg.com/media/FGpVcecVkAI2L8x.png%3Alarge')
+        .setDescription(`Parancsnok, a Scherzo of Iron and Blood event mostantól elérhető a war archivesban. Bismarck végre visszatért, hogy irányítsa az Iron Blood-ot!\n\nFanart: 中西 達哉 - (@tayatatsu)`)
+        .setImage('https://images-ext-2.discordapp.net/external/6un64lgjxe9Wl16Db4VcsAvs9Cx7_tVb2b0XHIUU8o4/https/pbs.twimg.com/media/FGt0w64VcAIb4ym.jpg%3Alarge?width=974&height=548')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
