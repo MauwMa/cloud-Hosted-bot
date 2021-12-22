316049e89246e0a069fadacc552a58e55d681aa1
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-22 08:52:18 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle("Emlékeztető")
+        .setTitle("【2021 Popularity Poll Finals】")
         .setURL('')
         .setTimestamp()
-        .setDescription(`Parancsnok, a Scherzo of Iron and Blood event mostantól elérhető a war archivesban. Bismarck végre visszatért, hogy irányítsa az Iron Blood-ot!\n\nFanart: 中西 達哉 - (@tayatatsu)`)
-        .setImage('https://images-ext-2.discordapp.net/external/6un64lgjxe9Wl16Db4VcsAvs9Cx7_tVb2b0XHIUU8o4/https/pbs.twimg.com/media/FGt0w64VcAIb4ym.jpg%3Alarge?width=974&height=548')
+        .setDescription("Jelenleg a 2021 Népszerűség Szavazás Utolsó Fordulója zajlik! Hogy megünnepeljük ezt a különleges alkalmat, HQ ajándékkal készült~ Ne felejtsetek szavazni a kedvenc shipgirlötökre az event végéig December 24. Magyar ideőszámítás szerint 7:59-ig.")
+        .setImage('https://images-ext-2.discordapp.net/external/SNIZJCWoNyWMEQzi1gDyTfvqGJsMQeQB4QP0KpGHi1Q/https/pbs.twimg.com/media/FHIzEu6UYAM25h0.jpg%3Alarge?width=1005&height=548')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
