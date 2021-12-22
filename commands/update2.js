548cf705d9ef5d416423a05f5119e4d63d24771f
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-22 08:55:34 +0100

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('★A Lehetőség Hátszele★')
+        .setTitle('【Call to Arms: Iron Blood】')
         .setURL('')
         .setTimestamp()
-        .setDescription('Csak meg kell etetni egy érmével és mozgatni a kampót a karral... Óh! Elkaptam egyet! Ez könnyebb volt, mint gondoltam.!\n\nUSS Independence az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')        
-        .setImage('https://images-ext-2.discordapp.net/external/u2z6gpXEqvW2JAxlhtwRvJj4xWlmhq_exzJYY3lwV4g/https/pbs.twimg.com/media/FE96LPwVQAwJRJq.jpg%3Alarge?width=1113&height=551')
+        .setDescription('Call to Arms: Iron Blood, egy előevent, a következő karbantartás után kerül be. Szerezz extra tapasztalatot Prinz Eugen, Peter Strasser, Tirpitz és U-47-el, hogy felkészülj a nagy eventre.')        
+        .setImage('https://images-ext-2.discordapp.net/external/g4S-dj_1nVgxyeAqNJ7s7XyrXgZ87YnmvmGT2MhrGO4/https/pbs.twimg.com/media/FHILcCEVQAMtKaD.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
