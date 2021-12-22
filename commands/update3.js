598705979a4206d68422173b987d1269cc222e52
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-22 08:56:50 +0100

    Update update3.js

--- commands/update3.js
+++ commands/update3.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update3 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('★A Kék Ég Állomás★')
+        .setTitle('✠Ulrich von Hutten✠')
         .setURL('')
         .setTimestamp()
-        .setDescription('A vonat még mindig nem ért ide? ...Oh, milyen véletlen hogy itt látlak, Parancsnok. Hova indultál?\n\nRikka Takarada az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/-xw4uTGRAKbZXOTR0e302D5dnFqYQAQI1GVtChnXWpA/https/pbs.twimg.com/media/FE9mrw4UYAgSpqT.jpg%3Alarge?width=1239&height=551')
+        .setDescription('KMS Ulrich von Hutten felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/Y_stpBTgjs_VhrLBGWqjicIGarfb_eo5Clj6PfZSvpg/https/pbs.twimg.com/media/FHITrzEUUAcE5IQ.jpg%3Alarge?width=1047&height=548')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update3);
         if (update3){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
