f4f3106e2367fbda4c866d113894325f118aa063
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-22 09:00:04 +0100

    Update update3.js

--- commands/update3.js
+++ commands/update3.js
@@ -8,7 +8,7 @@ module.exports = {
         .setTitle('✠Ulrich von Hutten✠')
         .setURL('')
         .setTimestamp()
-        .setDescription('KMS Ulrich von Hutten felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setDescription('Ulrich von Hutten vagyok, az Iron Blood remény és megbánás szimbóluma. Az érkezésem ünneplést vagy kétségbeesést okoz, ezt neked kell eldöntened. Szívesen fogok válaszolni rá.\n\nKMS Ulrich von Hutten felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
         .setImage('https://images-ext-1.discordapp.net/external/Y_stpBTgjs_VhrLBGWqjicIGarfb_eo5Clj6PfZSvpg/https/pbs.twimg.com/media/FHITrzEUUAcE5IQ.jpg%3Alarge?width=1047&height=548')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update3);
         if (update3){
