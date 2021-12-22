2c052dca02b52a6ca942a142c0db5d4169248d18
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-22 17:18:18 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle("【2021 Popularity Poll Finals】")
+        .setTitle("❀Eseménytelen ünnepi üdvözlet❀")
         .setURL('')
         .setTimestamp()
-        .setDescription("Jelenleg a 2021 Népszerűség Szavazás Utolsó Fordulója zajlik! Hogy megünnepeljük ezt a különleges alkalmat, HQ ajándékkal készült~ Ne felejtsetek szavazni a kedvenc shipgirlötökre az event végéig December 24. Magyar ideőszámítás szerint 7:59-ig.")
-        .setImage('https://images-ext-2.discordapp.net/external/SNIZJCWoNyWMEQzi1gDyTfvqGJsMQeQB4QP0KpGHi1Q/https/pbs.twimg.com/media/FHIzEu6UYAM25h0.jpg%3Alarge?width=1005&height=548')
+        .setDescription("IJN Suruga az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.")
+        .setImage('https://i.imgur.com/cCrzi2B.jpg')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
