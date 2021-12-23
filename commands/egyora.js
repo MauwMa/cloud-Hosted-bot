e5367d4602d51d7dfd53fd2a98caa3ee18e239cd
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-23 11:35:01 +0100

    dsa

--- commands/egyora.js
+++ commands/egyora.js
@@ -4,12 +4,12 @@ module.exports = {
     permissions: ["KICK_MEMBERS"],
     description: 'egyora',
     execute (client, message, args, Discord){
-        var exportScheduler = schedule.scheduleJob('0 35 6 09 12 *', function() { 
+        var exportScheduler = schedule.scheduleJob('0 0 6 30 12 *', function() { 
             const egyora = new Discord.MessageEmbed()
             .setColor('#ffff00')
             .setTitle('Értesítés!')
             .setURL('')
-            .setDescription('25 perces előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 6 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
+            .setDescription('1 órás előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 6 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
             .setImage('https://images-ext-2.discordapp.net/external/PGLOP1lyRoOuSu0_VOVLG4pDYnNQRtj_iBvYnSzdF30/https/pbs.twimg.com/media/FE21HPTVUAEzk36.jpg%3Alarge?width=965&height=551')
             .setTimestamp()
             client.channels.cache.find(channel => channel.id === "789936292413571102").send(egyora); 
--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle("❀Eseménytelen ünnepi üdvözlet❀")
+        .setTitle("❀Mikulás Eltévedt Segéde!❀")
         .setURL('')
         .setTimestamp()
-        .setDescription("IJN Suruga az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.")
-        .setImage('https://i.imgur.com/cCrzi2B.jpg')
+        .setDescription("Vigyázz! Félre, Parancsnok! ...Fuh. Hogy megmagyarázzam, segítek a Mikulásnak kiosztani a karácsonyi ajándékokat! Most ha megbocsájtasz, nem húzhatom az időt vagy elkések... Várj, rossz helyre jöttem? MICSODA?!\n\nIJN Akatsuki új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.")
+        .setImage('https://images-ext-2.discordapp.net/external/m9j0iwAdVqoHG8Y07o2n6MTVDc0eMkOWIXwrox25yn0/https/pbs.twimg.com/media/FHMzWVaVkAA9Ffe.jpg%3Alarge?width=1005&height=548')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
