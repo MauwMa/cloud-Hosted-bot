52318bfc05996f97c41cb13b087a1915f9226cbe
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-23 14:44:05 +0100

    dd

--- commands/karerr.js
+++ commands/karerr.js
@@ -7,8 +7,8 @@ module.exports = {
         .setColor('#ffff00')
         .setTitle('Karbantartás!!')
         .setURL('')
-        .setDescription('A főhadiszállás egy váratlan problémába ütközött a karbantartás miatt és emiatt 2 órával meghosszabbítjuk a karbantartást. Bármilyen változás fog történni azt meg fogjuk osztani.\n\nKöszönjük a megértéseteket!')
-        .setImage('https://i.imgur.com/0BEHNji.png')
+        .setDescription('A főhadiszállás egy váratlan problémába ütközött a karbantartás miatt és emiatt határozatlan időre meghosszabbítjuk a karbantartást. Bármilyen változás fog történni azt meg fogjuk osztani.\n\nKöszönjük a megértéseteket!')
+        .setImage('https://images-ext-2.discordapp.net/external/PGLOP1lyRoOuSu0_VOVLG4pDYnNQRtj_iBvYnSzdF30/https/pbs.twimg.com/media/FE21HPTVUAEzk36.jpg%3Alarge?width=965&height=551')
         .setTimestamp()
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(karerr);
         if (karerr){
