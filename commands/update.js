98668b8b49a41927d90e16f57134090de9028743
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-26 13:53:50 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle("❀Vágás, Szeletelés és Felszolgálás❀")
+        .setTitle("✠Vas-Fekete Elízium✠")
         .setURL('')
         .setTimestamp()
-        .setDescription("Okéska, egy frissen vágott Sashimi azonnal jön! ...Huh? Túl agresszívan közlekedem? Annak ellenére hogy min mentem keresztül miattad, így is panaszkodsz? ...Nos, mindegy. Jó étvágyat!\n\nIJN Hakuryuu új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.")
-        .setImage('https://images-ext-1.discordapp.net/external/NlQQ0s1QFZoJCfUJpDsymhiuwb8WYENORln8a8YIcQE/https/pbs.twimg.com/media/FHY-ALiVgAMeUmu.jpg%3Alarge?width=1171&height=676')
+        .setDescription("Üdvözöllek, mit szeretnél rendelni? ...Ha szeretnéd, hanyagolhatjuk a színjátékot. Amúgy sem vagyok hozzászokva a pincérkedéshez, szóval jobban preferálom így. Hehe.\n\nKMS Bismarck új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.")
+        .setImage('https://images-ext-2.discordapp.net/external/DSpLTCPESJMvIz7H6U5PUjXqENkzz1xk1PQ7ZMbqdgw/https/pbs.twimg.com/media/FHchJCFacAAjkJ7.jpg%3Alarge?width=950&height=548')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
