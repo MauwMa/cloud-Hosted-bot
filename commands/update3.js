33f6aaa8e857077d5d698a4f02a171149c1a46c0
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-27 18:41:45 +0100

    Update update3.js

--- commands/update3.js
+++ commands/update3.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update3 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('❀Őrizdmeg a hidegvéred❀')
+        .setTitle('♚Bunkó Cseléd♚')
         .setURL('')
         .setTimestamp()
-        .setDescription('Üdvöz...?! Mit keresel itt? Várj, ne menj- Csak meglepődtem, ez minden... Nos, mit szeretnél rendelni?\n\nIJN Noshiro új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/IFr7CtMBF6ARbcMTTJrui3Nws4rPz3TqGrgkksvuwC4/https/pbs.twimg.com/media/FHhHr1jUcAILo88.jpg%3Alarge?width=950&height=548')
+        .setDescription('Üdv, üdv. Cselédként dolgozok itt, mint ahogyan mondtad nekem. Öhm... Hívj ha már eldöntötted a rendelésed.\n\nHMS Penelope új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/ggfGTvDv0jXfM_39UhxvCuNkbPpWZhyIxyvAR6cZsU8/https/pbs.twimg.com/media/FHlWs3AVcAMGw03.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update3);
         if (update3){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
