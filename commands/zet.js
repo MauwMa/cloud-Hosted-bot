60e82d082e19a14d8bcecddaf164661592d401a0
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-27 19:17:33 +0100

    Update zet.js

--- commands/zet.js
+++ commands/zet.js
@@ -4,7 +4,7 @@ module.exports = {
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args){
 
-        message.channel.send('Az új Iron Blood főesemény, "Tower of Transcendence", December 30. fog kezdődni\n új Ultra Rare csatahajóval, Ulrich Von Hutten és még másokkal!\nhttps://youtu.be/82Rwtv9F1Zg')
+        message.channel.send('Az Iron Blood főesemény, "Tower of Transcendence", December 30. fog kezdődni\núj Ultra Rare csatahajóval, Ulrich Von Hutten-nel és még másokkal!\nhttps://youtu.be/82Rwtv9F1Zg')
 
     if (message.deletable) message.delete();
 
