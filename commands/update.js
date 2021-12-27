3eed195ab6e3ae3888948db6d82552f8e6826337
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-27 09:08:43 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle("✠A Kosz Háború Krónikák✠")
+        .setTitle("✠Cica Kávézó Kitakarítása✠")
         .setURL('')
         .setTimestamp()
-        .setDescription("Hegyezd szemed eme kis koszra. Amennyire biztos voltam a takarításban, nem hagyhatom ezt a koszt a helyén. Csatlakozol hozzám ebben a csatában?\n\nKMS Z46 új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.")
-        .setImage('https://images-ext-1.discordapp.net/external/bZtagAG6_fFpBM8tve0f7P-YexnWASXsgEETAOjwkaY/https/pbs.twimg.com/media/FHciXj7VIAU2eyY.jpg%3Alarge?width=950&height=548')
+        .setDescription("Te kis...! Nézd milyen rumlit csináltál! És én vagyok az, akinek fel kell takarítania! ...Óh. Tökéletes időzítés, Parancsnok. Tudnák használni egy segítőkezet a padlóhoz, azután amit ez a kis szar csinált.\n\nKMS Magdeburg új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.")
+        .setImage('https://images-ext-2.discordapp.net/external/Bj_F-RtYO0GiYIf4vAbV3AMbQsa1bzASMYoS967zZOo/https/pbs.twimg.com/media/FHhIZ8jUcAMPM83.jpg%3Alarge?width=950&height=548')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
