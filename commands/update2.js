a7a973aab0832e507aa1c7a121cc53a319f48ce7
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-27 19:13:53 +0100

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -5,11 +5,11 @@ module.exports = {
     execute (client, message, args, Discord){
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('✠Cseléd csak Meked✠')
+        .setTitle('✠Kirakati Edzés✠')
         .setURL('')
         .setTimestamp()
-        .setDescription(`Üdvözöllek! Ó, Hello, Parancsnok! Heheh, szóval hogyan tetszik a cseléd ruha? Szépen néz ki, ha negem kérdezel!\n\nKMS U-1206 új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
-        .setImage('https://images-ext-2.discordapp.net/external/rykxunDNWiD3B3pvGlqaMcoGAJZGDfjTWmpLFlxpiW0/https/pbs.twimg.com/media/FHhJIfAVEAAXXMm.jpg%3Alarge')
+        .setDescription(`Parancsnok, ah, öhm... Rengeteg új dolog van a menünkön és limitált ideig elérhetőek csak... K-Kérlek adj nekik egy esélyt! Fuuu... H-Hála Istennek, sikerült dadogás nélkül elmondani...\n\nKMS Leipzig új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
+        .setImage('https://images-ext-1.discordapp.net/external/Lc5SUefOQ3zWQMmEmnUO8f9dmn0LyDnl1Ny_4X8_3rQ/https/pbs.twimg.com/media/FHn3dfpUcAImpCO.jpg%3Alarge?width=1171&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
