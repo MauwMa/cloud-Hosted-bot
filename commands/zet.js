8a12e164b305985514e359b8af99d4584c0f6f51
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-29 15:47:59 +0100

    Update zet.js

--- commands/zet.js
+++ commands/zet.js
@@ -1,9 +1,16 @@
 module.exports = {
     name: 'zet',
     description: 'zet',
-    permissions: ["SEND_MESSAGES"],
+    permissions: ["KICK_MEMBERS"],
     execute (client, message, args, Discord){
-        message.channel.send({files: ["hms.mp4"]});
+        const zet = new Discord.MessageEmbed()
+        .setColor('#2ecc71')
+        .setAuthor({ name: 'Azur Lane Hungary' , iconURL: 'https://i.imgur.com/6DgcVUu.png'})
+        .setTitle('teszt teszt teszt teszt ')
+        .setTimestamp()
+        .setURL('')
+        .setDescription('teszt teszt teszt teszt teszt teszt ')
+        message.channel.send(zet);
 
         if (message.deletable) message.delete();
     }    
