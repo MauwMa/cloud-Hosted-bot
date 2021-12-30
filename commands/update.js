a817f4ff602ef9ce756b2e64c81347456ad2e519
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-30 17:07:14 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("♚Igenis, Őcselédsége♚")
+        .setTitle("Emlékeztető")
         .setURL('')
         .setTimestamp()
-        .setDescription('Warspite, mi az isten minden ez... A Cseléd Csapatnak át kell nyálaznia magát mindezen a papírmunkán? Már el is fáradtam... bárcsak sosem mondtam volna, hogy meg akarom tapasztalni milyen cselédnek lenni... És te, hagyd abba a bámulást és segíts végre, szolgám!\n\nHMS Queen Elizabeth az új L2D ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok..')
-        .setImage('https://images-ext-2.discordapp.net/external/V90XV2fe4-d0pyWbuDA027dWAO3VV2oTWP8RqhyrixQ/https/pbs.twimg.com/media/FHsMuRaUcAECcpK.jpg%3Alarge')
+        .setDescription('Parancsnokok,\n\n Ez a nyolc limitált skinek, amelyek 3 napra tértek vissza 01/01 magyar időszámítás szerint 7:59-ig lesznek még elérhetőek. Ne hagyjátok ki az esélyt, hogy megszerezzétek őket~')
+        .setImage('https://images-ext-2.discordapp.net/external/Ep-4ryedliH8Vcp2OrjyqioHbx5alaMbRCY9x5XY4uQ/https/pbs.twimg.com/media/FHybpNXVEAI_0US.jpg%3Alarge?width=1440&height=646')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
