bb54b413ea0012053ab39ce3018a41af5e1364b5
MauwMa <43650202+MauwMa@users.noreply.github.com>
2021-12-31 09:58:13 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -9,8 +9,8 @@ module.exports = {
         .setTitle("Emlékeztető")
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok,\n\n Ez a nyolc limitált skinek, amelyek 3 napra tértek vissza 01/01 magyar időszámítás szerint 7:59-ig lesznek még elérhetőek. Ne hagyjátok ki az esélyt, hogy megszerezzétek őket~')
-        .setImage('https://images-ext-2.discordapp.net/external/Ep-4ryedliH8Vcp2OrjyqioHbx5alaMbRCY9x5XY4uQ/https/pbs.twimg.com/media/FHybpNXVEAI_0US.jpg%3Alarge?width=1440&height=646')
+        .setDescription('Parancsnokok,\n\n A Tower of Transcendence event jelenleg megy! Ezek a limitált karakterek az event építésből elérhetőek egészen 1/13/2022 magyar időszámítás szerint 7:59-ig.')
+        .setImage('https://images-ext-1.discordapp.net/external/H7kTB9nyHgQK6NPCCTH3iw2kNdDom3bQCid8nzxMs8I/https/pbs.twimg.com/media/FH2X238VUAE4jgM.jpg%3Alarge?width=1069&height=548')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
