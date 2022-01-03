91825e4284577d02f1de16691d6707fb058c73ee
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-01-03 19:02:57 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -9,8 +9,8 @@ module.exports = {
         .setTitle("")
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok,\n\n Boldog Új Évet! Z46 és Friedrich der Große megérkeztek, hogy velünk ünnepeljék meg az Új Évet!\n\nFanart: 薔薇缶 - (@canned_rose)')
-        .setImage('https://images-ext-2.discordapp.net/external/sbvOCpoAB0tmaIn1HyLDf6gcIvubvNWgiezM_PuHFzQ/https/pbs.twimg.com/media/FH8G-3IVQAMkJSJ.jpg%3Alarge?width=974&height=548')
+        .setDescription('Parancsnokok,\n\n az Azur Lane hivatalos Twitter oldala elérte a 270k követőt! New Jersey itt van, hogy velünk együtt ünnepelje meg ezt a mérföldkövet~\n\nFanart: hanusu - (Pixiv=2488853)')
+        .setImage('https://images-ext-2.discordapp.net/external/7mXmUwyGA3o3_tInH89IU56kHQpxVfQ6HiQmaVMzAKk/https/pbs.twimg.com/media/FH7GchvVkAAJ082.jpg%3Alarge?width=1618&height=910')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
