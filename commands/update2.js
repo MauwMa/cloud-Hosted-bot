bd388ff6b39f7e3ca7e04e1c4dd566d4a06ff5ef
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-01-19 18:40:59 +0100

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -6,11 +6,11 @@ module.exports = {
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('★Tavaszi Ihlet★')
+        .setTitle('♚Charybdis♚')
         .setURL('')
         .setTimestamp()
-        .setDescription(`Ezt nézd meg, Parancsnok! Hehehe, hozzáadtam egy tüzijáték kilövőt a szuperhős felszerelésemhez. Menő, mi? ...Jobban érdeklődsz az új kinézetem után? De,,, a tűzijáték...\n\nUSS Reno új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')`)        
-        .setImage('https://images-ext-2.discordapp.net/external/OMdBgYpthh7xcjAu_g5JUaOZ6U6WrU-imra1BMfUP1c/https/pbs.twimg.com/media/FJXyThAacAA5A9C.jpg%3Alarge?width=1034&height=548')
+        .setDescription(`Dido-osztályú könnyű cirkáló, Charybdis a szolgálatodra. Megtiszteltetés személyesen találkozni veled, Mester. Meleg napfény járja át a mindennapi életed, a békés napokban amik jönnek. \n\nHMS Charybdis felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.`)        
+        .setImage('https://images-ext-2.discordapp.net/external/DGO1tB8_74gDh1_yLnMXHoBKgMRcbKEu-VVy0LVA-L4/https/pbs.twimg.com/media/FJeA8NbakAEG1X1.jpg%3Alarge?width=1047&height=548')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
