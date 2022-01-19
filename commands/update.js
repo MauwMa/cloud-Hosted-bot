6286578e30740be63d17850d53a0e0b32c51cc56
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-01-19 18:35:39 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("❀Virágzó Tavasz, Fénylő Hold❀")
+        .setTitle("❀Firecrackers and Steamed Buns❀")
         .setURL('')
         .setTimestamp()
-        .setDescription('A Lunar Új Évre milyen virágokkal kellene feldíszítenem az irodát? Ááh, Nem tudom eldönteni! Öhm, Parancsnok, átnézed belülről? Körbe vezetlek! Fuuh...\n\nIJN Hanazuki új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/ICsGflBm-vd732TaCiZD0BPUFss7tvyj46WO03QEp2A/https/pbs.twimg.com/media/FJT92K-acAA0bjJ.jpg%3Alarge?width=1034&height=548')
+        .setDescription('Mmh... Boldog Új Évet. Azon vagyok, hogy a petárdákat a barátaimhoz vigyem... A buci amit eszek? Ezt a erőfeszítéseimért kaptam köszönetképp.\n\nIJN Yoizuki új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/iEUk0Ekl1HHPEWpXUi-MI4Rg_68LK-LNqrIzvSXQOpU/https/pbs.twimg.com/media/FJXylblaUAIgLr7.jpg%3Alarge?width=1034&height=548')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
