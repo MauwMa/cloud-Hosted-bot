654d661fba5d3f4be4e5c513087230f78e9e93da
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-01-20 21:21:02 +0100

    dd

--- commands/update2.js
+++ commands/update2.js
@@ -10,7 +10,7 @@ module.exports = {
         .setURL('')
         .setTimestamp()
         .setDescription(`Boldog Új Évet, Parancsnok. Hogy megünnepeljük ezt a szezont, készítettem egy szabott ruhát magamnak, amely tradicionális Dargon Empery ruhákat használ alapnak. Nem nézek ki... gyerekesen, ugye?\n\nIJN Oite felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.`)        
-        .setImage('https://images-ext-2.discordapp.net/external/DGO1tB8_74gDh1_yLnMXHoBKgMRcbKEu-VVy0LVA-L4/https/pbs.twimg.com/media/FJeA8NbakAEG1X1.jpg%3Alarge?width=1047&height=548')
+        .setImage('https://images-ext-1.discordapp.net/external/-FuPs5w9i_Za70fFhto-SVKIt51_uGiWGPiAzir4HTQ/https/pbs.twimg.com/media/FJeBTBLagAArkgf.jpg%3Alarge?width=1274&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
