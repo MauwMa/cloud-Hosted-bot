e439857da39420175457f64a0857163bddf7d264
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-01-20 21:20:28 +0100

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -6,10 +6,10 @@ module.exports = {
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('♚Charybdis♚')
+        .setTitle('❀Jet Ink, Silver Quills❀')
         .setURL('')
         .setTimestamp()
-        .setDescription(`Dido-osztályú könnyű cirkáló, Charybdis a szolgálatodra. Megtiszteltetés személyesen találkozni veled, Mester. Meleg napfény járja át a mindennapi életed, a békés napokban amik jönnek. \n\nHMS Charybdis felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.`)        
+        .setDescription(`Boldog Új Évet, Parancsnok. Hogy megünnepeljük ezt a szezont, készítettem egy szabott ruhát magamnak, amely tradicionális Dargon Empery ruhákat használ alapnak. Nem nézek ki... gyerekesen, ugye?\n\nIJN Oite felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.`)        
         .setImage('https://images-ext-2.discordapp.net/external/DGO1tB8_74gDh1_yLnMXHoBKgMRcbKEu-VVy0LVA-L4/https/pbs.twimg.com/media/FJeA8NbakAEG1X1.jpg%3Alarge?width=1047&height=548')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
