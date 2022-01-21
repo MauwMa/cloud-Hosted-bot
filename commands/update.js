239db1292cbff5376f3511345c8733ac2c8d2b09
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-01-21 15:32:17 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("❀Firecrackers and Steamed Buns❀")
+        .setTitle("❀A Táncoló Felhők Köntöse❀")
         .setURL('')
         .setTimestamp()
-        .setDescription('A nevem Bristol, Eagle Union romboló! Üdvözletem, Parancsnok! Ez a kikötő rengeteg rejtélynek add otthont... A feladatommá teszem mindegyiknek megoldását! Heheheheh!\n\nUSS Bristol a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok')
-        .setImage('https://images-ext-2.discordapp.net/external/IePJNi5c6g7eZaZyfgh6OZ2hqAZnA6bgkeXGEMzTHN0/https/pbs.twimg.com/media/FJimDXEaUAIEExn.jpg%3Alarge?width=1291&height=676')
+        .setDescription('Parancsnok, köszönöm hogy eljöttél megnézni. Jelenleg elpróbálom az előadásomat a Tavaszi Fesztiválra. Remélem el fogja nyerni tetszésedet.\n\nIJN Asashio új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/f751syJdj1EVJ92_gc0weW_oK3WBdnhgIrSsKA-bwYg/https/pbs.twimg.com/media/FJimzk8VUAA-Us6.jpg%3Alarge?width=1034&height=548')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
