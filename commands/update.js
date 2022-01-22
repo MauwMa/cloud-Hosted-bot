441143e269abe2c094cdb59f64cdab080e53fb34
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-01-22 10:12:47 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("❀A Táncoló Felhők Köntöse❀")
+        .setTitle("⚓Chen Hai⚓")
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnok, köszönöm hogy eljöttél megnézni. Jelenleg elpróbálom az előadásomat a Tavaszi Fesztiválra. Remélem el fogja nyerni tetszésedet.\n\nIJN Asashio új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/f751syJdj1EVJ92_gc0weW_oK3WBdnhgIrSsKA-bwYg/https/pbs.twimg.com/media/FJimzk8VUAA-Us6.jpg%3Alarge?width=1034&height=548')
+        .setDescription('Szóval te vagy itt az agy az operációk mögött, mi? ...Hehe, végre találkozhattam a legendás Parancsnokkal szemtől szembe. Megtiszteltetés veled együtt dolgozni. Bízom benne, hogy fel fogsz élni a saját magasztos elvárásaidnak.\n\nChen Hai felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/2fZ-0Sa5YxRQJtb6bgQbxfvoDL9EboicqqnL8ue3RFs/https/pbs.twimg.com/media/FJnfnMCVIAUJEEH.jpg%3Alarge?width=1291&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
