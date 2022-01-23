c5dda8ea4d002853cc4881e79515d66f90736cf6
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-01-23 08:31:29 +0100

    Update update3.js

--- commands/update3.js
+++ commands/update3.js
@@ -6,11 +6,11 @@ module.exports = {
         const update3 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('')
+        .setTitle('♚A Hibátlan Szépség♚')
         .setURL('')
         .setTimestamp()
-        .setDescription('A Shougatsu Szerencse Doboz 2022 karbantartás után lesz elérhető.')
-        .setImage('https://images-ext-2.discordapp.net/external/KVxe03MvYPz7wE23cuMFxZl1K08rhLglXwxm3zGH4ik/https/pbs.twimg.com/media/FHxWyecVkAED-0F.jpg%3Alarge')
+        .setDescription('Akashi, következőnek nézzek a kamerába? Értettem. Óh, Parancsnok, nem számítottam rá hogy megáldasz minket a jelenléteddel! Igen, a fotózkodás zökkenőmentesen zajlik. Amúgy meg, van kedvenc pózod amit szeretnél látni?\n\nHMS Rodney új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/6jWjmIQFmf5ywvnuh33QA-lnw8frex-ZmP6ryTeaBfw/https/pbs.twimg.com/media/FJsV4hbVIAAsM1v.jpg%3Alarge?width=1274&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update3);
         if (update3){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
