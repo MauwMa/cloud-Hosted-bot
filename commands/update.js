d3ce166be84d46371cadc71d978ef9c22d8914a1
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-01-25 17:38:29 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("⚓A Csodák Előcsarnoka⚓")
+        .setTitle("✠Új Évi Kémia✠")
         .setURL('')
         .setTimestamp()
-        .setDescription('Itt vagy, Parancsnok, mint ahogy megígérted. Hogyan tetszik az itteni légkör? Hehe, foglalj helyet és élvezd lassan a teádat. Végül is, rengeteg időnk van~\n\nChen Hai új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/hwINYao_P-LJNUpqf7_uTFbW621IZ-xr47UtEBBT2JQ/https/pbs.twimg.com/media/FJ3yioragAU_SJt.jpg%3Alarge?width=1274&height=676')
+        .setDescription('A mai kémia órán megtanuljuk, hogy hogyan melegítsük fel a bögre teánkat és az opcionális főzetet hogy erősítsük az ízét! Vállalod az ízének kóstolásának megtiszteltetését, Parancsnok? \n\nKMS U-73 új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/kzz8fvKbwKl5btl9VCd0hBuK8diFWp2VFjAxXoJUddw/https/pbs.twimg.com/media/FJ8FO2aakAAOCqK.jpg%3Alarge?width=1274&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
