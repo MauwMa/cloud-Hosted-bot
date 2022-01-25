3d848ef69a94b702b65a7e8bd58e91b336a2a3c4
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-01-25 17:32:37 +0100

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -6,11 +6,11 @@ module.exports = {
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('★Mese a Császárságból★')
+        .setTitle('♚Hajdani Növénygyűjtő♚')
         .setURL('')
         .setTimestamp()
-        .setDescription(`Boldog Hold Új Évet, Parancsnok! Végre eljött az idő, hogy belenézzünk a Dragon Empery régi legendáiba! Az igazság eme legendák mögött, hogy olyan lenyűgőzőek legyenek, mint maguk a legendák!\n\nUSS Bristol új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
-        .setImage('https://images-ext-2.discordapp.net/external/iAk8s0mFK8gxUgyv9kCYa468TmvDo2E1880y9egDGIc/https/pbs.twimg.com/media/FJwZyU8VgAAq4k6.jpg%3Alarge?width=1274&height=676')
+        .setDescription(`Hogyan érzed magad, Parancsnok? Hehe, most nem hordom a megszokott nővéri köpenyemet, de úgy hiszem nem nagyon különböző~\n\nHMS Hermione új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
+        .setImage('https://images-ext-1.discordapp.net/external/T35AxVFXre5ZCx7ThL1jmHSWKABUYAcMN7XpncZ6nAo/https/pbs.twimg.com/media/FJ2y0qNaMAUt6nK.jpg%3Alarge?width=1274&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
