76bb5220a5931c5c94a30328762c925cba8ec660
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-01-26 17:38:43 +0100

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -6,10 +6,10 @@ module.exports = {
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('❀Nestling Up for the Long Nights❀')
+        .setTitle('❀Befészkelődni a hosszú éjszakákra❀')
         .setURL('')
         .setTimestamp()
-        .setDescription(`\n\nIJN Naganami új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
+        .setDescription(`Segíteni szerettem volna a stresszed eloszlatásában, ezért úgy gondoltam hogy népmeséket olvasok neked...Amikor ezt megemlítettem, Akashi ezeket a ruhákat ajánlotta nekem... M-Mit gondolsz?\n\nIJN Naganami új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
         .setImage('https://images-ext-2.discordapp.net/external/bZnu8eESn5z-0Qrv_NLsatfm-HvYCE_-F9AUxNeldBA/https/pbs.twimg.com/media/FJ8Gjz8agAIB00b.jpg%3Alarge?width=1274&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
