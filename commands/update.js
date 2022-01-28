2fbec4f7a5334fe29389e1445233e637996a888e
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-01-28 17:15:23 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,10 +6,10 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("❀Dinasztia Shipgirl❀")
+        .setTitle("🔥Junyou META🔥")
         .setURL('')
         .setTimestamp()
-        .setDescription('Felszerelkeztem az teljes Dragon Empery felszereléssel, igen. Lehet nem néz ki olyan menőn mint a játékban, de Ayanamit kielégíti. Heh.\n\nIJN Ayanami új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setDescription('Junyou... vagyij. Kérlek adj utasításokat... Parancsnok.\n\nJunyou META hamarosan leszáll. Ingyenesen megszerezhető lesz Cruise Mission elvégzéséért.')
         .setImage('https://images-ext-2.discordapp.net/external/eoknrX53De8BDeHXtju3tQO8EFmyLOsfFI_1cdAmb5o/https/pbs.twimg.com/media/FJ8GQ4SaAAEhUIn.jpg%3Alarge?width=1274&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
