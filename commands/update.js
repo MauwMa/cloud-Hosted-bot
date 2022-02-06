fba02e0bed89af7cb8ae34351c5816b59940220f
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-02-06 19:35:04 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("")
+        .setTitle("【Az Új Korszak, Az Új Erő】")
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok\n\nA Dragon Empery Estate bútorzat szett Február 10. magyar időszámítás szerint 08:00-ig lesz elérhető. Szerezzétek meg ezt a bútorzat szettet, hogy belekóstoljatok Dragon Empery mindennapi életébe~')
-        .setImage('https://images-ext-1.discordapp.net/external/3fmx7aqliOaa4-Vz9RFN1TVpekOf3MWM8JAtgpkHuS0/https/pbs.twimg.com/media/FK1xqbkXsAERkhQ.jpg%3Alarge?width=1141&height=676')
+        .setDescription('Parancsnokok, megszereztétek már An Shan és Chang Chun retrofitjét? Az event Február 10. magyar időszámítás szerint 08:00-kor fog végetérni.')
+        .setImage('https://images-ext-2.discordapp.net/external/CwkXfQPr83ckOLs1Qscz2-hxiMWqpq54j4D3sMtIQa4/https/pbs.twimg.com/media/FKf76U_UYAIlfbY.jpg%3Alarge?width=1363&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
