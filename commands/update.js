cc84c8400c5b754a64554f03312c12078caf9c83
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-02-06 07:39:19 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("【Világ a Festményen Belül】")
+        .setTitle("")
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok, elvégeztétek az eventet és köszöntöttétek Hai Chit és Hai Tient a kikötőtökben? Az event magyar időszámítás szerint Február 10.-én 08:00-kor ér véget.')
-        .setImage('https://images-ext-1.discordapp.net/external/acdyw1JRxPYDptxKZ_VUwv7GzhCX-Jxo6DkFZVx_0Qw/https/pbs.twimg.com/media/FKf7diqVcAA2ChH.jpg%3Alarge')
+        .setDescription('Parancsnokok\n\nA Dragon Empery Estate bútorzat szett Február 10. magyar időszámítás szerint 08:00-ig lesz elérhető. Szerezzétek meg ezt a bútorzat szettet, hogy belekóstoljatok Dragon Empery mindennapi életébe~')
+        .setImage('https://images-ext-1.discordapp.net/external/3fmx7aqliOaa4-Vz9RFN1TVpekOf3MWM8JAtgpkHuS0/https/pbs.twimg.com/media/FK1xqbkXsAERkhQ.jpg%3Alarge?width=1141&height=676')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
