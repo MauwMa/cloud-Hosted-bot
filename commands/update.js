6efd050dfc1b8cebb55de22f4489f2872bfec395
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-02-11 18:17:20 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("❀Szerencsés Csokoládé❀")
+        .setTitle("")
         .setURL('')
         .setTimestamp()
-        .setDescription('Rendben, ennek jónak kell lennie... Hé, buta Parancsnok ne csak álldogálj. Igen, te. Ne üsd bele az orrod. Te leszel az első, aki megkapja az ÉN csokimat, szóval jobban teszed ha háládat könnyekben fejeznéd ki.\n\nIJN Shigure új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/fLkz61nvkRK2Jz1CiQJkYNjCOElQagPQ6sm-ZDzzPpM/https/pbs.twimg.com/media/FLEBK_0VQAAgzyc.jpg%3Alarge?width=1005&height=548')
+        .setDescription('Parancsnok,\n\nJelenleg megy a Northern Overture rerun event. Ezek a limitált karakterek 2/17 magyar időszámítás szerint 08:00-ig építhetőek.')
+        .setImage('https://images-ext-1.discordapp.net/external/qMODT67iqEEBjd2oUYe_AMDiEjENkmPRPiEyhhN6rG0/https/pbs.twimg.com/media/FLUI7DQUcAEc5HA.jpg%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
