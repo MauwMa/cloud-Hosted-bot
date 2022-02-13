359256fd3b1db9076e0e883b1257c9c135ee0f62
MauwMa <43650202+MauwMa@users.noreply.github.com>
2022-02-13 21:19:47 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("")
+        .setTitle("【Valentin napi Ajándék】")
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnok,\n\nJelenleg megy a Northern Overture rerun event. Ezek a limitált karakterek 2/17 magyar időszámítás szerint 08:00-ig építhetőek.')
-        .setImage('https://images-ext-1.discordapp.net/external/qMODT67iqEEBjd2oUYe_AMDiEjENkmPRPiEyhhN6rG0/https/pbs.twimg.com/media/FLUI7DQUcAEc5HA.jpg%3Alarge')
+        .setDescription('2/14-től 2/24 magyar időszámítás szerint 08:00-ig bejelentkezve Valentin napi ajándékot kapsz a secretary hajódtól, aki a legbaloldalibb oldalon van. Minden üzenet mást fog tartalmazni a hajótól függően.\n\n A Collabos hajók üzeneteit Akashi üzenetként kapjátok meg. A META karakterek különbözőt adnak az eredetihez képest. Az ajándék tartalma ugyan az lesz, minden karakternek. (1 üzenet)')
+        .setImage('https://images-ext-2.discordapp.net/external/YaxgGFfyMypdJFCd3CaraWY0hg8qmZ2wpCfr3OYFN0k/https/pbs.twimg.com/media/FLYCqc2VQAAyzEE.png%3Alarge')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
