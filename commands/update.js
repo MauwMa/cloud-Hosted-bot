2c39446dc5c0c52c3b3d4ccc3a403dceecf73e82
MauwMa <sgtmauwma@gmail.com>
2022-02-18 22:29:38 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("【Valentin napi Ajándék】")
+        .setTitle("☭Kronshtadt☭")
         .setURL('')
         .setTimestamp()
-        .setDescription('2/14-től 2/24 magyar időszámítás szerint 08:00-ig bejelentkezve Valentin napi ajándékot kapsz a secretary hajódtól, aki a legbaloldalibb oldalon van. Minden üzenet mást fog tartalmazni a hajótól függően.\n\n A Collabos hajók üzeneteit Akashi üzenetként kapjátok meg. A META karakterek különbözőt adnak az eredetihez képest. Az ajándék tartalma ugyan az lesz, minden karakternek. (1 üzenet)')
-        .setImage('https://images-ext-2.discordapp.net/external/YaxgGFfyMypdJFCd3CaraWY0hg8qmZ2wpCfr3OYFN0k/https/pbs.twimg.com/media/FLYCqc2VQAAyzEE.png%3Alarge')
+        .setDescription('Kronshtadt vagyok a Northern Parliament cirkálója. Nem a tűzerőm az egyetlen érdemem - sokkal többem van annál. Biztos vagyok benne, hogy kideríted magadnak miközben az ellenséggel összeütközünk, Elvtárs.\n\nSN Kronshtadt felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/GPyJsnsyj8q6krqSow-f41B8ozGPOxOiPDBdjiq-e0M/https/pbs.twimg.com/media/FL4EKsPUUAAg_PJ.jpg')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
