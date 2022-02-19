ad440bdc4308b29306c72332977ec399b06e254d
MauwMa <sgtmauwma@gmail.com>
2022-02-19 21:47:59 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("☭Kronshtadt☭")
+        .setTitle("☭Nem megszokott tőlem ez?☭")
         .setURL('')
         .setTimestamp()
-        .setDescription('Kronshtadt vagyok a Northern Parliament cirkálója. Nem a tűzerőm az egyetlen érdemem - sokkal többem van annál. Biztos vagyok benne, hogy kideríted magadnak miközben az ellenséggel összeütközünk, Elvtárs.\n\nSN Kronshtadt felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/GPyJsnsyj8q6krqSow-f41B8ozGPOxOiPDBdjiq-e0M/https/pbs.twimg.com/media/FL4EKsPUUAAg_PJ.jpg')
+        .setDescription('"Muhahaha... Most hát, ezek az utcák hozzám tartoznak..." Bocsi, megharaptam a nyelvemet... Hmm, elég nehéz ezt a szerepet játszani végül is.\n\nSN Avrora új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/6vY59s9DcjAmS26gbuwuDxc5DXdkIGtg5HJyeaMQ9ao/https/pbs.twimg.com/media/FL98J2bVUAI-dIz.jpg?width=881&height=468')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
