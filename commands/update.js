efd023637348b63d30cc6560667781787b8b92cc
MauwMa <sgtmauwma@gmail.com>
2022-02-20 14:25:24 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("☭Nem megszokott tőlem ez?☭")
+        .setTitle("☭Kiev☭")
         .setURL('')
         .setTimestamp()
-        .setDescription('"Muhahaha... Most hát, ezek az utcák hozzám tartoznak..." Bocsi, megharaptam a nyelvemet... Hmm, elég nehéz ezt a szerepet játszani végül is.\n\nSN Avrora új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/6vY59s9DcjAmS26gbuwuDxc5DXdkIGtg5HJyeaMQ9ao/https/pbs.twimg.com/media/FL98J2bVUAI-dIz.jpg?width=881&height=468')
+        .setDescription('Kiev vagyok a 48 Project osztályú Northern Parliament rombolója. Mostantól én fogok segíteni neked mindenféle dologban, Parancsnok... E-Ez, öhm, elég bemutatkozásnak?\n\nSN Kiev új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/GN4TGny2JquY79KPd3RVjKnMTRMJhwwLxXJw0QwcPZw/https/pbs.twimg.com/media/FL98jkMVQAAELqc.jpg?width=895&height=468')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
