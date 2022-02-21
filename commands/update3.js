77f2d14c8438c2243f7754457f96ca161dffb428
MauwMa <sgtmauwma@gmail.com>
2022-02-21 20:44:32 +0100

    Update update3.js

--- commands/update3.js
+++ commands/update3.js
@@ -6,11 +6,11 @@ module.exports = {
         const update3 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('【Megjegyzés】')
+        .setTitle('☭Volga☭')
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnok, a SY-1 rakétákhoz való fejlesztés general partokba fognak kerülni. Minden <:Pnz:790201580812894218> és main gun part amelyek a fejlesztésére lett elköltve, a játékon belüli e-mailben visszaküldjük. Elnézést a kellemetlenségért és köszönjük a támogatást.')
-        .setImage('https://images-ext-2.discordapp.net/external/3wjj99RZuroj5FtlyQdJymGnIcYStPS-o5ZKD3vmB4U/https/pbs.twimg.com/media/FLOjZwsVgAITKss.jpg%3Alarge?width=960&height=548')
+        .setDescription('Nem nézel ki valami jól... Eszel te mostanában rendesen? És mi a helyzet az alvással? Hmm... Talán van valami köze az érzelmi stresszhez, vagy kapcsolati problémákkal? ...Oh. A nevem Volga, Northern Parliament Volgája. Óh jaj, tényleg elfelejtettem bemutatkozni egészen idáig?\n\nSN Volga felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/_l0Vsh7loA4iePWU9z31Jtd1XzPstZPbgvoNuXEeSWM/https/pbs.twimg.com/media/FMHQSWjUUAYW373.jpg')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update3);
         if (update3){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
