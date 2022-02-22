594fa68b47d9079368a5f75056218967f7fdab85
MauwMa <sgtmauwma@gmail.com>
2022-02-22 17:25:48 +0100

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -6,11 +6,11 @@ module.exports = {
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('☭Egy ellenállhatatlan AR kaland☭')
+        .setTitle('☭A Nagyszerű Menekűlés☭')
         .setURL('')
         .setTimestamp()
-        .setDescription(`Itt Volga. Jelenleg egy fontos szemtanút követek, *korgás* ...Azért korog a hasam, mert ez a AR játék ilyen sok staminát követel? Áhh, elvesztettem a szemtanút...\n\nSN Volga új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
-        .setImage('https://images-ext-2.discordapp.net/external/Oldj6QDCqSF2VoH1CHV3gbBHDA5ilMZZgi-sM4Qf23U/https/pbs.twimg.com/media/FMHQtYcVUAIn7pP.jpg')
+        .setDescription(`A választott kincsed most már hozzám Arkhangelsk mester tolvajhoz tartozik! Azt hiszed le tudsz csukni? Akkor ne csak bámulj, hanem gyere és kapj el!\n\nSN Arkhangelsk új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
+        .setImage('https://images-ext-2.discordapp.net/external/Q4x6yq7JoEk351950UmFjHzyzT5lfdwXvlk2aez4JAk/https/pbs.twimg.com/media/FMMaP7yUcAAH73i.jpg')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
