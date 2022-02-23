7dc2f3d9cb6503e2bb0e9f26858bf38779c2b834
MauwMa <sgtmauwma@gmail.com>
2022-02-23 19:42:32 +0100

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -6,11 +6,11 @@ module.exports = {
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('☭Az Utcai Ezüst Szónáta☭')
+        .setTitle('☭A Hajsza!☭')
         .setURL('')
         .setTimestamp()
-        .setDescription(`Megnyugtat, hogy itt látlak. Tudom, hogy ez az ellenörzés simán fog menni amíg te itt vagy. Mhm, minden rendben.\n\nSN Kiev új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
-        .setImage('https://images-ext-2.discordapp.net/external/j1pl1mn49dzBENf9Cf61Y4dS-i617b6foQUsAXAZBhg/https/pbs.twimg.com/media/FMMal-QVkAEWexj.jpg')
+        .setDescription(`Ide figyeljetek seggfejek! Körbekerítettünk titeket! Kronshtadt vagyok a Northern Parliament Különleges Erők vezetője! Egy választásotok van: gyertek ki feltartott kézzel és adjátok meg magatokat!\n\nSN Kronshtadt L2D új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
+        .setImage('https://images-ext-2.discordapp.net/external/nB6KjGu6yAzAO63vnwCQphIwIdkwdGmLS72ybdSPubc/https/pbs.twimg.com/media/FMR7LNfVQAAEXLj.jpg')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
