9392fe7cd1f59f480df411b1c964417b224f8f1c
MauwMa <sgtmauwma@gmail.com>
2022-02-23 22:13:03 +0100

    Update egyorateszt.js

--- commands/egyorateszt.js
+++ commands/egyorateszt.js
@@ -1,11 +1,11 @@
 const schedule = require('node-schedule');
 module.exports = {
-    name: 'egyora',
+    name: 'egyorateszt',
     permissions: ["MENTION_EVERYONE"],
-    description: 'egyora',
+    description: 'egyorateszt',
     execute (client, message, args, Discord){
-        var exportScheduler = schedule.scheduleJob('0 12 21 24 02 *', function() { 
-            const egyora = new Discord.MessageEmbed()
+        var exportScheduler = schedule.scheduleJob('0 16 21 24 02 *', function() { 
+            const egyorateszt = new Discord.MessageEmbed()
             .setColor('#ffff00')
             .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
             .setTitle('Értesítés!')
@@ -13,7 +13,7 @@ module.exports = {
             .setDescription('1 órás előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 8 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
             .setImage('https://images-ext-2.discordapp.net/external/PGLOP1lyRoOuSu0_VOVLG4pDYnNQRtj_iBvYnSzdF30/https/pbs.twimg.com/media/FE21HPTVUAEzk36.jpg%3Alarge?width=965&height=551')
             .setTimestamp()
-            client.channels.cache.find(channel => channel.id === "853629612986990612").send(egyora); 
+            client.channels.cache.find(channel => channel.id === "853629612986990612").send(egyorateszt); 
         });
     }    
 }
