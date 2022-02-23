7be8a28d1e410140ca0994a8b12523cffa41542f
MauwMa <sgtmauwma@gmail.com>
2022-02-23 19:30:52 +0100

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -6,11 +6,11 @@ module.exports = {
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('☭A Nagyszerű Menekűlés☭')
+        .setTitle('☭Az Utcai Ezüst Szónáta☭')
         .setURL('')
         .setTimestamp()
-        .setDescription(`A választott kincsed most már hozzám Arkhangelsk mester tolvajhoz tartozik! Azt hiszed le tudsz csukni? Akkor ne csak bámulj, hanem gyere és kapj el!\n\nSN Arkhangelsk új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
-        .setImage('https://images-ext-2.discordapp.net/external/Q4x6yq7JoEk351950UmFjHzyzT5lfdwXvlk2aez4JAk/https/pbs.twimg.com/media/FMMaP7yUcAAH73i.jpg')
+        .setDescription(`Megnyugtat, hogy itt látlak. Tudom, hogy ez az ellenörzés simán fog menni amíg te itt vagy. Mhm, minden rendben.\n\nSN Kiev új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
+        .setImage('https://images-ext-2.discordapp.net/external/j1pl1mn49dzBENf9Cf61Y4dS-i617b6foQUsAXAZBhg/https/pbs.twimg.com/media/FMMal-QVkAEWexj.jpg')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
