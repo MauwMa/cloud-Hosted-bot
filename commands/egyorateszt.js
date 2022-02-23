571ad1e75c05e3444b0a2ff72323f1b012ea6d93
MauwMa <sgtmauwma@gmail.com>
2022-02-23 21:26:23 +0100

    Create egyorateszt.js

---
+++ commands/egyorateszt.js
@@ -0,0 +1,19 @@
+const schedule = require('node-schedule');
+module.exports = {
+    name: 'egyorateszt',
+    permissions: ["KICK_MEMBERS"],
+    description: 'egyorateszt',
+    execute (client, message, args, Discord){
+        var exportScheduler = schedule.scheduleJob('0 30 8 23 02 *', function() { 
+            const egyorateszt = new Discord.MessageEmbed()
+            .setColor('#ffff00')
+            .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
+            .setTitle('Értesítés!')
+            .setURL('')
+            .setDescription('1 órás előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 8 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
+            .setImage('https://images-ext-2.discordapp.net/external/PGLOP1lyRoOuSu0_VOVLG4pDYnNQRtj_iBvYnSzdF30/https/pbs.twimg.com/media/FE21HPTVUAEzk36.jpg%3Alarge?width=965&height=551')
+            .setTimestamp()
+            client.channels.cache.find(channel => channel.id === "791956226521956394").send(egyorateszt); 
+        });
+    }    
+}
