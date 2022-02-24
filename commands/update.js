1f4cd7028774a926f47de42c0d34ba863979f99a
MauwMa <sgtmauwma@gmail.com>
2022-02-24 06:43:59 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("☭Az Élet Gépezete☭")
+        .setTitle("☭A Driftelő Igazság☭")
         .setURL('')
         .setTimestamp()
-        .setDescription('Igen, ezek az adatok és eredmények elég érdekesek... Bár nekem semmit nem jelentenek! Én gépész zseni vagyok, nem elemző. Ahh-ha-ha-haah!\n\nSN Soobrazitelny felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/L1eBbYUtbZ9S53Olm4spAWhCifx4opPLjsrFzewzgWs/https/pbs.twimg.com/media/FMR7fl6VQAISguT.jpg?width=881&height=468')
+        .setDescription('Állj meg! Nincs merre menekülnöd! ...Szép munka, Elvtárs Parancsnok, milyen izgalmas üldözésben volt részünk! Mi, úgy gondolod hogy hozzám illik ez a munka? Csak ugratsz engem, bahaha! \n\nSN kirov L2D új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/pMeqoTPtQPJvcwDi4obIYHeSlBjhqcLs7_uw3leJDiw/https/pbs.twimg.com/media/FMR7ujMVcAANXEp.jpg?width=881&height=468')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
