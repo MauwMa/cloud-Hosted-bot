f6e4f1a64fb3f6e19c0513d925ea3fa115ddfef1
MauwMa <sgtmauwma@gmail.com>
2022-02-24 06:37:41 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("☭Arkhangelsk☭")
+        .setTitle("☭Az Élet Gépezete☭")
         .setURL('')
         .setTimestamp()
-        .setDescription('Csatahajó Arkhangelsk vagyok. Hehe, szókimondóan beszélni velem pont az, amit szeretek. Látok egy kis potenciát benned.\n\nSN Arkhangelsk felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/zOAeFlT7go9gi2MbL59mmCD1vPVyFsb1VVeAe3l6sak/https/pbs.twimg.com/media/FMHRPGHVgAMAY2t.jpg')
+        .setDescription('Igen, ezek az adatok és eredmények elég érdekesek... Bár nekem semmit nem jelentenek! Én gépész zseni vagyok, nem elemző. Ahh-ha-ha-haah!\n\nSN Soobrazitelny felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/L1eBbYUtbZ9S53Olm4spAWhCifx4opPLjsrFzewzgWs/https/pbs.twimg.com/media/FMR7fl6VQAISguT.jpg?width=881&height=468')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
