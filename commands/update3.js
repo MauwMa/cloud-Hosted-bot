5412e054d91d487a0801e903ded49f04b5216b4b
MauwMa <sgtmauwma@gmail.com>
2022-02-25 18:47:18 +0100

    Update update3.js

--- commands/update3.js
+++ commands/update3.js
@@ -9,8 +9,8 @@ module.exports = {
         .setTitle('')
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnok, az Abyssal Refrain event elkezdődött! Kronshtadt és a többi Northern Parliament tagok összegyűltek, hogy megvizsgálják a Szirén technológia rejtélyét.')
-        .setImage('https://images-ext-1.discordapp.net/external/yHEuGXgOZW0YeDtzXVcGeT2ObjcuP-l7NaBUiUpCHHQ/https/pbs.twimg.com/media/FMYGZ3ZVIAcGekP.jpg?width=1170&height=658')
+        .setDescription('Parancsnok\n\nA jelenlegi event a Abyssal Refrain! Ezek a limitált karakterek az event építésben egészen Március 10.-ig lesznek elérhetőek.')
+        .setImage('https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/274814042_1665902797107506_8671470509112947035_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=730e14&_nc_ohc=veSNe-BxTUUAX-JPQGx&_nc_ht=scontent-vie1-1.xx&oh=00_AT-mWli6kzp4qu-FqC8K3gpsG0qL6trQeloYnjk6dnHXlA&oe=621DF62C')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update3);
         if (update3){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
