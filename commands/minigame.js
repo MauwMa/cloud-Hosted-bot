69b848029d3d364af0742cab52c5a2e82e2319cb
MauwMa <sgtmauwma@gmail.com>
2022-03-09 18:25:11 +0100

    Update minigame.js

--- commands/minigame.js
+++ commands/minigame.js
@@ -6,10 +6,10 @@ module.exports = {
         const minigame = new Discord.MessageEmbed()
         .setColor('2ecc71')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("Mini Game")
+        .setTitle("Parancsnokok, egy új mini game érkezett nyaaa~ Frakcióként szerintetek, ki a legjobb Main/Vanguard helyre? \n\nTemplate:\n\nEagle Union   Main:   Vanguard:   \n\nRoyal Navy   Main:   Vanguard:   \n\nSakura Empire   Main:   Vanguard:   \n\nIron Blood   Main:   Vanguard:   \n\nDragon Empery   Vanguard:   \n\nSardegna Empire   Main:   Vanguard:   \n\nNorthern Parliament   Main:   Vanguard:   \n\nFrench Navy   Main:   Vanguard:   ")
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok, egy új mini game érkezett nyaaa~ Frakcióként szerintetek, ki a legjobb Main/Vanguard helyre? \n\nTemplate:\n\nEagle Union   Main:   Vanguard:   \n\nRoyal Navy   Main:   Vanguard:   \n\nSakura Empire   Main:   Vanguard:   \n\nIron Blood   Main:   Vanguard:   \n\nDragon Empery   Vanguard:   \n\nSardegna Empire   Main:   Vanguard:   \n\nNorthern Parliament   Main:   Vanguard:   \n\nFrench Navy   Main:   Vanguard:   ')
+        .setDescription('')
         .setImage('')
         client.channels.cache.find(channel => channel.id === "791956226521956394").send(minigame);
         if (minigame){
