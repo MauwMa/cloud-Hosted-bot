1bfe552b7d43b25f0ade4ad8016a75c41ec7999d
MauwMa <sgtmauwma@gmail.com>
2022-03-09 18:18:02 +0100

    Update szar.js

--- commands/szar.js
+++ commands/szar.js
@@ -4,7 +4,7 @@ module.exports = {
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args){
 
-        message.channel.send("Parancsnokok, egy új mini gamel készültem nyaaa~ Frakcióként szerintetek, ki a legjobb Main/Vanguard helyre? \n\nFrakciók:\n\nEagle Union   Main:   Vanguard:   \n\nRoyal Navy   Main:   Vanguard:   \n\nSakura Empire   Main:   Vanguard:   \n\nIron Blood   Main:   Vanguard:   \n\nDragon Empery   Main:   Vanguard:   \n\nSardegna Empire   Main:   Vanguard:   \n\nNorthern Parliament   Main:   Vanguard:   \n\nFrench Navy   Main:   Vanguard:   ");
+        message.channel.send("Parancsnokok, egy új mini game érkezett nyaaa~ Frakcióként szerintetek, ki a legjobb Main/Vanguard helyre? \n\nFrakciók:\n\nEagle Union   Main:   Vanguard:   \n\nRoyal Navy   Main:   Vanguard:   \n\nSakura Empire   Main:   Vanguard:   \n\nIron Blood   Main:   Vanguard:   \n\nDragon Empery   Main:   Vanguard:   \n\nSardegna Empire   Main:   Vanguard:   \n\nNorthern Parliament   Main:   Vanguard:   \n\nFrench Navy   Main:   Vanguard:   ");
 
     if (message.deletable) message.delete();
 
