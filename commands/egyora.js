a6cbaf34a6447cec419738b870da10e610adf8f0
MauwMa <sgtmauwma@gmail.com>
2022-03-09 18:12:59 +0100

    dsa

--- commands/egyora.js
+++ commands/egyora.js
@@ -10,7 +10,7 @@ module.exports = {
             .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
             .setTitle('Értesítés!')
             .setURL('')
-            .setDescription('1 órás előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 8 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
+            .setDescription('1 órás előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 6 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
             .setImage('https://images-ext-2.discordapp.net/external/5v50XmQ9nF9KyOXWmb0nOWfrE2ORMd0UFkonjtTr7Ec/https/pbs.twimg.com/media/FNQCnhJakAgtc2h.jpg?width=820&height=468')
             .setTimestamp()
             client.channels.cache.find(channel => channel.id === "789936292413571102").send(egyora); 
--- commands/szar.js
+++ commands/szar.js
@@ -4,7 +4,7 @@ module.exports = {
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args){
 
-        message.channel.send("Parancsnokok, egy új mini gamel készültem nyaaa~ Frakcióként szerintetek, ki a legjobb Main/Vanguard helyre? \n\n Frakciók: Eagle Union, Royal Navy, Sakura Empire, Iron Blood, Dragon Empery, Sardegna Empire, Northern Parliament, Iris Libre, Vichya Dominion");
+        message.channel.send("Parancsnokok, egy új mini gamel készültem nyaaa~ Frakcióként szerintetek, ki a legjobb Main/Vanguard helyre? \n\nFrakciók: Eagle Union, Royal Navy, Sakura Empire, Iron Blood, Dragon Empery, Sardegna Empire, Northern Parliament, Iris Libre, Vichya Dominion");
 
     if (message.deletable) message.delete();
 
