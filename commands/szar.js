db9906885fecf5731bc0e94f3f372b34512e2cfb
MauwMa <sgtmauwma@gmail.com>
2022-03-09 18:14:42 +0100

    Update szar.js

--- commands/szar.js
+++ commands/szar.js
@@ -4,7 +4,7 @@ module.exports = {
     permissions: ["KICK_MEMBERS"],
     execute (client, message, args){
 
-        message.channel.send("Parancsnokok, egy új mini gamel készültem nyaaa~ Frakcióként szerintetek, ki a legjobb Main/Vanguard helyre? \n\nFrakciók: Eagle Union, Royal Navy, Sakura Empire, Iron Blood, Dragon Empery, Sardegna Empire, Northern Parliament, Iris Libre, Vichya Dominion");
+        message.channel.send("Parancsnokok, egy új mini gamel készültem nyaaa~ Frakcióként szerintetek, ki a legjobb Main/Vanguard helyre? \n\nFrakciók: Eagle Union\n\n Royal Navy\n\n Sakura Empire\n\n Iron Blood\n\n Dragon Empery\n\n Sardegna Empire\n\n Northern Parliament\n\n French Navy");
 
     if (message.deletable) message.delete();
 
