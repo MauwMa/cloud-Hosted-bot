1c1d95564801d05785475619dfeef567e74fb67a
MauwMa <sgtmauwma@gmail.com>
2022-03-14 17:52:45 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("☭A Driftelő Igazság☭")
+        .setTitle("❀Szellős Suhanás❀")
         .setURL('')
         .setTimestamp()
-        .setDescription('Állj meg! Nincs merre menekülnöd! ...Szép munka, Elvtárs Parancsnok, milyen izgalmas üldözésben volt részünk! Mi, úgy gondolod hogy hozzám illik ez a munka? Csak ugratsz engem, bahaha! \n\nSN kirov L2D új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/pMeqoTPtQPJvcwDi4obIYHeSlBjhqcLs7_uw3leJDiw/https/pbs.twimg.com/media/FMR7ujMVcAANXEp.jpg?width=881&height=468')
+        .setDescription('Kár lenne nem kihasználni ezt a csodálatos időjárást, Parancsnok. Miért lennél a zsúfolt vonaton, mikor évezni tudod a nyugodt biciklizést a napfényben, hm? Heehee♪\n\nIJN Kongou az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/dTAFxFWWbf6yQ6pGG4FIpTxwEvB4dBStUepymzwOMHw/https/pbs.twimg.com/media/FNzqAxHaQAEUIzl.jpg')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
