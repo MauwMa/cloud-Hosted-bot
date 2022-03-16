5b2a102480cc3db483a27a816e09ee971101ed6d
MauwMa <sgtmauwma@gmail.com>
2022-03-16 08:26:28 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("❀Szellős Suhanás❀")
+        .setTitle("★Ördög a Szomszéd★")
         .setURL('')
         .setTimestamp()
-        .setDescription('Kár lenne nem kihasználni ezt a csodálatos időjárást, Parancsnok. Miért lennél a zsúfolt vonaton, mikor évezni tudod a nyugodt biciklizést a napfényben, hm? Heehee♪\n\nIJN Kongou az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/dTAFxFWWbf6yQ6pGG4FIpTxwEvB4dBStUepymzwOMHw/https/pbs.twimg.com/media/FNzqAxHaQAEUIzl.jpg')
+        .setDescription('Ezt nézd meg - egymás mellé ültünk az órán. Már előre várom, hogy mit fogunk csinálni "tanóránkívüli tevékenységként" együtt. Hehehe~\n\nUSS Archerfish az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/N9cyz3dfoRN4D4WxDSnP9hxMz48yJ6UqHMqaymn4Fls/https/pbs.twimg.com/media/FN4KoZWaUAAE-A5.jpg?width=914&height=491')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
