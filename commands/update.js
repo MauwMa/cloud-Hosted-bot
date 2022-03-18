1a1a2f8f1a5e2375317c8bd38e2867de245ab893
MauwMa <sgtmauwma@gmail.com>
2022-03-18 11:27:14 +0100

    dsa

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("❀Kajáljunk❀")
+        .setTitle("❀Egészséges Adag Napfényből!❀")
         .setURL('')
         .setTimestamp()
-        .setDescription('Végre túl vagyunk a reggeli órákon~ Parancsnok, szeretnél egy remeket ebédelni velem? Heheh, kíváncsi vagy hogy mit csináltam? Hamarosan úgyis meg fogod tudni~\n\nIJN Ooshio az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/SaQvAZvD9FtqLCHdnChtmwvaALZdwco8de_90Qr1yDs/https/pbs.twimg.com/media/FN4K0NraQAIWhoB.jpg?width=914&height=491')
+        .setDescription('Hmmgh! Ó igen...  Áh, itt vagy te is, Parancsnok! Akarsz csatlakozni hozzám? Gyere élvezni a délutáni napsütést, olyan jó érzés!\n\nIJN Wakaba az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-2.discordapp.net/external/t9JHrxgw_nEyILTQASSA13SMFNsoftv8shN29XJ58Jc/https/pbs.twimg.com/media/FN8-mvcagAIJgBg.jpg?width=914&height=491')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
--- commands/update2.js
+++ commands/update2.js
@@ -6,11 +6,11 @@ module.exports = {
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('☭A Hajsza!☭')
+        .setTitle('⛨Impero⛨')
         .setURL('')
         .setTimestamp()
-        .setDescription(`Ide figyeljetek seggfejek! Körbekerítettünk titeket! Kronshtadt vagyok a Northern Parliament Különleges Erők vezetője! Egy választásotok van: gyertek ki feltartott kézzel és adjátok meg magatokat!\n\nSN Kronshtadt L2D új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.`)        
-        .setImage('https://images-ext-2.discordapp.net/external/nB6KjGu6yAzAO63vnwCQphIwIdkwdGmLS72ybdSPubc/https/pbs.twimg.com/media/FMR7LNfVQAAEXLj.jpg')
+        .setDescription(`Vittorio Veneto osztályú, Impero vagyok. Bemutatkozásnak vége - irány vissza aludni... Amúgy meg, briliáns vagyok. Szóval nem kell főnökösködni... Csak add meg a teremet és jóban leszünk. Éjszakát.\n\nRN Impero felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.`)        
+        .setImage('https://images-ext-2.discordapp.net/external/nk8Jfd8_owHPnpyCs5vuyHX5TYewogDv-u1-PUv7tO0/https/pbs.twimg.com/media/FODXrdEaAAAPhlj.jpg?width=939&height=491')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
