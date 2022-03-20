c99f8494d8c00265ae9a1a629ed676d6cb7b90f4
MauwMa <sgtmauwma@gmail.com>
2022-03-20 08:22:18 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("⛨Pilledt Könyvtáros⛨")
+        .setTitle("⛨Háztető Ebédszünet⛨")
         .setURL('')
         .setTimestamp()
-        .setDescription('Úgy tűnik az iskolai egyenruhák hypolva vannak. Veneto ezt választotta nekem. Te minek öltöztél, Parancsnok? Tanárnak? Iskolatársnak?\n\nRN Impero az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-1.discordapp.net/external/BfWynqkAJwNcmU_iDV0f3bKZtcdFXp5LK2ebNmwjlQU/https/pbs.twimg.com/media/FOIg31UakAMU1kK.jpg?width=914&height=491')
+        .setDescription('Jaj nekem, már kész vagy a feladataiddal? Biztosan nem hagytad ki a munkát csak azért, hogy velünk egyél... Áh, sajnálom hogy kételkedni mertem benned. Rendben, gyerünk. Trento csak ránk vár.\n\nRN Trieste az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/J9Mdop3PzlvTQHXHBecRZ1oiqgQBbXKmSYgZmJg4sHI/https/pbs.twimg.com/media/FONoppUaQAQKdqs.jpg?width=914&height=491')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
--- commands/update2.js
+++ commands/update2.js
@@ -6,11 +6,11 @@ module.exports = {
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('⛨Pompeo Magno⛨')
+        .setTitle('⛨Trieste⛨')
         .setURL('')
         .setTimestamp()
-        .setDescription(`Pompey Kapitány a Nagyszerű megérkezett a kikötőbe! ...Err! Aham! Jó napokat neked, Parancsnok! Pompeo Magno vagyok Sardegna Empire-ből! Jelölj ki bármilyen és minden munkához engem, és egy szemvillantás alatt megcsinálom mindet!\n\nRN Pompeo Magno felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.`)        
-        .setImage('https://images-ext-1.discordapp.net/external/TytTQoJMRrSXu-2siktwGKg4fJYfV9Z1nQ3EYz4uY_Y/https/pbs.twimg.com/media/FOIhTpaakAMA_s-.jpg?width=939&height=491')
+        .setDescription(`Trieste vagyok, Trento-osztályú kettes számú nehéz cirkálója. A feladatom, hogy segítsek neked a munkádban. Most hát, abba tudnád hagyni a bámulást és visszamenni dolgozni?\n\nRN Trieste felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.`)        
+        .setImage('https://images-ext-1.discordapp.net/external/zpjnlF0bSLJ23Da7BISi_yhVqbidUgR1RrBD9iJV5Kk/https/pbs.twimg.com/media/FONoWuQaAAADlft.jpg?width=939&height=491')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
