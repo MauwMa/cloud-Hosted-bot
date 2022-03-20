df4f29e7cfe6ef64f7a8fef4c4938668a270a716
MauwMa <sgtmauwma@gmail.com>
2022-03-20 08:12:03 +0100

    Update update2.js

--- commands/update2.js
+++ commands/update2.js
@@ -6,11 +6,11 @@ module.exports = {
         const update2 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('⛨Impero⛨')
+        .setTitle('⛨Pompeo Magno⛨')
         .setURL('')
         .setTimestamp()
-        .setDescription(`Vittorio Veneto osztályú, Impero vagyok. Bemutatkozásnak vége - irány vissza aludni... Amúgy meg, briliáns vagyok. Szóval nem kell főnökösködni... Csak add meg a teremet és jóban leszünk. Éjszakát.\n\nRN Impero felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.`)        
-        .setImage('https://images-ext-2.discordapp.net/external/nk8Jfd8_owHPnpyCs5vuyHX5TYewogDv-u1-PUv7tO0/https/pbs.twimg.com/media/FODXrdEaAAAPhlj.jpg?width=939&height=491')
+        .setDescription(`Pompey Kapitány a Nagyszerű megérkezett a kikötőbe! ...Err! Aham! Jó napokat neked, Parancsnok! Pompeo Magno vagyok Sardegna Empire-ből! Jelölj ki bármilyen és minden munkához engem, és egy szemvillantás alatt megcsinálom mindet!\n\nRN Pompeo Magno felkészül a bevetésre és hamarosan a dokkodat fogja díszíteni a közel jövőben, Parancsnok.`)        
+        .setImage('https://images-ext-1.discordapp.net/external/TytTQoJMRrSXu-2siktwGKg4fJYfV9Z1nQ3EYz4uY_Y/https/pbs.twimg.com/media/FOIhTpaakAMA_s-.jpg?width=939&height=491')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update2);
         if (update2){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
