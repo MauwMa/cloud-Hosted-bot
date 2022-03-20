9dd159eb0af76b4ad86d3a57e2147b010f3d3cd8
MauwMa <sgtmauwma@gmail.com>
2022-03-20 08:06:50 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,11 +6,11 @@ module.exports = {
         const update = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle("❀Egészséges Adag Napfényből!❀")
+        .setTitle("⛨Pilledt Könyvtáros⛨")
         .setURL('')
         .setTimestamp()
-        .setDescription('Hmmgh! Ó igen...  Áh, itt vagy te is, Parancsnok! Akarsz csatlakozni hozzám? Gyere élvezni a délutáni napsütést, olyan jó érzés!\n\nIJN Wakaba az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
-        .setImage('https://images-ext-2.discordapp.net/external/t9JHrxgw_nEyILTQASSA13SMFNsoftv8shN29XJ58Jc/https/pbs.twimg.com/media/FN8-mvcagAIJgBg.jpg?width=914&height=491')
+        .setDescription('Úgy tűnik az iskolai egyenruhák hypolva vannak. Veneto ezt választotta nekem. Te minek öltöztél, Parancsnok? Tanárnak? Iskolatársnak?\n\nRN Impero az új ruhájába öltözik. A közeljövőben a dokkodban fog várni, Parancsnok.')
+        .setImage('https://images-ext-1.discordapp.net/external/BfWynqkAJwNcmU_iDV0f3bKZtcdFXp5LK2ebNmwjlQU/https/pbs.twimg.com/media/FOIg31UakAMU1kK.jpg?width=914&height=491')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update);
         if (update){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
