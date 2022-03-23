941945e57a5d235c926f3e814f0e211a55ab82e6
MauwMa <sgtmauwma@gmail.com>
2022-03-23 19:42:35 +0100

    Update update3.js

--- commands/update3.js
+++ commands/update3.js
@@ -6,11 +6,11 @@ module.exports = {
         const update3 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('⛨Virtuális Torony⛨ ')
+        .setTitle('')
         .setURL('')
         .setTimestamp()
-        .setDescription('Az új Sardegna Empire event, Virtuális Torony, Március 24.-én kezdődik. Ez az event Impero karatkert és még megannyit fog bemutatni.')
-        .setImage('https://images-ext-1.discordapp.net/external/XzN1YDWAvlXUrZn9TgY9rXiNI_nZBiqtlAJiriqpAak/https/pbs.twimg.com/media/FOYe6GOVgAE4QAi.jpg?width=981&height=491')
+        .setDescription('Parancsnok,\n\nA Manjuu Campus bútorzat szett a következő karbantartás után lesz elérhető. Szerezd meg ezt a szettet, hogy megtapasztald az izgalmas életet a Manjuu Campuson.')
+        .setImage('https://images-ext-1.discordapp.net/external/hElJOuQv1xNLA5lPC3sh56vsza3-zjpb1QFrVVOfOt8/https/pbs.twimg.com/media/FOhzw2hVEAMXnEl.jpg?width=829&height=491')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update3);
         if (update3){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
