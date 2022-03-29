b7a704e2144cfab4fd50f2436d440d85235392c4
MauwMa <sgtmauwma@gmail.com>
2022-03-29 16:16:15 +0200

    Create essex.js

---
+++ commands/essex.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'essex',
+    description: 'essex',
+    permissions: ["VIEW_CHANNEL"],
+    execute (client, message, args){
+
+        message.channel.send("<:Essex:827117741798457394>")
+
+    if (message.deletable) message.delete();
+
+    }    
+}
+
+
