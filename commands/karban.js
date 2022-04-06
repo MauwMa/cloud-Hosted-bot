191c41e102ed659348101c3e387688e51201d302
MauwMa <sgtmauwma@gmail.com>
2022-04-06 08:28:28 +0200

    dd

--- commands/karban.js
+++ commands/karban.js
@@ -8,7 +8,7 @@ module.exports = {
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
         .setTitle('Karbantartás!!')
         .setURL('')
-        .setDescription('Parancsnokok, 03/24 Csütörtökön Magyar Időszámítás szerint \nreggel 08:00-kor lesz karbantartás. Kérjük a beosztásaitokat tervezzétek e szerint nyaaa~')
+        .setDescription('Parancsnokok, 04/07 Csütörtökön Magyar Időszámítás szerint \nreggel 09:00-kor lesz karbantartás. Kérjük a beosztásaitokat tervezzétek e szerint nyaaa~')
         .setImage('https://images-ext-2.discordapp.net/external/5v50XmQ9nF9KyOXWmb0nOWfrE2ORMd0UFkonjtTr7Ec/https/pbs.twimg.com/media/FNQCnhJakAgtc2h.jpg?width=820&height=468')
         .setTimestamp()
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(karban);
