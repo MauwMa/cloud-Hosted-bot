a36ec4e0780fc4178d3fa0c2f48bc0dd20655f42
MauwMa <sgtmauwma@gmail.com>
2022-04-07 07:24:01 +0200

    Update egyora.js

--- commands/egyora.js
+++ commands/egyora.js
@@ -10,7 +10,7 @@ module.exports = {
             .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
             .setTitle('Értesítés!')
             .setURL('')
-            .setDescription('1 órás előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 6 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
+            .setDescription('1 órás előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 8 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
             .setImage('https://images-ext-1.discordapp.net/external/POW9Zcg0jlIx3yQZzLwD7S82LiDA-104cmXijUe4MFc/https/pbs.twimg.com/media/FOW3x8RVUAI1NHK.jpg?width=860&height=491')
             .setTimestamp()
             client.channels.cache.find(channel => channel.id === "789936292413571102").send(egyora); 
