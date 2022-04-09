b2e1337eb85aa7f014d633373f979510ccfaa16b
MauwMa <sgtmauwma@gmail.com>
2022-04-09 07:49:24 +0200

    Update update3.js

--- commands/update3.js
+++ commands/update3.js
@@ -9,8 +9,8 @@ module.exports = {
         .setTitle('')
         .setURL('')
         .setTimestamp()
-        .setDescription('Parancsnokok, \n\nHQ rengeteg visszajelzést kapott a frissítéssel kapcsolatban. HQ elnézéstkér a kényelmetlenségekért és 200 gemet küld kompenzációként.')
-        .setImage('https://images-ext-1.discordapp.net/external/VHcXcfw_rr8ZjVtL3qSm-i9uH06vb3lWAVvHg9T3IAw/https/pbs.twimg.com/media/FP0tsamVUAU4Y3b.jpg?width=883&height=504')
+        .setDescription('Parancsnokok,\n\nEzek a karakterek az új kinézetükbe öltöztek és felkészültek az új tevékenységekre~ Ezek a kinézetek véglegesen elérhetőek lesznek .')
+        .setImage('https://images-ext-2.discordapp.net/external/Vg05fKX03TSgZpuOxWeYLE7G3BOqlKkcCcUwXRZ9lJc/https/pbs.twimg.com/media/FP091nPVsAI_fX0.jpg?width=1022&height=503')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update3);
         if (update3){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
