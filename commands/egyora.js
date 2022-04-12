491db7d643b6f376a3f024cb1fce8090428c8f01
MauwMa <sgtmauwma@gmail.com>
2022-04-12 12:13:31 +0200

    dd

--- commands/egyora.js
+++ commands/egyora.js
@@ -4,13 +4,13 @@ module.exports = {
     permissions: ["MENTION_EVERYONE"],
     description: 'egyora',
     execute (client, message, args, Discord){
-        var exportScheduler = schedule.scheduleJob('0 40 6 7 4 *', function() { 
+        var exportScheduler = schedule.scheduleJob('0 0 6 14 4 *', function() { 
             const egyora = new Discord.MessageEmbed()
             .setColor('#ffff00')
             .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
             .setTitle('Értesítés!')
             .setURL('')
-            .setDescription('20 perces előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 8 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
+            .setDescription('1 órás előzetes értesítés a következő karbantartásról.\n Minden szerver előreláthatólag 8 órán keresztül le lesznek állítva. \nKérjük ne felejtsétek el megetetni a hajóitokat nyaa~!')
             .setImage('https://images-ext-1.discordapp.net/external/POW9Zcg0jlIx3yQZzLwD7S82LiDA-104cmXijUe4MFc/https/pbs.twimg.com/media/FOW3x8RVUAI1NHK.jpg?width=860&height=491')
             .setTimestamp()
             client.channels.cache.find(channel => channel.id === "789936292413571102").send(egyora); 
