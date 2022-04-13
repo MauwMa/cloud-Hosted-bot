5fbeb58db57a7c2a19cd57bb7eb9180c0303ce4b
MauwMa <sgtmauwma@gmail.com>
2022-04-13 08:09:01 +0200

    Update update3.js

--- commands/update3.js
+++ commands/update3.js
@@ -6,11 +6,11 @@ module.exports = {
         const update3 = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setAuthor("Azur Lane Hungary", "https://i.imgur.com/LxJJ70i.png")
-        .setTitle('【Aurora Noctis Rerun】')
+        .setTitle('')
         .setURL('')
         .setTimestamp()
-        .setDescription('Az Aurora Noctis Rerun a következő karbantartással fog elkezdődni. Jelentkezzbe az event időszakában, hogy Építés Jegyeket és Event Pontokat kapj. A további Jegyek a küldetések jutalmaiban lesznek.')
-        .setImage('https://images-ext-2.discordapp.net/external/Db3Fs2kSJuQZvzKdHHLx4FmAqn2St6TaDPszRMAorzs/https/pbs.twimg.com/media/FQDkIRJWYAIzqeM.png')
+        .setDescription('Parancsnokok, \n\n Ping Hai, Ning Hai, Bush és a többiek elérhetőek lesznek a Guild Shopban. Véglegesen kiválthatóak lesznek. Már meghívtad őket a kikötődbe?')
+        .setImage('https://images-ext-2.discordapp.net/external/1RIZiK-maGPxs8EP3v5rm8JDe9OlJJYOLxnbJVrrTKQ/https/pbs.twimg.com/media/FQIpV1_UYAEclU5.jpg?width=983&height=504')
         client.channels.cache.find(channel => channel.id === "789936292413571102").send(update3);
         if (update3){
             return message.channel.send(`Elküldtem az updatet nyaaa~`);
